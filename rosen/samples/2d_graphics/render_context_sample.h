a8c8af27e5b00a5267591d39cdd4242418758eac
mk_gc <mengkun@huawei.com>
2022-04-15 19:37:19 +0800

    Revert "update drawingengine sample code"
    This reverts commit aad2c9d3e968df16f21d58e8b0dff6734947e9de.
    
    Change-Id: I7d54eb411db06b6e5845fdfc60d43dd566c557c9
    Signed-off-by: mk_gc <mengkun@huawei.com>

--- rosen/samples/2d_graphics/render_context_sample.h
+++ rosen/samples/2d_graphics/render_context_sample.h
@@ -13,71 +13,81 @@
  * limitations under the License.
  */
 
-#ifndef DRAWING_ENGINE_SAMPLE_H
-#define DRAWING_ENGINE_SAMPLE_H
-
-#include "include/core/SkCanvas.h"
-#include "include/core/SkPaint.h"
+#ifndef RENDER_CONTEXT_SAMPLE_H
+#define RENDER_CONTEXT_SAMPLE_H
 
 #include <display_type.h>
 #include <surface.h>
-#include <event_handler.h>
+#include <sync_fence.h>
+
 #include "hdi_backend.h"
 #include "hdi_layer_info.h"
 #include "hdi_output.h"
 #include "hdi_screen.h"
-#include "drawing_engine/drawing_utils.h"
-#include "drawing_engine/drawing_proxy.h"
-#include "drawing_engine/drawing_surface/rs_surface_ohos.h"
+#include "render_context/render_context.h"
+#include <unordered_map>
 
 namespace OHOS {
 namespace Rosen {
-class DrawingEngineSample : public IBufferConsumerListenerClazz {
+
+class RenderContextSample : public IBufferConsumerListenerClazz {
 public:
-    DrawingEngineSample() = default;
-    virtual ~DrawingEngineSample() = default;
+    RenderContextSample() = default;
+    virtual ~RenderContextSample() = default;
+
     void Run();
+
 private:
     uint32_t freq_ = 30;
-    uint32_t display_w = 0;
-    uint32_t display_h = 0;
+    uint32_t display_w = 480;
+    uint32_t display_h = 960;
     uint32_t currentModeIndex_ = 0;
+    uint32_t backGroundWidth = 0;
+    uint32_t backGroundHeight = 0;
     uint32_t drawingWidth = 0;
     uint32_t drawingHeight = 0;
     std::vector<DisplayModeInfo> displayModeInfos_;
     std::vector<std::shared_ptr<HdiOutput>> outputs_;
-    std::shared_ptr<OHOS::AppExecFwk::EventHandler> handler;
+
+    sptr<Surface> backGroundCSurface;
+    sptr<Surface> backGroundPSurface;
     sptr<Surface> drawingCSurface;
     sptr<Surface> drawingPSurface;
 
     bool ready_ = false;
     bool initDeviceFinished_ = false;
     bool deviceConnected_ = false;
-    bool Initilized = false;
     std::unique_ptr<HdiScreen> screen_ = nullptr;
     HdiBackend* backend_ = nullptr;
     std::shared_ptr<HdiOutput> output_ = nullptr;
+    struct NativeWindow* nwindow = nullptr;
 
-    DrawingProxy* drawingProxy = nullptr;
+    RenderContext* renderContext = nullptr;
+    EGLSurface eglSurface = EGL_NO_SURFACE;
 
     std::unordered_map<uint64_t, sptr<SurfaceBuffer>> prevBufferMap_;
     std::unordered_map<uint64_t, sptr<SyncFence>> prevFenceMap_;
 
     void Init();
-    void InitContext();
-    void OutPutDisplay();
+    void InitEGL();
+    void Draw();
+    void DrawSurface();
     void Sync(int64_t, void *data);
     void CreatePhysicalScreen();
     void DoPrepareCompleted(sptr<Surface> &surface, const struct PrepareCompleteParam &param);
     virtual void OnBufferAvailable() override;
-    SurfaceError DoDraw();
-    void Draw(SkCanvas* canvas);
+    SurfaceError ProduceBackGroundBuffer(uint32_t width, uint32_t height);
+    SurfaceError ProduceDrawingBuffer(uint32_t width, uint32_t height);
+    bool FillDrawingLayer(std::shared_ptr<HdiLayerInfo> &showLayer, uint32_t index, uint32_t zorder, IRect &dstRect);
+    bool FillBackGroundLayer(std::shared_ptr<HdiLayerInfo> &showLayer, uint32_t zorder, IRect &dstRect);
+    bool DrawBackgroundLayer(std::shared_ptr<HdiLayerInfo> &layer);
     bool DrawDrawingLayer(std::shared_ptr<HdiLayerInfo> &layer);
+    void CreateBackGroundSurface();
     void CreateDrawingSurface();
     void OnHotPlugEvent(std::shared_ptr<HdiOutput> &output, bool connected);
+
     static void OnScreenPlug(std::shared_ptr<HdiOutput> &output, bool connected, void* data);
-    static void OnPrepareCompleted(
-        sptr<Surface> &surface, const struct PrepareCompleteParam &param, void* data);
+    static void OnPrepareCompleted(OHOS::sptr<Surface> &surface, const struct PrepareCompleteParam &param, void* data);
 };
 } // namespace Rosen
 } // namespace OHOS
