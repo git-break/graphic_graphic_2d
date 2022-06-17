675b2d70b96c264112ea11087233457be3d481cf
xxfeng_hw <yuxiaofeng8@huawei.com>
2022-06-17 14:01:43 +0800

    Reset every node's prevBuffers if we release them successfully, to avoid releasing the same buffers next frame.
    Signed-off-by: xxfeng_hw <yuxiaofeng8@huawei.com>
    Change-Id: Ibe758c5a961dc83784cb1b23eb493c2e9d2229d7

--- rosen/modules/render_service_base/include/pipeline/rs_surface_handler.h
+++ rosen/modules/render_service_base/include/pipeline/rs_surface_handler.h
@@ -29,6 +29,14 @@ public:
     virtual ~RSSurfaceHandler() noexcept = default;
 
     struct SurfaceBufferEntry {
+        void Reset()
+        {
+            buffer = nullptr;
+            acquireFence = SyncFence::INVALID_FENCE;
+            releaseFence = SyncFence::INVALID_FENCE;
+            damageRect = Rect {0, 0, 0, 0};
+            timestamp = 0;
+        }
         sptr<SurfaceBuffer> buffer;
         sptr<SyncFence> acquireFence = SyncFence::INVALID_FENCE;
         sptr<SyncFence> releaseFence = SyncFence::INVALID_FENCE;
@@ -86,7 +94,7 @@ public:
         preBuffer_.releaseFence = std::move(fence);
     }
 
-    SurfaceBufferEntry GetPreBuffer()
+    SurfaceBufferEntry& GetPreBuffer()
     {
         return preBuffer_;
     }
