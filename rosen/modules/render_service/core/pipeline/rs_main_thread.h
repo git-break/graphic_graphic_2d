482a63ab4b67a92def60df4d1b3e97c0f3829768
chenqinxin <chenqinxin1@huawei.com>
2022-08-26 09:50:12 +0800

    uni->non uni, command of move surfaceview should wait for visitor switch
    Signed-off-by: chenqinxin <chenqinxin1@huawei.com>
    Change-Id: I442d0f61c4a26971d1eb9e4a7e05dd628eafebbf

--- rosen/modules/render_service/core/pipeline/rs_main_thread.h
+++ rosen/modules/render_service/core/pipeline/rs_main_thread.h
@@ -165,6 +165,7 @@ private:
     std::unordered_map<NodeId, std::map<uint64_t, std::vector<std::unique_ptr<RSCommand>>>> cachedCommands_;
     std::vector<std::unique_ptr<RSCommand>> effectiveCommands_;
     std::vector<std::unique_ptr<RSCommand>> pendingEffectiveCommands_;
+    std::vector<std::unique_ptr<RSCommand>> followVisitorCommands_;
 
     TransactionDataMap cachedTransactionDataMap_;
     TransactionDataIndexMap effectiveTransactionDataIndexMap_;
