35535d8f71e5b204f06fdba56976be279fe2b245
Zhang Peng <zhangpeng280@huawei.com>
2022-11-24 16:14:59 +0800

    add ExtractPid util function
    Signed-off-by: Zhang Peng <zhangpeng280@huawei.com>
    Change-Id: I5f3063c937b302b6a3235894b1c29ed256ed36c2

--- rosen/modules/render_service_base/include/pipeline/rs_surface_render_node.h
+++ rosen/modules/render_service_base/include/pipeline/rs_surface_render_node.h
@@ -446,7 +446,7 @@ public:
 
     bool IsFocusedWindow(pid_t focusedWindowPid)
     {
-        return static_cast<pid_t>(GetNodeId() >> 32) == focusedWindowPid; // higher 32 bits of nodeid is pid
+        return ExtractPid(GetNodeId()) == focusedWindowPid;
     }
 
     Occlusion::Region ResetOpaqueRegion(const RectI& absRect,
