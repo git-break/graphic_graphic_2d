0f11b089bdf2c6807f2bac62cf8ff120fa781ecb
w200200 <wangyonglang@huawei.com>
2023-05-05 14:00:21 +0800

    Add the remaining of measurer_impl.cpp
    Change-Id: I6a51ff7f88c0dea85f5d2c6ec46a99a59e16e7b4
    Signed-off-by: w200200 <wangyonglang@huawei.com>

--- rosen/modules/texgine/src/measurer_impl.cpp
+++ rosen/modules/texgine/src/measurer_impl.cpp
@@ -256,6 +256,185 @@ void MeasurerImpl::DoSeekScript(std::list<struct MeasuringRun> &runs, hb_unicode
         it->script = script;
     }
 }
+
+int MeasurerImpl::Shape(CharGroups &cgs, std::list<struct MeasuringRun> &runs, std::vector<Boundary> boundaries)
+{
+    ScopedTrace scope("MeasurerImpl::Shape");
+    cgs = CharGroups::CreateEmpty();
+    LOGSCOPED(sl, LOG2EX_DEBUG(), "shape");
+    size_t index = 0;
+    for (auto &run : runs) {
+        if (run.end <= run.start) {
+            LOG2EX(ERROR) << "run have error range";
+            throw TEXGINE_EXCEPTION(ErrorStatus);
+        }
+
+        std::stringstream ss;
+        ss << "[" << run.start << ", " << run.end << ")";
+        LOGSCOPED(sl, LOG2EX_DEBUG(), ss.str());
+
+        if (DoShape(cgs, run, index)) {
+            return 1;
+        }
+    }
+
+    for (auto &[start, end] : boundaries) {
+        const auto &wordcgs = cgs.GetSubFromU16RangeAll(start, end);
+        wordcgs.Get(wordcgs.GetNumberOfCharGroup() - 1).invisibleWidth_ += wordSpacing_;
+    }
+    return 0;
+}
+
+int MeasurerImpl::DoShape(CharGroups &cgs, MeasuringRun &run, int &index)
+{
+    auto typeface = run.typeface;
+    if (typeface == nullptr) {
+        LOG2EX(ERROR) << "there is no typeface";
+        return 1;
+    }
+
+    auto hbuffer = hb_buffer_create();
+    if (!hbuffer) {
+        LOG2EX(ERROR) << "hbuffer is nullptr";
+        return 1;
+    }
+
+    hb_buffer_add_utf16(hbuffer, reinterpret_cast<const uint16_t *>(text_.data()),
+        INVALID_TEXT_LENGTH, run.start, run.end - run.start);
+    hb_buffer_set_direction(hbuffer, rtl_ ? HB_DIRECTION_RTL : HB_DIRECTION_LTR);
+    hb_buffer_set_unicode_funcs(hbuffer, hb_unicode_funcs_create(hb_icu_get_unicode_funcs()));
+    hb_buffer_set_script(hbuffer, run.script);
+    hb_buffer_set_language(hbuffer, hb_language_from_string(locale_.c_str(), INVALID_TEXT_LENGTH));
+
+    auto hface = hb_face_create_for_tables(HbFaceReferenceTableTypeface, typeface->Get()->GetTypeface().get(), 0);
+    if (!hface) {
+        LOG2EX(ERROR) << "hface is nullptr";
+        return 1;
+    }
+
+    auto hfont = hb_font_create(hface);
+    if (!hfont) {
+        LOG2EX(ERROR) << "hfont is nullptr";
+        return 1;
+    }
+
+    std::vector<hb_feature_t> ff;
+    GenerateHBFeatures(ff, fontFeatures_);
+    hb_shape(hfont, hbuffer, ff.data(), ff.size());
+
+    if (GetGlyphs(cgs, run, index, hbuffer, typeface)) {
+        return 1;
+    }
+
+    hb_buffer_destroy(hbuffer);
+    hb_font_destroy(hfont);
+    hb_face_destroy(hface);
+    return 0;
+}
+
+int MeasurerImpl::GetGlyphs(CharGroups &cgs, MeasuringRun &run, int &index, hb_buffer_t *hbuffer,
+    std::shared_ptr<Texgine::Typeface> typeface)
+{
+    uint32_t ng = 0u;
+    auto hginfos = hb_buffer_get_glyph_infos(hbuffer, &ng);
+    if (!hginfos) {
+        LOG2EX(ERROR) << "hginfos is nullptr";
+        return 1;
+    }
+
+    auto hgpositions = hb_buffer_get_glyph_positions(hbuffer, nullptr);
+    if (!hgpositions) {
+        LOG2EX(ERROR) << "hgpositions is nullptr";
+        return 1;
+    }
+
+    LOG2EX_DEBUG() << "ng: " << ng;
+    auto upe = typeface->Get()->GetUnitsPerEm();
+    if (!upe) {
+        LOG2EX(ERROR) << "upe is 0";
+        return 1;
+    }
+
+    auto glyphEm = 1.0 * size_ / upe;
+    std::map<uint32_t, CharGroup> cgsByCluster{{run.end, {}}};
+    for (uint32_t i = 0; i < ng; i++) {
+        auto &cg = cgsByCluster[hginfos[i].cluster];
+        cg.glyphs_.push_back({
+            .codepoint_ = hginfos[i].codepoint,
+            .advanceX_ = glyphEm * hgpositions[i].x_advance,
+            .advanceY_ = glyphEm * hgpositions[i].y_advance,
+            .offsetX_ = glyphEm * hgpositions[i].x_offset,
+            .offsetY_ = glyphEm * hgpositions[i].y_offset,
+        });
+        cg.typeface_ = typeface;
+    }
+
+    DoCgsByCluster(cgsByCluster);
+    cgsByCluster.erase(run.end);
+    for (uint32_t i = 0; i < ng; i++) {
+        DumpCharGroup(index++, cgsByCluster[hginfos[i].cluster], glyphEm, hginfos[i], hgpositions[i]);
+    }
+
+    for (const auto &[cluster, cg] : cgsByCluster) {
+        cgs.PushBack(cg);
+    }
+
+    if (rtl_) {
+        cgs.ReverseAll();
+    }
+    return 0;
+}
+
+void MeasurerImpl::DoCgsByCluster(std::map<uint32_t, Texgine::CharGroup> &cgsByCluster)
+{
+    for (auto it = cgsByCluster.begin(); it != cgsByCluster.end(); it++) {
+        auto start = (it++)->first;
+        if (it == cgsByCluster.end()) {
+            break;
+        }
+
+        auto end = (it--)->first;
+        if (start > text_.size() || end > text_.size()) {
+            LOG2EX(ERROR) << "cgsByCluster is not align with text_";
+            throw TEXGINE_EXCEPTION(ErrorStatus);
+        }
+
+        it->second.chars_.insert(it->second.chars_.end(), text_.begin() + start, text_.begin() + end);
+        it->second.visibleWidth_ = 0;
+        for (const auto &glyph : it->second.glyphs_) {
+            it->second.visibleWidth_ += glyph.advanceX_;
+        }
+
+        it->second.invisibleWidth_ = letterSpacing_;
+        if (u_isWhitespace(it->second.chars_[0]) != 0) {
+            it->second.invisibleWidth_ += it->second.visibleWidth_;
+            it->second.visibleWidth_ = 0;
+        }
+    }
+}
+
+void MeasurerImpl::GenerateHBFeatures(std::vector<hb_feature_t> &fontFeatures, const FontFeatures *ff)
+{
+    LOGSCOPED(sl, LOG2EX_DEBUG(), "GenerateHBFeatures");
+    if (ff == nullptr) {
+        return;
+    }
+
+    auto features = ff->GetFeatures();
+    if (features.empty()) {
+        return;
+    }
+
+    for (auto &[ft, fv] : features) {
+        hb_feature_t hf;
+        if (hb_feature_from_string(ft.c_str(), ft.size(), &hf)) {
+            hf.value = fv;
+            fontFeatures.push_back(hf);
+        }
+
+        LOG2EX_DEBUG() << "feature tag:" << hf.tag << ", feature value:" << hf.value;
+    }
+}
 } // namespace TextEngine
 } // namespace Rosen
 } // namespace OHOS
