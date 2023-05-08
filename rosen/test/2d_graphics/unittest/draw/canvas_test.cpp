02ffa736c16345760a08dc19d51169a1a5a6adb1
huangzejia1 <huangzejia1@huawei.com>
2023-05-08 15:36:52 +0800

    [drawin/UT] draw/canvas-3
    Signed-off-by: huangzejia1 <huangzejia1@huawei.com>
    Change-Id: I9102cb55e742e1b8d7f68de827481d013ceb0a66

--- rosen/test/2d_graphics/unittest/draw/canvas_test.cpp
+++ rosen/test/2d_graphics/unittest/draw/canvas_test.cpp
@@ -48,6 +48,207 @@ HWTEST_F(CanvasTest, CreateAndDestroy001, TestSize.Level1)
     auto canvas = std::make_unique<Canvas>();
     EXPECT_TRUE(nullptr != canvas);
 }
+
+/**
+ * @tc.name: CanvasSaveLayerTest001
+ * @tc.desc:
+ * @tc.type: FUNC
+ * @tc.require: AR000GGNV3
+ * @tc.author:
+ */
+HWTEST_F(CanvasTest, CanvasSaveLayerTest001, TestSize.Level1)
+{
+    auto canvas = std::make_unique<Canvas>();
+    ASSERT_TRUE(nullptr != canvas);
+    SaveLayerOps saveLayerOps;
+    canvas->SaveLayer(saveLayerOps);
+}
+
+/**
+ * @tc.name: CanvasSaveLayerTest002
+ * @tc.desc:
+ * @tc.type: FUNC
+ * @tc.require: AR000GGNV3
+ * @tc.author:
+ */
+HWTEST_F(CanvasTest, CanvasSaveLayerTest002, TestSize.Level1)
+{
+    auto canvas = std::make_unique<Canvas>();
+    ASSERT_TRUE(nullptr != canvas);
+    Rect rect(0.0f, 0.0f, 10.0f, 20.0f);
+    Brush brush;
+    uint32_t saveLayerFlags = 0;
+    SaveLayerOps saveLayerRec(&rect, &brush, saveLayerFlags);
+    canvas->SaveLayer(saveLayerRec);
+}
+
+/**
+ * @tc.name: CanvasRestoreTest001
+ * @tc.desc:
+ * @tc.type: FUNC
+ * @tc.require: AR000GGNV3
+ * @tc.author:
+ */
+HWTEST_F(CanvasTest, CanvasRestoreTest001, TestSize.Level1)
+{
+    auto canvas = std::make_unique<Canvas>();
+    ASSERT_TRUE(nullptr != canvas);
+    Rect rect(0.0f, 0.0f, 10.0f, 20.0f);
+    Brush brush;
+    uint32_t saveLayerFlags = 0;
+    SaveLayerOps saveLayerRec(&rect, &brush, saveLayerFlags);
+    canvas->SaveLayer(saveLayerRec);
+    canvas->Restore();
+}
+
+/**
+ * @tc.name: CanvasGetSaveCountTest001
+ * @tc.desc:
+ * @tc.type: FUNC
+ * @tc.require: AR000GGNV3
+ * @tc.author:
+ */
+HWTEST_F(CanvasTest, CanvasGetSaveCountTest001, TestSize.Level1)
+{
+    auto canvas = std::make_unique<Canvas>();
+    ASSERT_TRUE(nullptr != canvas);
+    canvas->Save();
+    EXPECT_TRUE(2 == canvas->GetSaveCount());
+}
+
+/**
+ * @tc.name: CanvasRestoreToCountTest001
+ * @tc.desc:
+ * @tc.type: FUNC
+ * @tc.require: AR000GGNV3
+ * @tc.author:
+ */
+HWTEST_F(CanvasTest, CanvasRestoreToCountTest001, TestSize.Level1)
+{
+    auto canvas = std::make_unique<Canvas>();
+    ASSERT_TRUE(nullptr != canvas);
+    canvas->RestoreToCount(2);
+}
+
+/**
+ * @tc.name: CanvasAttachAndDetachPenTest001
+ * @tc.desc:
+ * @tc.type: FUNC
+ * @tc.require: AR000GGNV3
+ * @tc.author:
+ */
+HWTEST_F(CanvasTest, CanvasAttachAndDetachPenTest001, TestSize.Level1)
+{
+    auto canvas = std::make_unique<Canvas>();
+    ASSERT_TRUE(nullptr != canvas);
+    Pen pen(Color::COLOR_GREEN);
+    canvas->AttachPen(pen);
+    canvas->DetachPen();
+}
+
+/**
+ * @tc.name: CanvasAttachAndDetachBrushTest001
+ * @tc.desc:
+ * @tc.type: FUNC
+ * @tc.require: AR000GGNV3
+ * @tc.author:
+ */
+HWTEST_F(CanvasTest, CanvasAttachAndDetachBrushTest001, TestSize.Level1)
+{
+    auto canvas = std::make_unique<Canvas>();
+    ASSERT_TRUE(nullptr != canvas);
+    Brush brush(Color::COLOR_GREEN);
+    canvas->AttachBrush(brush);
+    canvas->DetachBrush();
+}
+
+/**
+ * @tc.name: GetBounds001
+ * @tc.desc:
+ * @tc.type: FUNC
+ * @tc.require: AR000GGNV3
+ * @tc.author:
+ */
+HWTEST_F(CanvasTest, GetBounds001, TestSize.Level1)
+{
+    Rect rect(0.0f, 0.0f, 10.0f, 20.0f);
+    Brush brush;
+    uint32_t saveLayerFlags = 0;
+    ImageFilter imageFilter(ImageFilter::FilterType::BLUR, 10.0f, 10.0f, nullptr);
+    SaveLayerOps saveLayerRec(&rect, &brush, &imageFilter, saveLayerFlags);
+    auto ret = saveLayerRec.GetBounds();
+    EXPECT_EQ(ret->GetLeft(), 0.0f);
+    EXPECT_EQ(ret->GetBottom(), 20.0f);
+}
+
+/**
+ * @tc.name: GetBrush001
+ * @tc.desc:
+ * @tc.type: FUNC
+ * @tc.require: AR000GGNV3
+ * @tc.author:
+ */
+HWTEST_F(CanvasTest, GetBrush001, TestSize.Level1)
+{
+    Rect rect(0.0f, 0.0f, 10.0f, 20.0f);
+    Brush brush;
+    uint32_t saveLayerFlags = 0;
+    ImageFilter imageFilter(ImageFilter::FilterType::BLUR, 10.0f, 10.0f, nullptr);
+    SaveLayerOps saveLayerRec(&rect, &brush, &imageFilter, saveLayerFlags);
+    auto ret = saveLayerRec.GetBrush();
+    EXPECT_TRUE(ret != nullptr);
+}
+
+/**
+ * @tc.name: GetImageFilter001
+ * @tc.desc:
+ * @tc.type: FUNC
+ * @tc.require: AR000GGNV3
+ * @tc.author:
+ */
+HWTEST_F(CanvasTest, GetImageFilter001, TestSize.Level1)
+{
+    Rect rect(0.0f, 0.0f, 10.0f, 20.0f);
+    Brush brush;
+    uint32_t saveLayerFlags = 0;
+    ImageFilter imageFilter(ImageFilter::FilterType::BLUR, 10.0f, 10.0f, nullptr);
+    SaveLayerOps saveLayerRec(&rect, &brush, &imageFilter, saveLayerFlags);
+    auto ret = saveLayerRec.GetImageFilter();
+    EXPECT_TRUE(ret != nullptr);
+}
+
+/**
+ * @tc.name: GetSaveLayerFlags001
+ * @tc.desc:
+ * @tc.type: FUNC
+ * @tc.require: AR000GGNV3
+ * @tc.author:
+ */
+HWTEST_F(CanvasTest, GetSaveLayerFlags001, TestSize.Level1)
+{
+    Rect rect(0.0f, 0.0f, 10.0f, 20.0f);
+    Brush brush;
+    uint32_t saveLayerFlags = 0;
+    ImageFilter imageFilter(ImageFilter::FilterType::BLUR, 10.0f, 10.0f, nullptr);
+    SaveLayerOps saveLayerRec(&rect, &brush, &imageFilter, saveLayerFlags);
+    auto ret = saveLayerRec.GetSaveLayerFlags();
+    EXPECT_EQ(ret, 0);
+}
+
+/**
+ * @tc.name: AutoCanvasRestoreTest001
+ * @tc.desc:
+ * @tc.type: FUNC
+ * @tc.require: AR000GGNV3
+ * @tc.author:
+ */
+HWTEST_F(CanvasTest, AutoCanvasRestoreTest001, TestSize.Level1)
+{
+    Canvas canvas;
+    bool doSave = true;
+    auto autoCanvasRestore = std::make_unique<AutoCanvasRestore>(canvas, doSave);
+    ASSERT_TRUE(nullptr != autoCanvasRestore);
+}
 } // namespace Drawing
 } // namespace Rosen
 } // namespace OHOS
