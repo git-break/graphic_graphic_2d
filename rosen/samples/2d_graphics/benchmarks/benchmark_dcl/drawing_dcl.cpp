a4097033b412384df0c82329cdeb9eacdb241b69
凯鑫 <liukaixin@huawei.com>
2023-05-09 21:11:16 +0800

    add some print
    Signed-off-by: 凯鑫 <liukaixin@huawei.com>

--- rosen/samples/2d_graphics/benchmarks/benchmark_dcl/drawing_dcl.cpp
+++ rosen/samples/2d_graphics/benchmarks/benchmark_dcl/drawing_dcl.cpp
@@ -230,7 +230,7 @@ public:
             std::cout << "MyAllocator::Dealloc data addr not match!" << std::endl;
         }
         if (mapFile_ != nullptr) {
-            if (::munmap(mapFile_, size_)) {
+            if (::munmap(mapFile_, size_) == -1) {
                 std::cout << "munmap failed!" <<std::endl;
             }
             mapFile_ = nullptr;
