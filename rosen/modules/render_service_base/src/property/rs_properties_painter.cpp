b1dbda49aea52f6eed8ae3d0388ea38137c790dc
g00649947 <gaoweihua2@huawei.com>
2023-06-09 10:48:10 +0800

    resolving the conflict between blur and opacity
    Signed-off-by: g00649947 <gaoweihua2@huawei.com>

--- rosen/modules/render_service_base/src/property/rs_properties_painter.cpp
+++ rosen/modules/render_service_base/src/property/rs_properties_painter.cpp
@@ -850,6 +850,8 @@ void RSPropertiesPainter::DrawFilter(const RSProperties& properties, RSPaintFilt
     filter->PreProcess(imageSnapshot);
     canvas.resetMatrix();
     auto visibleIRect = canvas.GetVisibleRect().round();
+    canvas.SaveAlpha();
+    canvas.SetAlpha(1.0);
     if (visibleIRect.intersect(clipIBounds)) {
         canvas.clipRect(SkRect::Make(visibleIRect));
         auto visibleIPadding = visibleIRect.makeOutset(-1, -1);
@@ -873,6 +875,7 @@ void RSPropertiesPainter::DrawFilter(const RSProperties& properties, RSPaintFilt
             SkRect::Make(clipIPadding), &paint);
 #endif
     }
+    canvas.RestoreAlpha();
     filter->PostProcess(canvas);
 }
 #else
