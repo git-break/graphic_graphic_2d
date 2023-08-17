021baa5c990722d0655547bbb96d077480197931
c00607827 <cenyouyi@huawei.com>
2023-08-17 21:54:42 +0800

    cleancode
    Signed-off-by: c00607827 <cenyouyi@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
+++ rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
@@ -15,6 +15,7 @@
 #include "pipeline/rs_main_thread.h"
 
 #include <list>
+#include <algorithm>
 #include "include/core/SkGraphics.h"
 #include "memory/rs_memory_graphic.h"
 #include "pipeline/parallel_render/rs_sub_thread_manager.h"
@@ -525,12 +526,11 @@ void RSMainThread::CheckParallelSubThreadNodesStatus()
 
 bool RSMainThread::IsNeedSkip(NodeId instanceRootNodeId, pid_t pid)
 {
-    for (auto& cacheCmdSkipNodeId: cacheCmdSkippedInfo_[pid].first) {
-        if (cacheCmdSkipNodeId == instanceRootNodeId) {
-            return true;
+    return std::any_of(cacheCmdSkippedInfo_[pid].first.begin(), cacheCmdSkippedInfo_[pid].first.end(),
+        [&](const auto& cacheCmdSkipNodeId) {
+            return cacheCmdSkipNodeId == instanceRootNodeId;
         }
-    }
-    return false;
+    );
 }
 
 void RSMainThread::SkipCommandByNodeId(std::vector<std::unique_ptr<RSTransactionData>>& transactionVec, pid_t pid)
