fd5348a849aa83889c9504c29110d7db7a1f5dfc
zhumingming <1095916953@qq.com>
2023-08-20 10:13:23 +0000

    修复RSCanvasDrawingRenderNode没有清理GPU资源。
    Signed-off-by: zhumingming <1095916953@qq.com>

--- rosen/modules/render_service_base/src/pipeline/rs_canvas_drawing_render_node.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_canvas_drawing_render_node.cpp
@@ -18,6 +18,7 @@
 #include "include/core/SkCanvas.h"
 #ifdef NEW_SKIA
 #include "include/gpu/GrBackendSurface.h"
+#include "include/gpu/GrDirectContext.h"
 #endif
 
 #include "common/rs_common_def.h"
@@ -43,6 +44,29 @@ RSCanvasDrawingRenderNode::~RSCanvasDrawingRenderNode()
     if (preThreadInfo_.second && surface_) {
         preThreadInfo_.second(std::move(surface_));
     }
+#endif
+}
+
+void RSCanvasDrawingRenderNode::OnTreeStateChanged()
+{
+    RSRenderNode::OnTreeStateChanged();
+    if (IsOnTheTree()) {
+        return;
+    }
+    
+#ifndef USE_ROSEN_DRAWING
+#if (defined RS_ENABLE_GL)
+    #ifdef NEW_SKIA
+        auto grContext = canvas_ != nullptr ? static_cast<GrDirectContext*>(canvas_->recordingContext()) : nullptr;
+    #else
+    #endif
+    if (grContext == nullptr) {
+        RS_LOGE("RSCanvasDrawingRenderNode: GrContext is nullptr");
+        return;
+    }
+    grContext->purgeUnlockedResources(true);
+#else 
+#endif
 #endif
 }
 
