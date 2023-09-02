6a145b58a5c21042c805c3f6de4724bbe72f3144
zhaoxiaoyu <zhaoxiaoyu7@huawei.com>
2023-09-02 15:51:29 +0800

    remove sync transaction for RT
    Signed-off-by: zhaoxiaoyu <zhaoxiaoyu7@huawei.com>
    Change-Id: I1825598d742c0692ae6892315cf6182f193a3def

--- rosen/modules/render_service_client/core/transaction/rs_transaction.cpp
+++ rosen/modules/render_service_client/core/transaction/rs_transaction.cpp
@@ -37,7 +37,7 @@ void RSTransaction::OpenSyncTransaction()
     if (transactionProxy != nullptr) {
         RS_TRACE_NAME("OpenSyncTransaction");
         transactionProxy->StartSyncTransaction();
-        transactionProxy->Begin();
+        transactionProxy->CreateSyncTransaction();
     }
 }
 
@@ -60,7 +60,7 @@ void RSTransaction::Begin()
     if (transactionProxy != nullptr) {
         RS_TRACE_NAME("BeginSyncTransaction");
         transactionProxy->StartSyncTransaction();
-        transactionProxy->Begin();
+        transactionProxy->CreateSyncTransaction();
     }
 }
 
