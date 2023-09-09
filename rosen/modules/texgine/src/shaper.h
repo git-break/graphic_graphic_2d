27ff87b2ab5d3b365ca1c2f8a794ad1035854a1c
huangzejia1 <huangzejia1@huawei.com>
2023-09-09 17:56:35 +0800

    !6219 修复固有宽度计算的问题
    Change-Id: Icc2a24b382c652d776d2eddb261f93713e298054
    Signed-off-by: huangzejia1 <huangzejia1@huawei.com>

--- rosen/modules/texgine/src/shaper.h
+++ rosen/modules/texgine/src/shaper.h
@@ -35,6 +35,8 @@ public:
     std::vector<LineMetrics> DoShape(std::vector<VariantSpan> spans, const TypographyStyle &tstyle,
         const std::shared_ptr<FontProviders> &fontProviders, const double widthLimit);
     bool DidExceedMaxLines() const;
+    double GetMinIntrinsicWidth() const;
+    double GetMaxIntrinsicWidth() const;
 
 private:
     std::vector<LineMetrics> DoShapeBeforeEllipsis(std::vector<VariantSpan> spans, const TypographyStyle &tstyle,
@@ -43,8 +45,12 @@ private:
         const std::shared_ptr<FontProviders> &fontProviders, const double widthLimit);
     std::vector<LineMetrics> CreateEllipsisSpan(const TypographyStyle &ys,
         const std::shared_ptr<FontProviders> &fontProviders);
+    void ComputeIntrinsicWidth(const size_t maxLines);
+
     std::vector<LineMetrics> lineMetrics_;
     bool didExceedMaxLines_ = false;
+    double maxIntrinsicWidth_ = 0.0;
+    double minIntrinsicWidth_ = 0.0;
 };
 } // namespace TextEngine
 } // namespace Rosen
