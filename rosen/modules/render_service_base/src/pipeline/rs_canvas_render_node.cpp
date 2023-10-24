2b2bb18c655ed4e64db39d3f3d47c40395ec05f7
Zhihang Deng <dengzhihang@huawei.com>
2023-10-24 10:02:57 +0800

    Fixed some code style issues.
    Signed-off-by: Zhihang Deng <dengzhihang@huawei.com>

--- rosen/modules/render_service_base/src/pipeline/rs_canvas_render_node.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_canvas_render_node.cpp
@@ -44,10 +44,9 @@ constexpr PropertyId ANONYMOUS_MODIFIER_ID = 0;
 
 static inline SkBlendMode ConvertToSkBlendMode(int blendMode)
 {
-    static const std::unordered_map<int, SkBlendMode> skBlendModeLUT = 
-    {
-        { static_cast<int>(RSColorBlendModeType::DST_IN), SkBlendMode::kDstIn },
-        { static_cast<int>(RSColorBlendModeType::SRC_IN), SkBlendMode::kSrcIn }
+    static const std::unordered_map<int, SkBlendMode> skBlendModeLUT = {
+        {static_cast<int>(RSColorBlendModeType::DST_IN), SkBlendMode::kDstIn},
+        {static_cast<int>(RSColorBlendModeType::SRC_IN), SkBlendMode::kSrcIn}
     };
 
     auto iter = skBlendModeLUT.find(blendMode);
@@ -145,13 +144,11 @@ void RSCanvasRenderNode::ProcessAnimatePropertyBeforeChildren(RSPaintFilterCanva
     ApplyDrawCmdModifier(context, RSModifierType::TRANSITION);
     ApplyDrawCmdModifier(context, RSModifierType::ENV_FOREGROUND_COLOR);
     RSPropertiesPainter::DrawShadow(GetRenderProperties(), canvas);
-
     // Inter-UI component blur & blending effect -- An empty layer
     int blendMode = GetRenderProperties().GetColorBlendMode();
     if (blendMode != static_cast<int>(RSColorBlendModeType::NONE)) {
         canvas.saveLayer(nullptr, nullptr);
     }
-
     // In NEW_SKIA version, L96 code will cause dump if the 3rd parameter is true.
 #ifdef NEW_SKIA
     RSPropertiesPainter::DrawBackground(GetRenderProperties(), canvas, false);
@@ -192,7 +189,6 @@ void RSCanvasRenderNode::ProcessAnimatePropertyBeforeChildren(RSPaintFilterCanva
         RSPropertiesPainter::Clip(canvas, GetRenderProperties().GetFrameRect());
 #endif
     }
-
     // Inter-UI component blur & blending effect -- A Mask layer
     if (blendMode != static_cast<int>(RSColorBlendModeType::NONE)) {
         SkBlendMode skBlendMode = ConvertToSkBlendMode(blendMode);
