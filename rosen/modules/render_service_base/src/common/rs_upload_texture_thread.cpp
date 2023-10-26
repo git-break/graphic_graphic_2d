40bf570de9cb47b20910fac3fc69b3e8f500c9d8
WenyiRoad <zhouqin33@huawei.com>
2023-10-26 04:39:00 +0000

    update rosen/modules/render_service_base/src/common/rs_upload_texture_thread.cpp.
    Signed-off-by: WenyiRoad <zhouqin33@huawei.com>

--- rosen/modules/render_service_base/src/common/rs_upload_texture_thread.cpp
+++ rosen/modules/render_service_base/src/common/rs_upload_texture_thread.cpp
@@ -104,7 +104,7 @@ sk_sp<GrDirectContext> RSUploadTextureThread::CreateShareGrContext()
     auto handler = std::make_shared<MemoryHandler>();
     auto glesVersion = reinterpret_cast<const char*>(glGetString(GL_VERSION));
     auto size = glesVersion ? strlen(glesVersion) : 0;
-    /* /data/service/el0/render_service is shader cache dir*/
+    /* /data/service/el0/render_service is shader cache dir */
     handler->ConfigureContext(&options, glesVersion, size, "/data/service/el0/render_service", true);
 
     sk_sp<GrDirectContext> grContext = GrDirectContext::MakeGL(std::move(glInterface), options);
