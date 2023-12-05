a7fb6219ba84fa1e798f0b1809b1622de9828ed8
andrew0229 <zhangzhao62@huawei.com>
2023-12-05 13:34:43 +0000

    fix vk compile error
    Signed-off-by: andrew0229 <zhangzhao62@huawei.com>
    Change-Id: I2e86cc251bb74fbd14e7d53addc0b5fbe4ae8c3f

--- rosen/modules/render_service_base/src/platform/ohos/backend/rs_vulkan_context.cpp
+++ rosen/modules/render_service_base/src/platform/ohos/backend/rs_vulkan_context.cpp
@@ -268,15 +268,15 @@ bool RsVulkanContext::CreateSkiaBackendContext(GrVkBackendContext* context, bool
     VkPhysicalDeviceFeatures features;
     vkGetPhysicalDeviceFeatures(physicalDevice_, &features);
 
-    uint32_t features = 0;
+    uint32_t skia_features = 0;
     if (features.geometryShader) {
-        features |= kGeometryShader_GrVkFeatureFlag;
+        skia_features |= kGeometryShader_GrVkFeatureFlag;
     }
     if (features.dualSrcBlend) {
-        features |= kDualSrcBlend_GrVkFeatureFlag;
+        skia_features |= kDualSrcBlend_GrVkFeatureFlag;
     }
     if (features.sampleRateShading) {
-        features |= kSampleRateShading_GrVkFeatureFlag;
+        skia_features |= kSampleRateShading_GrVkFeatureFlag;
     }
 
     context->fInstance = instance_;
@@ -301,7 +301,7 @@ bool RsVulkanContext::CreateSkiaBackendContext(GrVkBackendContext* context, bool
 
     context->fVkExtensions = &skVkExtensions_;
     context->fDeviceFeatures2 = &physicalDeviceFeatures2_;
-    context->fFeatures = features;
+    context->fFeatures = skia_features;
     context->fGetProc = std::move(getProc);
     context->fOwnsInstanceAndDevice = false;
 
