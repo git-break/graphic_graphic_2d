clipsheep6/graphic_2d

add vkpipelinecache
Signed-off-by: yangchunlin <yangchunlin4@huawei.com>

rosen/modules/2d_graphics/src/render_context/cache_data.cpp
@@ -184,30 +184,31 @@ void CacheData::Rewrite(const void *key, const size_t keySize, const void *value
     cleanThreshold_ = 0;
 }
 
-size_t CacheData::Get(const void *key, const size_t keySize, void *value, const size_t valueSize)
+std::tuple<CacheData::ErrorCode, size_t> CacheData::Get(const void *key, const size_t keySize,
+    void *value, const size_t valueSize)
 {
     if (maxKeySize_ < keySize) {
         LOGE("abandon, because the key is too large");
-        return 0;
+        return {ErrorCode::VALUE_SIZE_OVER_MAX_SIZE, 0};
     }
     std::shared_ptr<DataPointer> fakeDataPointer(std::make_shared<DataPointer>(key, keySize, false));
     ShaderPointer fakeShaderPointer(fakeDataPointer, nullptr);
     auto index = std::lower_bound(shaderPointers_.begin(), shaderPointers_.end(), fakeShaderPointer);
     if (index == shaderPointers_.end() || fakeShaderPointer < *index) {
         LOGD("abandon, because no key is found");
-        return 0;
+        return {ErrorCode::KEY_NOT_FOUND, 0};
     }
     std::shared_ptr <DataPointer> valuePointer(index->GetValuePointer());
     size_t valuePointerSize = valuePointer->GetSize();
     if (valuePointerSize > valueSize) {
         LOGE("abandon, because of insufficient buffer space");
-        return 0;
+        return {ErrorCode::VALUE_SIZE_TOO_SAMLL, valuePointerSize};
     }
     if (memcpy_s(value, valueSize, valuePointer->GetData(), valuePointerSize)) {
         LOGE("abandon, failed to copy content");
-        return 0;
+        return {ErrorCode::COPY_FAILED, 0};
     }
-    return valuePointerSize;
+    return {ErrorCode::NO_ERROR, valuePointerSize};
 }
 
 size_t CacheData::SerializedSize() const

rosen/modules/2d_graphics/src/render_context/shader_cache.cpp
@@ -17,9 +17,11 @@
 
 #include <algorithm>
 #include <array>
+#include <cstddef>
 #include <openssl/sha.h>
 #include <random>
 #include <thread>
+#include <tuple>
 #include "rs_trace.h"
 #include "utils/log.h"
 
@@ -61,7 +63,7 @@ void ShaderCache::InitShaderCache(const char* identity, const size_t size, bool 
     std::array<uint8_t, SHA256_DIGEST_LENGTH> shaArray;
     auto key = ID_KEY;
 
-    auto loaded = cacheData_->Get(&key, sizeof(key), shaArray.data(), shaArray.size());
+    auto [errorCode, loaded] = cacheData_->Get(&key, sizeof(key), shaArray.data(), shaArray.size());
     if (!(loaded && std::equal(shaArray.begin(), shaArray.end(), idHash_.begin()))) {
         cacheData_->Clear();
         LOGD("abandon, bad hash value, cleared for future regeneration");
@@ -111,29 +113,37 @@ std::shared_ptr<Drawing::Data> ShaderCache::Load(const Drawing::Data& key)
         valueBuffer = nullptr;
         return nullptr;
     }
-
+    CacheData::ErrorCode errorCode = CacheData::ErrorCode::NO_ERROR;
+    size_t valueSize = 0;
+    std::tuple<CacheData::ErrorCode, size_t> res = {errorCode, valueSize};
 #ifndef USE_ROSEN_DRAWING
-    size_t valueSize = cacheData_->Get(key.data(), keySize, valueBuffer, bufferSize_);
+    res = cacheData_->Get(key.data(), keySize, valueBuffer, bufferSize_);
 #else
-    size_t valueSize = cacheData_->Get(key.GetData(), keySize, valueBuffer, bufferSize_);
+    res = cacheData_->Get(key.GetData(), keySize, valueBuffer, bufferSize_);
 #endif
-    if (!valueSize) {
+    errorCode = std::get<0>(res);
+    valueSize = std::get<1>(res);
+    if ((errorCode != CacheData::ErrorCode::NO_ERROR) && (valueSize > bufferSize_)) {
         free(valueBuffer);
         valueBuffer = nullptr;
-        void* newValueBuffer = realloc(valueBuffer, MAX_VALUE_SIZE);
+        void* newValueBuffer = realloc(valueBuffer, valueSize);
         if (!newValueBuffer) {
-            LOGE("load: failed to reallocate maxValueSize");
+            LOGE("load: failed to reallocate valueSize:%zu", valueSize);
             return nullptr;
         }
         valueBuffer = newValueBuffer;
+        // Get key data with updated valueSize
 #ifndef USE_ROSEN_DRAWING
-        valueSize = cacheData_->Get(key.data(), keySize, valueBuffer, bufferSize_);
+        res = cacheData_->Get(key.data(), keySize, valueBuffer, valueSize);
 #else
-        valueSize = cacheData_->Get(key.GetData(), keySize, valueBuffer, bufferSize_);
+        res = cacheData_->Get(key.GetData(), keySize, valueBuffer, valueSize);
 #endif
+        // update res after realloc and Get key
+        errorCode = std::get<0>(res);
+        valueSize = std::get<1>(res);
     }
 
-    if (!valueSize || valueSize > bufferSize_) {
+    if (errorCode != CacheData::ErrorCode::NO_ERROR) {
         LOGD("load: failed to get the cache value with the given key");
         free(valueBuffer);
         valueBuffer = nullptr;

git-break/graphic_graphic_2d

rosen/modules/2d_graphics/src/render_context/cache_data.cpp
@@ -184,30 +184,31 @@ void CacheData::Rewrite(const void *key, const size_t keySize, const void *value
     cleanThreshold_ = 0;
 }
 
-size_t CacheData::Get(const void *key, const size_t keySize, void *value, const size_t valueSize)
+std::tuple<CacheData::ErrorCode, size_t> CacheData::Get(const void *key, const size_t keySize,
+    void *value, const size_t valueSize)
 {
     if (maxKeySize_ < keySize) {
         LOGE("abandon, because the key is too large");
-        return 0;
+        return {ErrorCode::VALUE_SIZE_OVER_MAX_SIZE, 0};
     }
     std::shared_ptr<DataPointer> fakeDataPointer(std::make_shared<DataPointer>(key, keySize, false));
     ShaderPointer fakeShaderPointer(fakeDataPointer, nullptr);
     auto index = std::lower_bound(shaderPointers_.begin(), shaderPointers_.end(), fakeShaderPointer);
     if (index == shaderPointers_.end() || fakeShaderPointer < *index) {
         LOGD("abandon, because no key is found");
-        return 0;
+        return {ErrorCode::KEY_NOT_FOUND, 0};
     }
     std::shared_ptr <DataPointer> valuePointer(index->GetValuePointer());
     size_t valuePointerSize = valuePointer->GetSize();
     if (valuePointerSize > valueSize) {
         LOGE("abandon, because of insufficient buffer space");
-        return 0;
+        return {ErrorCode::VALUE_SIZE_TOO_SAMLL, valuePointerSize};
     }
     if (memcpy_s(value, valueSize, valuePointer->GetData(), valuePointerSize)) {
         LOGE("abandon, failed to copy content");
-        return 0;
+        return {ErrorCode::COPY_FAILED, 0};
     }
-    return valuePointerSize;
+    return {ErrorCode::NO_ERR, valuePointerSize};
 }
 
 size_t CacheData::SerializedSize() const

rosen/modules/2d_graphics/src/render_context/shader_cache.cpp
@@ -17,9 +17,11 @@
 
 #include <algorithm>
 #include <array>
+#include <cstddef>
 #include <openssl/sha.h>
 #include <random>
 #include <thread>
+#include <tuple>
 #include "rs_trace.h"
 #include "utils/log.h"
 
@@ -61,7 +63,7 @@ void ShaderCache::InitShaderCache(const char* identity, const size_t size, bool 
     std::array<uint8_t, SHA256_DIGEST_LENGTH> shaArray;
     auto key = ID_KEY;
 
-    auto loaded = cacheData_->Get(&key, sizeof(key), shaArray.data(), shaArray.size());
+    auto [errorCode, loaded] = cacheData_->Get(&key, sizeof(key), shaArray.data(), shaArray.size());
     if (!(loaded && std::equal(shaArray.begin(), shaArray.end(), idHash_.begin()))) {
         cacheData_->Clear();
         LOGD("abandon, bad hash value, cleared for future regeneration");
@@ -111,29 +113,36 @@ std::shared_ptr<Drawing::Data> ShaderCache::Load(const Drawing::Data& key)
         valueBuffer = nullptr;
         return nullptr;
     }
-
+    CacheData::ErrorCode errorCode = CacheData::ErrorCode::NO_ERR;
+    size_t valueSize = 0;
+    std::tuple<CacheData::ErrorCode, size_t> res = {errorCode, valueSize};
 #ifndef USE_ROSEN_DRAWING
-    size_t valueSize = cacheData_->Get(key.data(), keySize, valueBuffer, bufferSize_);
+    res = cacheData_->Get(key.data(), keySize, valueBuffer, bufferSize_);
 #else
-    size_t valueSize = cacheData_->Get(key.GetData(), keySize, valueBuffer, bufferSize_);
+    res = cacheData_->Get(key.GetData(), keySize, valueBuffer, bufferSize_);
 #endif
-    if (!valueSize) {
+    errorCode = std::get<0>(res);
+    valueSize = std::get<1>(res);
+    if (errorCode == CacheData::ErrorCode::VALUE_SIZE_TOO_SAMLL) {
         free(valueBuffer);
         valueBuffer = nullptr;
-        void* newValueBuffer = realloc(valueBuffer, MAX_VALUE_SIZE);
+        void* newValueBuffer = realloc(valueBuffer, valueSize);
         if (!newValueBuffer) {
-            LOGE("load: failed to reallocate maxValueSize");
+            LOGE("load: failed to reallocate valueSize:%zu", valueSize);
             return nullptr;
         }
         valueBuffer = newValueBuffer;
+        // Get key data with updated valueSize
 #ifndef USE_ROSEN_DRAWING
-        valueSize = cacheData_->Get(key.data(), keySize, valueBuffer, bufferSize_);
+        res = cacheData_->Get(key.data(), keySize, valueBuffer, valueSize);
 #else
-        valueSize = cacheData_->Get(key.GetData(), keySize, valueBuffer, bufferSize_);
+        res = cacheData_->Get(key.GetData(), keySize, valueBuffer, valueSize);
 #endif
+        // update res after realloc and Get key
+        errorCode = std::get<0>(res);
     }
 
-    if (!valueSize || valueSize > bufferSize_) {
+    if (errorCode != CacheData::ErrorCode::NO_ERR) {
         LOGD("load: failed to get the cache value with the given key");
         free(valueBuffer);
         valueBuffer = nullptr;