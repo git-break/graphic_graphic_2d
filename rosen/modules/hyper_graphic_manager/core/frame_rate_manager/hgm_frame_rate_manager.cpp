40b995c87d6857aeb7091b23e594280f645e835e
zzvscx <zhangsongwei2@huawei.com>
2024-01-19 09:51:28 +0800

    idleFps_改为就高
    Signed-off-by: zzvscx <zhangsongwei2@huawei.com>
    Change-Id: Idaf19377664e31950ca74d787d3a36e75e43fc74

--- rosen/modules/hyper_graphic_manager/core/frame_rate_manager/hgm_frame_rate_manager.cpp
+++ rosen/modules/hyper_graphic_manager/core/frame_rate_manager/hgm_frame_rate_manager.cpp
@@ -633,6 +633,7 @@ void HgmFrameRateManager::SyncAppVote()
 
     isTouchEnable_ = (configData->strategyConfigs_[curXmlStrategy].dynamicMode != 0);
     touchFps_ = configData->strategyConfigs_[curXmlStrategy].max;
+    idleFps_ = std::max(configData->strategyConfigs_[curXmlStrategy].min, static_cast<int32_t>(OLED_60_HZ));
 }
 
 void HgmFrameRateManager::MarkVoteChange()
