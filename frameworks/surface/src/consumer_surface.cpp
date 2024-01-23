6ca57a0cfb81ad88c4fff8d644d3734c19c1bac9
m00472246 <majingtao1@huawei.com>
2024-01-23 14:52:13 +0800

    修改回退 Signed-off-by: m00472246 <majingtao1@huawei.com>
    Signed-off-by: m00472246 <majingtao1@huawei.com>

--- frameworks/surface/src/consumer_surface.cpp
+++ frameworks/surface/src/consumer_surface.cpp
@@ -111,7 +111,7 @@ GSError ConsumerSurface::FlushBuffer(sptr<SurfaceBuffer>& buffer, const sptr<Syn
 }
 
 GSError ConsumerSurface::GetLastFlushedBuffer(sptr<SurfaceBuffer>& buffer,
-    sptr<SyncFence>& fence, float matrix[16], int32_t matrixSize)
+    sptr<SyncFence>& fence, float matrix[16])
 {
     return GSERROR_NOT_SUPPORT;
 }
