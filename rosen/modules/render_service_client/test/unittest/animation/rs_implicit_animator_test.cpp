a9d052faee17e3d6315713211068d49d8156f6fd
yanghf <yanghefei1@huawei.com>
2024-01-26 17:04:16 +0800

    add ut
    Signed-off-by: yanghf <yanghefei1@huawei.com>
    Change-Id: I1758f789431549e9fef5d081bbe49f5f7f1453c0

--- rosen/modules/render_service_client/test/unittest/animation/rs_implicit_animator_test.cpp
+++ rosen/modules/render_service_client/test/unittest/animation/rs_implicit_animator_test.cpp
@@ -54,5 +54,58 @@ HWTEST_F(RSImplicitAnimatorTest, GetBoundsTest, TestSize.Level1)
     EXPECT_TRUE(implicitAnimator != nullptr);
     GTEST_LOG_(INFO) << "RSImplicitAnimatorTest OpenImplicitAnimation001 end";
 }
+
+/**
+ * @tc.name: BeginImplicitDurationKeyFrameAnimation001
+ * @tc.desc: Verify the BeginImplicitDurationKeyFrameAnimation
+ * @tc.type:FUNC
+ */
+HWTEST_F(RSImplicitAnimatorTest, BeginImplicitDurationKeyFrameAnimation001, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "RSImplicitAnimatorTest BeginImplicitDurationKeyFrameAnimation001 start";
+    int duration = 1000;
+    RSAnimationTimingProtocol timingProtocol;
+    float mass = 0.5f;
+    float stiffness = 0.0f;
+    float damping = 0.5f;
+    float velocity = 1.0f;
+    auto timingCurve = RSAnimationTimingCurve::CreateSpringCurve(velocity, mass, stiffness, damping);
+    auto implicitAnimator = std::make_shared<RSImplicitAnimator>();
+    implicitAnimator->BeginImplicitDurationKeyFrameAnimation(duration, timingCurve);
+
+    implicitAnimator->OpenImplicitAnimation(RSAnimationTimingProtocol::DEFAULT, timingCurve);
+    implicitAnimator->BeginImplicitDurationKeyFrameAnimation(duration, timingCurve);
+
+    RSAnimationTimingProtocol timingProtocol1(0);
+    auto timingCurve1 = RSAnimationTimingCurve::DEFAULT;
+    implicitAnimator->OpenImplicitAnimation(timingProtocol1, timingCurve1);
+    implicitAnimator->BeginImplicitDurationKeyFrameAnimation(duration, timingCurve1);
+
+    implicitAnimator->OpenImplicitAnimation(timingProtocol, timingCurve1);
+    implicitAnimator->BeginImplicitDurationKeyFrameAnimation(duration, timingCurve1);
+    EXPECT_TRUE(implicitAnimator != nullptr);
+    GTEST_LOG_(INFO) << "RSImplicitAnimatorTest BeginImplicitDurationKeyFrameAnimation001 end";
+}
+
+/**
+ * @tc.name: EndImplicitDurationKeyFrameAnimation001
+ * @tc.desc: Verify the EndImplicitDurationKeyFrameAnimation
+ * @tc.type:FUNC
+ */
+HWTEST_F(RSImplicitAnimatorTest, EndImplicitDurationKeyFrameAnimation001, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "RSImplicitAnimatorTest EndImplicitDurationKeyFrameAnimation001 start";
+    int duration = 1000;
+    auto implicitAnimator = std::make_shared<RSImplicitAnimator>();
+    implicitAnimator->EndImplicitDurationKeyFrameAnimation();
+
+    RSAnimationTimingProtocol timingProtocol;
+    auto timingCurve = RSAnimationTimingCurve::DEFAULT;
+    implicitAnimator->OpenImplicitAnimation(timingProtocol, timingCurve);
+    implicitAnimator->BeginImplicitDurationKeyFrameAnimation(duration, timingCurve);
+    implicitAnimator->EndImplicitDurationKeyFrameAnimation();
+    EXPECT_TRUE(implicitAnimator != nullptr);
+    GTEST_LOG_(INFO) << "RSImplicitAnimatorTest EndImplicitDurationKeyFrameAnimation001 end";
+}
 } // namespace Rosen
 } // namespace OHOS
