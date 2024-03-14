ffa5a9f31f69e35128831ab30e072be3c58760ae
王凯 <wangkai493@huawei.com>
2024-03-14 03:40:22 +0000

    update frameworks/opengl_wrapper/src/EGL/egl_wrapper_display.cpp.
    Signed-off-by: 王凯 <wangkai493@huawei.com>

--- frameworks/opengl_wrapper/src/EGL/egl_wrapper_display.cpp
+++ frameworks/opengl_wrapper/src/EGL/egl_wrapper_display.cpp
@@ -637,10 +637,6 @@ EGLBoolean EglWrapperDisplay::SwapBuffers(EGLSurface surf)
     WLOGD("");
     std::lock_guard<std::mutex> lock(refLockMutex_);
 
-    if(disp_ == EGL_NO_DISPLAY) {
-        WLOGE("display is null");
-        return EGL_FALSE;
-    }
     EglWrapperSurface *surfPtr = EglWrapperSurface::GetWrapperSurface(surf);
     if (!CheckObject(surfPtr)) {
         if (surfPtr->GetEglSurface() == nullptr) {
