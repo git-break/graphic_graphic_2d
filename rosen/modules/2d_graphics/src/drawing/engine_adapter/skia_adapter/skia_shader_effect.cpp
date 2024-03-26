b57ddb6b25b10c04af86452f829ebf69994c5164
l00844999 <lizhan12@huawei.com>
2024-03-26 10:52:58 +0800

    [drawing] add colorspace and shader adapter
    Signed-off-by: l00844999 <lizhan12@huawei.com>

--- rosen/modules/2d_graphics/src/drawing/engine_adapter/skia_adapter/skia_shader_effect.cpp
+++ rosen/modules/2d_graphics/src/drawing/engine_adapter/skia_adapter/skia_shader_effect.cpp
@@ -50,6 +50,12 @@ void SkiaShaderEffect::InitWithColor(ColorQuad color)
     shader_ = SkShaders::Color(color);
 }
 
+void SkiaShaderEffect::InitWithColorSpace(const Color4f& color, std::shared_ptr<ColorSpace> colorSpace)
+{
+    const SkColor4f& skC4f = { .fR = color.redF_, .fG = color.greenF_, .fB = color.blueF_, .fA = color.alphaF_ };
+    shader_ = SkShaders::Color(skC4f, colorSpace->GetSkColorSpace());
+}
+
 void SkiaShaderEffect::InitWithBlend(const ShaderEffect& s1, const ShaderEffect& s2, BlendMode mode)
 {
     auto dst = s1.GetImpl<SkiaShaderEffect>();
