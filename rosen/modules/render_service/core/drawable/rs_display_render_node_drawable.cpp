119248f09201e30ee87d418c8a60f138437b3fce
sf2020 <yaojian7@huawei.com>
2024-03-26 19:06:55 +0800

    [DFX] trace字段关键字适配性能自动化工程

--- rosen/modules/render_service/core/drawable/rs_display_render_node_drawable.cpp
+++ rosen/modules/render_service/core/drawable/rs_display_render_node_drawable.cpp
@@ -84,7 +84,7 @@ std::unique_ptr<RSRenderFrame> RSDisplayRenderNodeDrawable::RequestFrame(
     std::shared_ptr<RSDisplayRenderNode> displayNodeSp, RSDisplayRenderParams& params,
     std::shared_ptr<RSProcessor> processor) const
 {
-    RS_TRACE_NAME("RSDisplayRenderNodeDrawable::RequestFrame");
+    RS_TRACE_NAME("RSDisplayRenderNodeDrawable:RequestFrame");
     auto renderEngine = RSUniRenderThread::Instance().GetRenderEngine();
     if (renderEngine == nullptr) {
         RS_LOGE("RSDisplayRenderNodeDrawable::RequestFrame RenderEngine is null!");
