17fca40776c9f7e3a35ede4fc4c386fcfe9e6239
zhouhuan31@huawei.com <zhouhuan31@huawei.com>
2024-04-08 20:18:41 +0800

    surface selfDirtyRegion delay fix
    Signed-off-by: zhouhuan31@huawei.com <zhouhuan31@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_uni_render_visitor.cpp
+++ rosen/modules/render_service/core/pipeline/rs_uni_render_visitor.cpp
@@ -1204,10 +1204,10 @@ void RSUniRenderVisitor::QuickPrepareDisplayRenderNode(RSDisplayRenderNode& node
 
 void RSUniRenderVisitor::QuickPrepareSurfaceRenderNode(RSSurfaceRenderNode& node)
 {
-    RS_OPTIONAL_TRACE_NAME_FMT("RSUniRender::QuickPrepareSurfaceRenderNode: "
-        "node[%llu] name[%s] pid[%d] nodeType[%d] "
-        "subTreeDirty[%d]", node.GetId(), node.GetName().c_str(), ExtractPid(node.GetId()),
-        static_cast<int>(node.GetSurfaceNodeType()), node.IsSubTreeDirty());
+    RS_OPTIONAL_TRACE_NAME("RSUniRender::QuickPrepare:[" + node.GetName() + "] nodeId[" +
+        std::to_string(node.GetId()) + "] pid[" + std::to_string(ExtractPid(node.GetId())) +
+        "] nodeType[" + std::to_string(static_cast<uint>(node.GetSurfaceNodeType())) +
+        "] subTreeDirty[" + std::to_string(node.IsSubTreeDirty()) + "]");
     RS_LOGD("RSUniRender::QuickPrepareSurfaceRenderNode:[%{public}s] nodeid:[%{public}" PRIu64 "]"
         "pid:[%{public}d] nodeType:[%{public}d] subTreeDirty[%{public}d]",
         node.GetName().c_str(), node.GetId(), ExtractPid(node.GetId()),
@@ -2103,6 +2103,7 @@ void RSUniRenderVisitor::PostPrepare(RSRenderNode& node, bool subTreeSkipped)
     }
     node.MapAndUpdateChildrenRect();
     node.UpdateLocalDrawRect();
+    node.ResetClipAbsDrawRectChangeState();
     if (isDrawingCacheEnabled_) {
         node.UpdateDrawingCacheInfoAfterChildren();
     }
