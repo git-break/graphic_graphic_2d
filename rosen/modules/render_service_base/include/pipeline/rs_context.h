a36f3d5868b862b863bd452bcddeb8625c54f23a
vyalkovdmitry <vyalkov.dmitry@huawei-partners.com>
2024-04-25 14:26:40 +0000

    update rosen/modules/render_service_base/include/pipeline/rs_context.h.
    Signed-off-by: vyalkovdmitry <vyalkov.dmitry@huawei-partners.com>

--- rosen/modules/render_service_base/include/pipeline/rs_context.h
+++ rosen/modules/render_service_base/include/pipeline/rs_context.h
@@ -153,9 +153,6 @@ private:
 
     friend class RSRenderThread;
     friend class RSMainThread;
-#ifdef RS_PROFILER_ENABLED
-    friend class RSProfiler;
-#endif
     friend class RSDrawFrame;
 #ifdef RS_PROFILER_ENABLED
     friend class RSProfiler;
