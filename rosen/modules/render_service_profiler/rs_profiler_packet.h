907f591376ea8b87de2a7e07427a29a000b13bcf
vyalkovdmitry <vyalkov.dmitry@huawei-partners.com>
2024-06-03 03:10:42 +0000

    update rosen/modules/render_service_profiler/rs_profiler_packet.h.
    Signed-off-by: vyalkovdmitry <vyalkov.dmitry@huawei-partners.com>

--- rosen/modules/render_service_profiler/rs_profiler_packet.h
+++ rosen/modules/render_service_profiler/rs_profiler_packet.h
@@ -176,6 +176,9 @@ template<typename T>
 
 [[maybe_unused]] inline bool Packet::Read(void* value, size_t size)
 {
+    if (!size) {
+        return false;
+    }
     if (readPointer_ + size > data_.size()) {
         return false;
     }
@@ -221,7 +224,11 @@ template<typename T>
 
 [[maybe_unused]] inline bool Packet::Write(const void* value, size_t size)
 {
-    size_t growSize = size - (data_.size() - writePointer_);
+    if (!size) {
+        return false;
+    }
+
+    const size_t growSize = size - (data_.size() - writePointer_);
     if (writePointer_ + size > data_.size()) {
         data_.resize(data_.size() + growSize);
     }
@@ -233,7 +240,6 @@ template<typename T>
     SetLength(data_.size());
     return true;
 }
-
 
 } // namespace OHOS::Rosen
 
