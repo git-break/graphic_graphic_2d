3eaabc132f697bbc15991c143dee42b8f1ed3283
haoxing <xinghao9@huawei.com>
2024-06-05 20:33:52 +0800

    Add Protection for RSSubThread::ThreadSafetyReleaseTexture~
    Change-Id: I1904145b8918ececfe47b54db7bbd254f8d4ca2d
    Signed-off-by: haoxing <xinghao9@huawei.com>

--- rosen/modules/render_service/core/pipeline/parallel_render/rs_sub_thread.cpp
+++ rosen/modules/render_service/core/pipeline/parallel_render/rs_sub_thread.cpp
@@ -363,6 +363,9 @@ void RSSubThread::ResetGrContext()
 
 void RSSubThread::ThreadSafetyReleaseTexture()
 {
+    if (grContext_ == nullptr) {
+        return;
+    }
     grContext_->FreeGpuResources();
 }
 
