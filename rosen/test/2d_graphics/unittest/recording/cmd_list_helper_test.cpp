517e4869383c7c89eaf94e4caae2d8a0a7b510e8
ZhouJie <zhoujie241@huawei.com>
2024-06-06 19:10:53 +0800

    折叠屏合一桌面更换壁纸旋转后时间乱码
    Signed-off-by: ZhouJie <zhoujie241@huawei.com>

--- rosen/test/2d_graphics/unittest/recording/cmd_list_helper_test.cpp
+++ rosen/test/2d_graphics/unittest/recording/cmd_list_helper_test.cpp
@@ -548,7 +548,7 @@ HWTEST_F(CmdListHelperTest, TextBlob, TestSize.Level1)
 {
     auto cmdList = DrawCmdList::CreateFromData({ nullptr, 0 }, false);
     OpDataHandle handle = CmdListHelper::AddTextBlobToCmdList(*cmdList, nullptr);
-    auto sptr = CmdListHelper::GetTextBlobFromCmdList(*cmdList, handle);
+    auto sptr = CmdListHelper::GetTextBlobFromCmdList(*cmdList, handle, 0);
     EXPECT_EQ(sptr, nullptr);
 }
 
@@ -563,7 +563,7 @@ HWTEST_F(CmdListHelperTest, Region, TestSize.Level1)
     auto cmdList = DrawCmdList::CreateFromData({ nullptr, 0 }, false);
     Region region;
     OpDataHandle handle = CmdListHelper::AddRegionToCmdList(*cmdList, region);
-    auto sptr = CmdListHelper::GetRegionFromCmdList(*cmdList, handle, 0);
+    auto sptr = CmdListHelper::GetRegionFromCmdList(*cmdList, handle);
     EXPECT_NE(sptr, nullptr);
 }
 
