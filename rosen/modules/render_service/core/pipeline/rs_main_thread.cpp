48a1b76b0640de85aeb9e1f71f52cb9015a4d9c2
weiyuantian <weiyuantian1@huawei.com>
2024-07-25 16:43:14 +0800

    标志位清零
    Signed-off-by: weiyuantian <weiyuantian1@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
+++ rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
@@ -171,7 +171,7 @@ constexpr const char* DESKTOP_NAME_FOR_ROTATION = "SCBDesktop";
 const std::string PERF_FOR_BLUR_IF_NEEDED_TASK_NAME = "PerfForBlurIfNeeded";
 constexpr const char* CAPTURE_WINDOW_NAME = "CapsuleWindow";
 constexpr const char* HIDE_NOTCH_STATUS = "persist.sys.graphic.hideNotch.status";
-constexpr const char* TEMPORARY_NAME = "temporaryName";
+constexpr const char* TEMPORARY_NAME = "TemporaryName";
 #ifdef RS_ENABLE_GL
 constexpr size_t DEFAULT_SKIA_CACHE_SIZE        = 96 * (1 << 20);
 constexpr int DEFAULT_SKIA_CACHE_COUNT          = 2 * (1 << 12);
