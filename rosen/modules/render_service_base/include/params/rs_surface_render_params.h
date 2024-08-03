571d9f35ebbcb04b5ffc97defb93884c036b1092
Klaus_q <wangqi175@huawei.com>
2024-08-03 17:47:44 +0800

    bugfix for DRM video rotation
    Signed-off-by: Klaus_q <wangqi175@huawei.com>
    Change-Id: I046dc5b4a83ad42fc54d563bbe6fa72c118552b5

--- rosen/modules/render_service_base/include/params/rs_surface_render_params.h
+++ rosen/modules/render_service_base/include/params/rs_surface_render_params.h
@@ -139,6 +139,10 @@ public:
     {
         return animateState_;
     }
+    bool GetForceClientForDRMOnly() const
+    {
+        return forceClientForDRMOnly_;
+    }
     const std::set<NodeId>& GetSecurityLayerIds() const
     {
         return securityLayerIds_;
@@ -466,6 +470,7 @@ private:
     bool isSkipLayer_ = false;
     bool isProtectedLayer_ = false;
     bool animateState_ = false;
+    bool forceClientForDRMOnly_ = false;
     bool isSubSurfaceNode_ = false;
     Gravity uiFirstFrameGravity_ = Gravity::TOP_LEFT;
     bool isNodeToBeCaptured_ = false;
