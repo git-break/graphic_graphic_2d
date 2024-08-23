dd04bf2cd2fc255f64bf0c4b8ed205c4b71d87fb
ZhouJie <zhoujie241@huawei.com>
2024-08-23 16:02:00 +0800

    修改锁
    Signed-off-by: ZhouJie <zhoujie241@huawei.com>

--- rosen/modules/2d_engine/rosen_text/adapter/skia_txt/typography.cpp
+++ rosen/modules/2d_engine/rosen_text/adapter/skia_txt/typography.cpp
@@ -152,7 +152,7 @@ double Typography::GetGlyphsBoundsRight()
 
 Drawing::FontMetrics Typography::MeasureText()
 {
-    std::shared_lock<std::shared_mutex> readLock(mutex_);
+    std::shared_lock<std::shared_mutex> writeLock(mutex_);
     return paragraph_->MeasureText();
 }
 
