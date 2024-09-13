89bdce49197e4e608127722a7bd7f1102a7d7fed
linghao-ji <jilinghao@huawei.com>
2024-09-13 11:57:45 +0800

    修复web遮挡小概率crash
    Signed-off-by: linghao-ji <jilinghao@huawei.com>
    Change-Id: I90d674d6b864fcfeb46e8dbc8dec2cddffdccba2

--- rosen/modules/render_service/core/pipeline/rs_main_thread.h
+++ rosen/modules/render_service/core/pipeline/rs_main_thread.h
@@ -284,6 +284,7 @@ public:
 
     void SurfaceOcclusionChangeCallback(VisibleData& dstCurVisVec);
     void SurfaceOcclusionCallback();
+    bool CheckSurfaceOcclusionNeedProcess(NodeId id);
     void SubscribeAppState();
     void HandleOnTrim(Memory::SystemMemoryLevel level);
     void SetCurtainScreenUsingStatus(bool isCurtainScreenOn);
