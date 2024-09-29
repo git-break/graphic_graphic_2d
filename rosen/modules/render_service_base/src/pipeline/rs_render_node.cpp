f521abff4b32e3441c41a4563112d04b72fca094
hid911900 <zhangzhichao38@huawei.com>
2024-09-29 11:52:37 +0800

    子系统中map使用的count()改为find()
    Signed-off-by: hid911900 <zhangzhichao38@huawei.com>

--- rosen/modules/render_service_base/src/pipeline/rs_render_node.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_render_node.cpp
@@ -192,9 +192,9 @@ bool RSRenderNode::IsPureContainer() const
 bool RSRenderNode::IsContentNode() const
 {
     auto& drawCmdModifiers_ = renderContent_->drawCmdModifiers_;
-    return ((drawCmdModifiers_.size() == 1 && 
+    return ((drawCmdModifiers_.size() == 1 &&
                 drawCmdModifiers_.find(RSModifierType::CONTENT_STYLE) != drawCmdModifiers_.end()) ||
-                drawCmdModifiers_.empty()) &&
+               drawCmdModifiers_.empty()) &&
            !GetRenderProperties().isDrawn_;
 }
 
@@ -1358,7 +1358,8 @@ const RectI& RSRenderNode::GetAbsDrawRect() const
 
 bool RSRenderNode::CheckAndUpdateGeoTrans(std::shared_ptr<RSObjAbsGeometry>& geoPtr)
 {
-    if (renderContent_->drawCmdModifiers_.find(RSModifierType::GEOMETRYTRANS) == renderContent_->drawCmdModifiers_.end()) {
+    if (renderContent_->drawCmdModifiers_.find(RSModifierType::GEOMETRYTRANS) ==
+        renderContent_->drawCmdModifiers_.end()) {
         return false;
     }
     RSModifierContext context = { GetMutableRenderProperties() };
@@ -1535,10 +1536,9 @@ bool RSRenderNode::Update(RSDirtyRegionManager& dirtyManager, const std::shared_
     parentDirty = parentDirty || (dirtyStatus_ != NodeDirty::CLEAN);
     auto parentProperties = parent ? &parent->GetRenderProperties() : nullptr;
     bool dirty = GetMutableRenderProperties().UpdateGeometry(parentProperties, parentDirty, offset);
-    if ((IsDirty() || dirty) && 
-        renderContent_->drawCmdModifiers_.find(RSModifierType::GEOMETRYTRANS) != renderContent_->drawCmdModifiers_.end()) {
-        RSModifierContext context = { GetMutableRenderProperties() 
-        };
+    if ((IsDirty() || dirty) && renderContent_->drawCmdModifiers_.find(RSModifierType::GEOMETRYTRANS) !=
+                                    renderContent_->drawCmdModifiers_.end()) {
+        RSModifierContext context = { GetMutableRenderProperties() };
         for (auto& modifier : renderContent_->drawCmdModifiers_[RSModifierType::GEOMETRYTRANS]) {
             modifier->Apply(context);
         }
