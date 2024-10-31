06284e60c316e594242dffc12190def51d874ee3
liugongding <liugongding@h-partners.com>
2024-10-31 16:57:22 +0800

    优化代码
    Signed-off-by: liugongding <liugongding@h-partners.com>

--- rosen/modules/render_service/core/system/rs_system_parameters.cpp
+++ rosen/modules/render_service/core/system/rs_system_parameters.cpp
@@ -195,12 +195,12 @@ bool RSSystemParameters::GetTcacheEnabled()
     return flag;
 }
 
-int RSSystemParameters::GetDumpCanvasDrawingNodeEnabled()
+bool RSSystemParameters::GetDumpCanvasDrawingNodeEnabled()
 {
     static CachedHandle g_Handle = CachedParameterCreate("debug.graphic.canvasDrawingEnabled", "0");
     int changed = 0;
     const char *enable = CachedParameterGetChanged(g_Handle, &changed);
-    return ConvertToInt(enable, 0) != 0;
+    return ConvertToInt(enable, 0) == 0;
 }
 
 bool RSSystemParameters::IsNeedScRGBForP3(const GraphicColorGamut& currentGamut)
