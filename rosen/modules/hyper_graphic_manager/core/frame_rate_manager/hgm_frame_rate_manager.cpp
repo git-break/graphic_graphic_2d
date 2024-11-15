c5f33e25085c09c60fb587ead34d3f032ea47786
hsc <husichao1@huawei.com>
2024-11-15 17:08:55 +0800

    request vsync when app framerate change
    Signed-off-by: hsc <husichao1@huawei.com>

--- rosen/modules/hyper_graphic_manager/core/frame_rate_manager/hgm_frame_rate_manager.cpp
+++ rosen/modules/hyper_graphic_manager/core/frame_rate_manager/hgm_frame_rate_manager.cpp
@@ -577,10 +577,8 @@ void HgmFrameRateManager::HandleFrameRateChangeForLTPO(uint64_t timestamp, bool 
     }
     isNeedUpdateAppOffset_ = false;
     pendingRefreshRate_ = std::make_shared<uint32_t>(currRefreshRate_);
-    if (currRefreshRate_ != lastRefreshRate) {
-        if (forceUpdateCallback_ != nullptr) {
-            forceUpdateCallback_(false, true);
-        }
+    if (forceUpdateCallback_ != nullptr) {
+        forceUpdateCallback_(false, true);
     }
     changeGeneratorRateValid_.store(false);
 }
