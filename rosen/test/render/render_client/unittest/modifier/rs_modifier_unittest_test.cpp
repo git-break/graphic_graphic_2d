461f7953ab755e941e05411193bca7f93fde8c4d
王德海 <wangdehai2@huawei.com>
2024-11-27 15:49:46 +0800

    fix tdd
    Signed-off-by: 王德海 <wangdehai2@huawei.com>
    Change-Id: I5a3cd79b1b9d3a95c7cf8e6c1dc3eb1f0f8e0966

--- rosen/test/render/render_client/unittest/modifier/rs_modifier_unittest_test.cpp
+++ rosen/test/render/render_client/unittest/modifier/rs_modifier_unittest_test.cpp
@@ -61,4 +61,249 @@ HWTEST_F(RsModifiersTest, AddModifier01, TestSize.Level1)
     ASSERT_TRUE(node != nullptr);
 }
 
+/**
+ * @tc.name: AddModifier02
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RsModifiersTest, AddModifier02, TestSize.Level1)
+{
+    auto prop = std::make_shared<RSAnimatableProperty<float>>(floatData[0]);
+    auto modifier = std::make_shared<RSAlphaModifier>(prop);
+
+    auto node = RSCanvasNode::Create();
+    node->AddModifier(modifier);
+    ASSERT_TRUE(node != nullptr);
+    ASSERT_EQ(node->GetStagingProperties().GetAlpha(), floatData[0]);
+
+    prop->Set(floatData[1]);
+    ASSERT_EQ(node->GetStagingProperties().GetAlpha(), floatData[1]);
+
+    node->RemoveModifier(modifier);
+    auto node1 = RSCanvasNode::Create();
+    ASSERT_EQ(node->GetStagingProperties().GetAlpha(), node1->GetStagingProperties().GetAlpha());
+}
+
+/**
+ * @tc.name: Modifier01
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RsModifiersTest, Modifier01, TestSize.Level1)
+{
+    auto prop = std::make_shared<RSAnimatableProperty<float>>(floatData[0]);
+    auto modifier = std::make_shared<RSAlphaModifier>(prop);
+    ASSERT_TRUE(modifier != nullptr);
+    ASSERT_EQ(modifier->GetPropertyId(), prop->GetId());
+}
+
+
+/**
+ * @tc.name: Modifier02
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RsModifiersTest, Modifier02, TestSize.Level1)
+{
+    auto prop = std::make_shared<RSAnimatableProperty<float>>(floatData[0]);
+    auto modifier = std::make_shared<RSAlphaModifier>(prop);
+    ASSERT_TRUE(modifier != nullptr);
+    ASSERT_EQ(std::static_pointer_cast<RSAnimatableProperty<float>>(modifier->GetProperty())->Get(), floatData[0]);
+
+    prop->Set(floatData[1]);
+    ASSERT_EQ(std::static_pointer_cast<RSAnimatableProperty<float>>(modifier->GetProperty())->Get(), floatData[1]);
+}
+/**
+ * @tc.name: FrameModifier01
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RsModifiersTest, FrameModifier01, TestSize.Level1)
+{
+    auto val = Vector4f(10.f);
+    auto prop = std::make_shared<RSAnimatableProperty<Vector4f>>(val);
+    auto modifier = std::make_shared<RSFrameModifier>(prop);
+
+    auto node = RSCanvasNode::Create();
+    node->AddModifier(modifier);
+    ASSERT_TRUE(node != nullptr);
+    ASSERT_EQ(node->GetStagingProperties().GetFrame(), val);
+
+    node->RemoveModifier(modifier);
+    auto node1 = RSCanvasNode::Create();
+    ASSERT_EQ(node->GetStagingProperties().GetFrame(), node1->GetStagingProperties().GetFrame());
+}
+
+/**
+ * @tc.name: FrameModifier02
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RsModifiersTest, FrameModifier02, TestSize.Level1)
+{
+    auto val = Vector4f();
+    auto prop = std::make_shared<RSAnimatableProperty<Vector4f>>(val);
+    auto modifier = std::make_shared<RSFrameModifier>(prop);
+
+    auto node = RSCanvasNode::Create();
+    node->AddModifier(modifier);
+    ASSERT_TRUE(node != nullptr);
+    ASSERT_EQ(node->GetStagingProperties().GetFrame(), val);
+    auto str = node->GetStagingProperties().Dump();
+
+    val = Vector4f(0.f, 0.f, 50.f, 60.f);
+    prop->Set(val);
+    ASSERT_EQ(node->GetStagingProperties().GetFrame(), val);
+    str = node->GetStagingProperties().Dump();
+}
+
+/**
+ * @tc.name: PositionZModifier01
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RsModifiersTest, PositionZModifier01, TestSize.Level1)
+{
+    auto val = 2.f;
+    auto prop = std::make_shared<RSAnimatableProperty<float>>(val);
+    auto modifier = std::make_shared<RSPositionZModifier>(prop);
+
+    auto node = RSCanvasNode::Create();
+    node->AddModifier(modifier);
+    ASSERT_TRUE(node != nullptr);
+    ASSERT_EQ(node->GetStagingProperties().GetPositionZ(), val);
+
+    node->RemoveModifier(modifier);
+    auto node1 = RSCanvasNode::Create();
+    ASSERT_EQ(node->GetStagingProperties().GetPositionZ(), node1->GetStagingProperties().GetPositionZ());
+}
+
+/**
+ * @tc.name: PositionZModifier02
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RsModifiersTest, PositionZModifier02, TestSize.Level1)
+{
+    auto val = 0.f;
+    auto prop = std::make_shared<RSAnimatableProperty<float>>(val);
+    auto modifier = std::make_shared<RSPositionZModifier>(prop);
+
+    auto node = RSCanvasNode::Create();
+    node->AddModifier(modifier);
+    ASSERT_TRUE(node != nullptr);
+    ASSERT_EQ(node->GetStagingProperties().GetPositionZ(), val);
+
+    val = -1.f;
+    prop->Set(val);
+    ASSERT_EQ(node->GetStagingProperties().GetPositionZ(), val);
+}
+
+/**
+ * @tc.name: BoundsModifier01
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RsModifiersTest, BoundsModifier01, TestSize.Level1)
+{
+    auto val = Vector4f(10.f);
+    auto prop = std::make_shared<RSAnimatableProperty<Vector4f>>(val);
+    auto modifier = std::make_shared<RSBoundsModifier>(prop);
+
+    auto node = RSCanvasNode::Create();
+    node->AddModifier(modifier);
+    ASSERT_TRUE(node != nullptr);
+    ASSERT_EQ(node->GetStagingProperties().GetBounds(), val);
+
+    node->RemoveModifier(modifier);
+    auto node1 = RSCanvasNode::Create();
+    ASSERT_EQ(node->GetStagingProperties().GetBounds(), node1->GetStagingProperties().GetBounds());
+}
+
+/**
+ * @tc.name: BoundsModifier02
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RsModifiersTest, BoundsModifier02, TestSize.Level1)
+{
+    auto val = Vector4f();
+    auto prop = std::make_shared<RSAnimatableProperty<Vector4f>>(val);
+    auto modifier = std::make_shared<RSBoundsModifier>(prop);
+
+    auto node = RSCanvasNode::Create();
+    node->AddModifier(modifier);
+    ASSERT_TRUE(node != nullptr);
+    ASSERT_EQ(node->GetStagingProperties().GetBounds(), val);
+    auto str = node->GetStagingProperties().Dump();
+
+    val = Vector4f(0.f, 0.f, 50.f, 60.f);
+    prop->Set(val);
+    ASSERT_EQ(node->GetStagingProperties().GetBounds(), val);
+    str = node->GetStagingProperties().Dump();
+}
+
+/**
+ * @tc.name: PivotModifier01
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RsModifiersTest, PivotModifier01, TestSize.Level1)
+{
+    auto val = Vector2f(1.f, 1.f);
+    auto prop = std::make_shared<RSAnimatableProperty<Vector2f>>(val);
+    auto modifier = std::make_shared<RSPivotModifier>(prop);
+
+    auto node = RSCanvasNode::Create();
+    node->AddModifier(modifier);
+    ASSERT_TRUE(node != nullptr);
+    ASSERT_EQ(node->GetStagingProperties().GetPivot(), val);
+
+    node->RemoveModifier(modifier);
+    auto node1 = RSCanvasNode::Create();
+    ASSERT_EQ(node->GetStagingProperties().GetPivot(), node1->GetStagingProperties().GetPivot());
+}
+
+/**
+ * @tc.name: PivotModifier02
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RsModifiersTest, PivotModifier02, TestSize.Level1)
+{
+    auto val = Vector2f();
+    auto prop = std::make_shared<RSAnimatableProperty<Vector2f>>(val);
+    auto modifier = std::make_shared<RSPivotModifier>(prop);
+
+    auto node = RSCanvasNode::Create();
+    node->AddModifier(modifier);
+    ASSERT_TRUE(node != nullptr);
+    ASSERT_EQ(node->GetStagingProperties().GetPivot(), val);
+
+    val = Vector2f(0.5f, 0.5f);
+    prop->Set(val);
+    ASSERT_EQ(node->GetStagingProperties().GetPivot(), val);
+}
+
+/**
+ * @tc.name: RotationModifier01
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RsModifiersTest, RotationModifier01, TestSize.Level1)
+{
+    auto val = 90.f;
+    auto prop = std::make_shared<RSAnimatableProperty<float>>(val);
+    auto modifier = std::make_shared<RSRotationModifier>(prop);
+
+    auto node = RSCanvasNode::Create();
+    node->AddModifier(modifier);
+    ASSERT_TRUE(node != nullptr);
+    ASSERT_EQ(node->GetStagingProperties().GetRotation(), val);
+
+    node->RemoveModifier(modifier);
+    auto node1 = RSCanvasNode::Create();
+    ASSERT_EQ(node->GetStagingProperties().GetRotation(), node1->GetStagingProperties().GetRotation());
+}
+
 } // namespace OHOS::Rosen
