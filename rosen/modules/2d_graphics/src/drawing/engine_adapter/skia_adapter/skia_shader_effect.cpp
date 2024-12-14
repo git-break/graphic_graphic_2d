8b23cc4d7fda65d254571d7ec430606489039243
lixiaoxiang <lixiaoxiang0608@163.com>
2024-12-14 19:14:30 +0800

    update colorspace
    Signed-off-by: lixiaoxiang <lixiaoxiang0608@163.com>

--- rosen/modules/2d_graphics/src/drawing/engine_adapter/skia_adapter/skia_shader_effect.cpp
+++ rosen/modules/2d_graphics/src/drawing/engine_adapter/skia_adapter/skia_shader_effect.cpp
@@ -52,12 +52,12 @@ void SkiaShaderEffect::InitWithColorSpace(const Color4f& color, std::shared_ptr<
 {
     const SkColor4f& skC4f = { .fR = color.redF_, .fG = color.greenF_, .fB = color.blueF_, .fA = color.alphaF_ };
     if (colorSpace == nullptr){
-        shader_=nullptr;
+        shader_ = nullptr;
         return;
     }
-    auto skColorSpace=colorSpace->GetImpl<SkiaColorSpace>();
+    auto skColorSpace = colorSpace->GetImpl<SkiaColorSpace>();
     if (skColorSpace == nullptr){
-        shader_=nullptr;
+        shader_ = nullptr;
         return;
     }
     shader_ = SkShaders::Color(skC4f, skColorSpace->GetColorSpace());
