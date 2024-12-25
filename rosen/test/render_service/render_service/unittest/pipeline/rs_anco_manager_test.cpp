204b6ccf27226e9879425fbdebf6cc0ece06aa1a
jianghongxi <jianghongxi3@huawei.com>
2024-12-25 13:16:32 +0000

    update rosen/test/render_service/render_service/unittest/pipeline/rs_anco_manager_test.cpp.
    Signed-off-by: jianghongxi <jianghongxi3@huawei.com>

--- rosen/test/render_service/render_service/unittest/pipeline/rs_anco_manager_test.cpp
+++ rosen/test/render_service/render_service/unittest/pipeline/rs_anco_manager_test.cpp
@@ -90,8 +90,8 @@ HWTEST_F(RSAncoManagerTest, SetAncoHebcStatus, TestSize.Level2)
     system::SetParameter("persist.sys.graphic.anco.disableHebc", "1");
     auto ancoManager = RSAncoManager::Instance();
     ASSERT_NE(ancoManager, nullptr);
-    ancoManager->SetAncoHebcStatus(AncoHebcStatus::INITAL);
-    ASSERT_EQ(ancoManager->GetAncoHebcStatus(), AncoHebcStatus::INITAL);
+    ancoManager->SetAncoHebcStatus(AncoHebcStatus::INITIAL);
+    ASSERT_EQ(ancoManager->GetAncoHebcStatus(), AncoHebcStatus::INITIAL);
     system::SetParameter("persist.sys.graphic.anco.disableHebc", hebc);
 }
 
