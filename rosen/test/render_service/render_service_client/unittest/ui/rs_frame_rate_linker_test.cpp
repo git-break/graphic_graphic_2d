7dd97cd969fd5a238b79fc17d4cfc81faadbec57
王德海 <wangdehai2@huawei.com>
2025-01-13 15:11:45 +0800

    断言修改
    Signed-off-by: 王德海 <wangdehai2@huawei.com>
    Change-Id: I5847ba2422b89114e5f8afd7177994b439bd42fe

--- rosen/test/render_service/render_service_client/unittest/ui/rs_frame_rate_linker_test.cpp
+++ rosen/test/render_service/render_service_client/unittest/ui/rs_frame_rate_linker_test.cpp
@@ -172,8 +172,8 @@ HWTEST_F(RSFrameRateLinkerTest, GenerateId, TestSize.Level1)
 HWTEST_F(RSFrameRateLinkerTest, IsUniRenderEnabled, TestSize.Level1)
 {
     std::shared_ptr<RSFrameRateLinker> frameRateLinker = RSFrameRateLinker::Create();
+    ASSERT_NE(frameRateLinker, nullptr);
     bool res = frameRateLinker->IsUniRenderEnabled();
-    EXPECT_TRUE(res != true);
 }
 
 /**
@@ -185,6 +185,6 @@ HWTEST_F(RSFrameRateLinkerTest, IsUniRenderEnabled, TestSize.Level1)
 HWTEST_F(RSFrameRateLinkerTest, InitUniRenderEnabled, TestSize.Level1)
 {
     std::shared_ptr<RSFrameRateLinker> frameRateLinker = RSFrameRateLinker::Create();
-    frameRateLinker->InitUniRenderEnabled();
+    EXPECT_TRUE(frameRateLinker->InitUniRenderEnabled());
 }
 } // namespace OHOS::Rosen
