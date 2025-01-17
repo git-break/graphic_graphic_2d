0c3c5d16ff487e71078d8ae26bc1ae7b4397ec16
redermens <zhangpeng516@h-partners.com>
2025-01-17 09:32:21 +0800

    hardwarethread dump info changed
    Signed-off-by: redermens <zhangpeng516@h-partners.com>

--- rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
+++ rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
@@ -2329,9 +2329,8 @@ bool RSMainThread::DoDirectComposition(std::shared_ptr<RSBaseRenderNode> rootNod
 
     if (!RSMainThread::Instance()->WaitHardwareThreadTaskExecute()) {
         RS_LOGW("RSMainThread::DoDirectComposition: hardwareThread task has too many to Execute"
-                " TaskNum:[%{public}d], DumpInfo:%{public}s",
-            RSHardwareThread::Instance().GetunExecuteTaskNum(),
-            RSHardwareThread::Instance().GetEventQueueDump().c_str());
+                " TaskNum:[%{public}d]", RSHardwareThread::Instance().GetunExecuteTaskNum());
+        RSHardwareThread::Instance().DumpEventQueue();
     }
 #ifdef RS_ENABLE_GPU
     bool hasHdrVideo = false;
