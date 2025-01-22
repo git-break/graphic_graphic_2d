42ef3d32db2eb8f9b58265e5f12032b0073cba54
yxu63 <xuyue63@huawei.com>
2025-01-22 10:13:11 +0800

    add trace
    Signed-off-by: yxu63 <xuyue63@huawei.com>
    Change-Id: I14aa97e94e10a2efffdba40e7a80fb2d68379ac1

--- rosen/modules/render_service_client/core/ui/rs_surface_node.cpp
+++ rosen/modules/render_service_client/core/ui/rs_surface_node.cpp
@@ -249,7 +249,8 @@ void RSSurfaceNode::OnBoundsSizeChanged() const
     std::lock_guard<std::mutex> lock(mutex_);
     if (boundsChangedCallback_) {
         boundsChangedCallback_(bounds);
-        RS_TRACE_NAME_FMT("RSSurfaceNode::OnBoundsSizeChanged already callback");
+        RS_TRACE_NAME_FMT("RSSurfaceNode::OnBoundsSizeChanged already callback nodeId:[%{public}" PRIu64 "],"
+            "bounds:[%{public}f, %{public}f, %{public}f, %{public}f]", bounds.x_, bounds.y_, bounds.z_, bounds.w_);
     }
 }
 
