968de9871b7ac7c1c1442035d49244c0417eeb8a
zhongpengzhi <zhongpengzhi@huawei.com>
2025-02-21 06:49:45 +0000

    热动态调帧
    Signed-off-by: zhongpengzhi <zhongpengzhi@huawei.com>

--- rosen/modules/hyper_graphic_manager/core/utils/hgm_command.h
+++ rosen/modules/hyper_graphic_manager/core/utils/hgm_command.h
@@ -33,6 +33,7 @@ constexpr int ADAPTIVE_SYNC_ENABLED = 1;
 constexpr int32_t SWITCH_SCREEN_SCENE = 1;
 constexpr int32_t STRING_BUFFER_MAX_SIZE = 256;
 constexpr int64_t IDEAL_PULSE = 2777778; // 2.777778ms
+const std::string HGM_CONFIG_TYPE_THERMAL_SUFFIX = "_THERMAL";
 
 enum OledRefreshRate {
     OLED_NULL_HZ = 0,
