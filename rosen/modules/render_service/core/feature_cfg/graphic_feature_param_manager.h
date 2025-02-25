546eb4c9fec3a66f352226f676beee3a2c2e2e01
sunqizhen <sunqizhen2@huawei.com>
2025-02-25 19:44:31 +0800

    Add DirtyRegion Param Parse
    Signed-off-by: sunqizhen <sunqizhen2@huawei.com>

--- rosen/modules/render_service/core/feature_cfg/graphic_feature_param_manager.h
+++ rosen/modules/render_service/core/feature_cfg/graphic_feature_param_manager.h
@@ -22,7 +22,8 @@
 
 #include "refbase.h"
 #include "xml_parser_base.h"
-
+#include "dirtyregion_param.h"
+#include "dirtyregion_param_parse.h"
 #include "drm_param_parse.h"
 #include "drm_param.h"
 #include "hdr_param_parse.h"
@@ -45,6 +46,8 @@ struct ModuleConfig {
 
 // add new module here
 const std::vector<ModuleConfig> FEATURE_MODULES = {
+    {FEATURE_CONFIGS[DIRTYREGION], [] {return std::make_unique<DirtyRegionParamParse>(); },
+        [] {return std::make_unique<DirtyRegionParam>(); }},
     {FEATURE_CONFIGS[HDR], [] {return std::make_unique<HDRParamParse>(); }, [] {return std::make_unique<HDRParam>(); }},
     {FEATURE_CONFIGS[DRM], [] {return std::make_unique<DRMParamParse>(); }, [] {return std::make_unique<DRMParam>(); }},
     {FEATURE_CONFIGS[HWC], [] {return std::make_unique<HWCParamParse>(); }, [] {return std::make_unique<HWCParam>(); }},
