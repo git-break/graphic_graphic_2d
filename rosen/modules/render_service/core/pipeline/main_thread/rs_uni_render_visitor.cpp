c7a9ee5544b9bc0d7603f54dac253aa8ae77caa9
kelinyang <yangxiaopeng8@huawei.com>
2025-03-20 15:42:12 +0800

    修复单手模式刷新率DFX显示问题
    Signed-off-by: kelinyang <yangxiaopeng8@huawei.com>

--- rosen/modules/render_service/core/pipeline/main_thread/rs_uni_render_visitor.cpp
+++ rosen/modules/render_service/core/pipeline/main_thread/rs_uni_render_visitor.cpp
@@ -4024,7 +4024,7 @@ void RSUniRenderVisitor::CheckMergeDebugRectforRefreshRate(std::vector<RSBaseRen
         bool needMapAbsRect = true;
         if (curDisplayNode_->GetWindowContainer()) {
             Vector2f scale = curDisplayNode_->GetWindowContainer()->GetRenderProperties().GetScale();
-            if (!ROSEN_EQ(std::abs(scale.x_, 1.0f), EPSILON_SCALE) || !ROSEN_EQ(std::abs(scale.y_, 1.0f), EPSILON_SCALE)) {
+            if (!ROSEN_EQ(scale.x_, 1.0f, EPSILON_SCALE) || !ROSEN_EQ(scale.y_, 1.0f, EPSILON_SCALE)) {
                 needMapAbsRect = false;
             }
         }
