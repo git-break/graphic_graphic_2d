43ed4c0f1f795b9eea25977802d62be1ff36f291
fengj130406 <fengj130406@outlook.com>
2025-04-08 09:14:30 +0800

    添加头文件
    Signed-off-by: fengj130406 <fengj130406@outlook.com>

--- rosen/modules/ressched/src/ressched_event_listener.cpp
+++ rosen/modules/ressched/src/ressched_event_listener.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <charconv>
 #include <limits>
 
 #include "ressched_event_listener.h"
