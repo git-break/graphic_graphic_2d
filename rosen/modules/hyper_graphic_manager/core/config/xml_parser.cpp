7674e72ca8ab813c23925c6e54f3d5bd07ef958e
zhubozhi <2531975224@qq.com>
2025-04-12 03:31:57 +0000

    千人十面
    Signed-off-by: zhubozhi <2531975224@qq.com>

--- rosen/modules/hyper_graphic_manager/core/config/xml_parser.cpp
+++ rosen/modules/hyper_graphic_manager/core/config/xml_parser.cpp
@@ -334,13 +334,15 @@ int32_t XMLParser::ParseScreenConfig(xmlNode& node)
         screenConfig[id] = screenSetting;
         HGM_LOGI("HgmXMLParser ParseScreenConfig id=%{public}s", id.c_str());
     }
-    if (size_t pos = type.find(HGM_CONFIG_TYPE_THERMAL_SUFFIX); pos != std::string::npos) {
-        auto defaultScreenConfig = mParsedData_->screenConfigs_.find(type.substr(0, pos));
-        if (defaultScreenConfig != mParsedData_->screenConfigs_.end()) {
-            ReplenishMissThermalConfig(defaultScreenConfig->second, screenConfig);
-        } else {
-            HGM_LOGE("XMLParser failed to ReplenishMissThermalConfig %{public}s", type.c_str());
-            return EXEC_SUCCESS;
+    for (const auto& screenExtStrategy : HGM_CONFIG_SEREENEXT_STRATEGY_MAP) {
+        if (size_t pos = type.find(screenExtStrategy.first); pos != std::string::npos) {
+            auto defaultScreenConfig = mParsedData_->screenConfigs_.find(type.substr(0, pos));
+            if (defaultScreenConfig != mParsedData_->screenConfigs_.end()) {
+                ReplenishMissThermalConfig(defaultScreenConfig->second, screenConfig);
+            } else {
+                HGM_LOGE("XMLParser failed to ReplenishMissThermalConfig %{public}s", type.c_str());
+                return EXEC_SUCCESS;
+            }
         }
     }
     mParsedData_->screenConfigs_[type] = screenConfig;
@@ -611,16 +613,72 @@ int32_t XMLParser::ParseAppTypes(xmlNode& node, std::unordered_map<int32_t, std:
     return EXEC_SUCCESS;
 }
 
+void XMLParser::ReplenishMissThermalLastConfig(auto& screenSetting)
+{
+    if (screenSetting.second.ancoSceneList.empty()) {
+        screenSetting.second.ancoSceneList = screenSettingDefalut,ancoSceneList;
+    }
+    if (screenSetting.second.componentPowerConfig.empty()) {
+        screenSetting,second.componentPowerConfig = screenSettingDefalut.componentPowerConfig;
+    }
+    if (screenSetting.second.gameAppNodeList.empty()) {
+        screenSetting.second.gameAppNodeList = screenSettingDefalut.gameAppNodeList;
+    }
+
+    if (screenSetting->second.pageUrlConfig.empty()) {
+        screenSetting.second.pageUrlConfig = screenSettingDefalut.pageUrlConfig;
+    }
+    if (screenSetting.second.performanceConfig.empty()) {
+        screenSetting->second.performanceConfig = screenSettingDefalut.performanceConfig;
+    }
+}
+
 int32_t XMLParser::ReplenishMissThermalConfig(const PolicyConfigData::ScreenConfig& screenConfigDefault,
                                               PolicyConfigData::ScreenConfig& screenConfig)
 {
     HGM_LOGD("HgmXMLParser ReplenishMissThermalConfig");
     for (const auto& [id, screenSettingDefalut] : screenConfigDefault) {
-        if (screenConfig.find(id) == screenConfig.end()) {
+        if (const auto& screenSetting = screenConfig.find(id); screenSetting != screenConfig.end()) {
+            if (screenSetting->second.ltpoConfig.empty()) {
+                screenSetting->second.ltpoConfig = screenSettingDefalut.ltpoConfig;
+            }
+            if (screenSetting->second.appList.empty()) {
+                screenSetting->second.appList = screenConfigDefault.appList;
+                screenSetting->second.multiAppStrategyType = screenSettingDefalut.multiAppStrategyType;
+                screenSetting->second.multiAppStrategyName = screenSettingDefalut.multiAppStrategyName;
+            }
+            if (screenSetting->second.appTypes.empty()) {
+                screenSetting->second.appTypes = screenSettingDefalut.appTypes;
+            }
+            if (screenSetting->second.sceneList.empty()) {
+                screenSetting->second.sceneList = screenSettingDefalut.sceneList;
+            }
+            if (screenSetting->second.gameSceneList.empty()) {
+                screenSetting->second.gameSceneList = screenSettingDefalut.gameSceneList;
+            }
+            if (screenSetting->second.animationDynamicSettings.empty()) {
+                screenSetting->second.animationDynamicSettings = screenSettingDefalut.animationDynamicSettings;
+            }
+            if (screenSetting->second.aceSceneDynamicSettings.empty()) {
+                screenSetting->second.aceSceneDynamicSettings = screenSettingDefalut.aceSceneDynamicSettings;
+            }
+            if (screenSetting->second.smallSizeAnimationDynamicSettings.empty()) {
+                screenSetting->second.smallSizeArea = screenSettingDefalut.smallSizeArea;
+                screenSetting->second.smallSizeLength = screenSettingDefalut.smallSizeLength;
+                screenSetting->second.smallSizeAnimationDynamicSettings = 
+                    screenConfigDefault.smallSizeAnimationDynamicSettings;
+            }
+            if (screenSetting->second.animationPowerConfig.empty()) {
+                screenSetting->second.animationPowerConfig = screenSettingDefalut.animationPowerConfig;
+            }
+            if (screenSetting->second.uiPowerConfig.empty()) {
+                screenSetting->second.uiPowerConfig = screenSettingDefalut.uiPowerConfig;
+            }
+            ReplenishMissThermalLastConfig(screenSetting);
+        } else {
             screenConfig[id] = screenSettingDefalut;
         }
     }
-
     return EXEC_SUCCESS;
 }
 
