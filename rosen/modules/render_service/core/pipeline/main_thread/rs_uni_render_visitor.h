a15ee57c997dfeb7c87364fa0959a06253f23ab5
yangfan479 <yangfan479@huawei.com>
2025-04-14 20:36:11 +0800

    SPOC Debug：优化透明窗口的stencil值 & 修复透明模糊显示问题
    Signed-off-by: yangfan479 <yangfan479@huawei.com>

--- rosen/modules/render_service/core/pipeline/main_thread/rs_uni_render_visitor.h
+++ rosen/modules/render_service/core/pipeline/main_thread/rs_uni_render_visitor.h
@@ -161,6 +161,7 @@ private:
     void CalculateOpaqueAndTransparentRegion(RSSurfaceRenderNode& node);
 
     void CheckFilterCacheNeedForceClearOrSave(RSRenderNode& node);
+    Occlusion::Region GetSurfaceTransparentFilterRegion(NodeId surfaceNodeId) const;
     void CollectTopOcclusionSurfacesInfo(RSSurfaceRenderNode& node, bool isParticipateInOcclusion);
     void UpdateOccludedStatusWithFilterNode(std::shared_ptr<RSSurfaceRenderNode>& surfaceNode) const;
     void PartialRenderOptionInit();
@@ -375,7 +376,7 @@ private:
     std::unordered_map<NodeId, std::vector<std::pair<NodeId, RectI>>> transparentDirtyFilter_;
     // record DRM nodes
     std::vector<std::weak_ptr<RSSurfaceRenderNode>> drmNodes_;
-    int16_t occlusionSurfaceOrder_ = -1;
+    int16_t occlusionSurfaceOrder_ = DEFAULT_OCCLUSION_SURFACE_ORDER;
     sptr<RSScreenManager> screenManager_;
     ScreenInfo screenInfo_;
     RectI screenRect_;
