ef61fdd6856e5b6ce68bac6a7e441c4a2be26abe
huangweijun <huangweijun18@huawei.com>
2025-04-16 14:29:20 +0800

    Signed-off-by: huangweijun <huangweijun18@huawei.com> Change-Id: I4fe33ad9a901b94e0b223878ebcef6408bef98f5

--- rosen/modules/render_service_client/core/ui/rs_canvas_node.cpp
+++ rosen/modules/render_service_client/core/ui/rs_canvas_node.cpp
@@ -99,6 +99,7 @@ ExtendRecordingCanvas* RSCanvasNode::BeginRecording(int width, int height)
     if (recordingCanvas_) {
         delete recordingCanvas_;
         recordingCanvas_ = nullptr;
+        RS_LOGE("RSCanvasNode::BeginRecording last beginRecording without finishRecording");
     }
 
     recordingCanvas_ = new ExtendRecordingCanvas(width, height);
