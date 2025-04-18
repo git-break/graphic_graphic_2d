b3dde02a7efd9ae923567964f0a02f2ef61f0ce0
zyan <zhangyan407@huawei.com>
2025-04-18 11:31:49 +0800

    预览器无法显示粒子动画图片类型问题修改
    Signed-off-by: zyan <zhangyan407@huawei.com>

--- rosen/modules/render_service_base/src/render/rs_particles_drawable.cpp
+++ rosen/modules/render_service_base/src/render/rs_particles_drawable.cpp
@@ -228,7 +228,7 @@ void RSParticlesDrawable::DrawCircle(Drawing::Canvas& canvas)
     canvas.DetachBrush();
 }
 
-bool CheckImageNull(std::shared_ptr<Drawing::Image>& image, const std::shared_ptr<Drawing::Image>& drawImage)
+bool RSParticlesDrawable::CheckImageNull(std::shared_ptr<Drawing::Image>& image, const std::shared_ptr<Drawing::Image>& drawImage)
 {
     if (image) {
         return false;
