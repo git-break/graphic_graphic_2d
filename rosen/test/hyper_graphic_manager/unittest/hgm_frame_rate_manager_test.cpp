a92e800208b31b8c4167c6a1b775d0a9eb57d77f
鲁保良 <lubaoliang1@h-partners.com>
2025-04-25 19:46:02 +0800

    a4
    Signed-off-by: 鲁保良 <lubaoliang1@h-partners.com>

--- rosen/test/hyper_graphic_manager/unittest/hgm_frame_rate_manager_test.cpp
+++ rosen/test/hyper_graphic_manager/unittest/hgm_frame_rate_manager_test.cpp
@@ -417,7 +417,7 @@ HWTEST_F(HgmFrameRateMgrTest, HgmConfigCallbackManagerTest002, Function | SmallT
             hccMgr->SyncRefreshRateUpdateCallback(OLED_60_HZ);
             std::unordered_map<pid_t, std::pair<int32_t, std::string>> foregroundPidAppMap;
             foregroundPidAppMap.try_emplace(pid, std::pair<int32_t, std::string>{0, "com.app10"});
-            hccMgr->SyncRefreshRateUpdateCallback(foregroundPidAppMap);
+            hccMgr->SyncHgmConfigChangeCallback(foregroundPidAppMap);
             ASSERT_EQ(hccMgr->pendingAnimDynamicCfgCallbacks_.find(pid) ==
                 hccMgr->pendingAnimDynamicCfgCallbacks_.end(), true);
         }
