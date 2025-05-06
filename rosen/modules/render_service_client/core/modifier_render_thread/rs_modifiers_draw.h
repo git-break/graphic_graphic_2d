bc8fc1d2737c7aa5d42621f998a6d00fc8932ce4
unknown <wangyan471@huawei.com>
2025-05-06 21:59:26 +0800

    修复负载前移CDN遗留问题
    Signed-off-by: unknown <wangyan471@huawei.com>

--- rosen/modules/render_service_client/core/modifier_render_thread/rs_modifiers_draw.h
+++ rosen/modules/render_service_client/core/modifier_render_thread/rs_modifiers_draw.h
@@ -91,7 +91,7 @@ private:
 
     static void DrawSnapshot(std::shared_ptr<Drawing::Canvas>& canvas, std::shared_ptr<Drawing::Image>& snapshot);
 
-    static void CheckAndDrawSnapshot(SurfaceEntry& surfaceEntry,
+    static bool CheckAndDrawSnapshot(SurfaceEntry& surfaceEntry,
         const std::shared_ptr<Drawing::DrawCmdList>& cmdList, NodeId nodeId);
 
     static void AddPixelMapDrawOp(const std::shared_ptr<Drawing::DrawCmdList>& cmdList,
