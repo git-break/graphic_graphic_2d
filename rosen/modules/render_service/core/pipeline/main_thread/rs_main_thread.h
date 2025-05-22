c313c6cb94fa8b11bc14f7ea95f046b001a62850
罗雅雪 <luoyaxue@huawei.com>
2025-05-22 09:42:32 +0800

    dirty
    Signed-off-by: 罗雅雪 <luoyaxue@huawei.com>

--- rosen/modules/render_service/core/pipeline/main_thread/rs_main_thread.h
+++ rosen/modules/render_service/core/pipeline/main_thread/rs_main_thread.h
@@ -342,16 +342,6 @@ public:
 
     bool IsRequestedNextVSync();
 
-    bool GetNextDVsyncAnimateFlag() const
-    {
-        return needRequestNextVsyncAnimate_;
-    }
-
-    bool IsFirstFrameOfPartialRender() const
-    {
-        return isFirstFrameOfPartialRender_;
-    }
-
     bool IsOcclusionNodesNeedSync(NodeId id, bool useCurWindow);
 
     void CallbackDrawContextStatusToWMS(bool isUniRender = false);
@@ -627,9 +617,6 @@ private:
 #endif
     bool isCurtainScreenOn_ = false;
     // partial render
-    bool isFirstFrameOfPartialRender_ = false;
-    bool isPartialRenderEnabledOfLastFrame_ = false;
-    bool isRegionDebugEnabledOfLastFrame_ = false;
     bool isForceRefresh_ = false;
     // record multidisplay status change
     bool isMultiDisplayPre_ = false;
