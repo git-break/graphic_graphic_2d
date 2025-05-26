49aa0f9569e6bcdf16bc82f0e316722aef066d32
183******26 <mashaobo10@h-partners.com>
2025-05-26 21:55:11 +0800

    add for SDP pixel format test
    Signed-off-by: 183******26 <mashaobo10@h-partners.com>

--- rosen/modules/render_service/core/pipeline/render_thread/rs_base_render_engine.cpp
+++ rosen/modules/render_service/core/pipeline/render_thread/rs_base_render_engine.cpp
@@ -45,6 +45,9 @@
 #ifdef RS_ENABLE_GPU
 #include "drawable/rs_display_render_node_drawable.h"
 #endif
+#ifdef RS_ENABLE_TV_PQ_METADATA
+#include "feature/tv_metadata/rs_tv_metadata_manager.h"
+#endif
 #include "utils/graphic_coretrace.h"
 
 namespace OHOS {
@@ -763,6 +766,15 @@ void RSBaseRenderEngine::DrawImage(RSPaintFilterCanvas& canvas, BufferDrawParam&
     if (image == nullptr) {
         return;
     }
+
+    // add for tv metadata
+#ifdef RS_ENABLE_TV_PQ_METADATA
+    TvPQMetadata info;
+    if (MetadataHelper::GetVideoTVMetadata(params.buffer, info) == GSERROR_OK) {
+        RSTvMetadataManager::Instance().RecordAndCombineMetadata(info);
+        RS_LOGD("RSBaseRenderEngine::DrawImage record metadata: buffer seq:%{public}d", params.buffer->GetSeqNum());
+    }
+#endif
     Drawing::SamplingOptions samplingOptions;
     if (!RSSystemProperties::GetUniRenderEnabled()) {
         samplingOptions = Drawing::SamplingOptions();
