36e875864f3d5af1e68d05feeafac2c9ce55a19f
m00899184 <maochicong@huawei.com>
2025-06-10 10:31:41 +0800

    代码回退
    Signed-off-by: m00899184 <maochicong@huawei.com>
    Change-Id: I374b159f2914fea30cba5062bfeb021e1a1f3fa9

--- rosen/modules/render_service/core/pipeline/main_thread/rs_render_service_connection.cpp
+++ rosen/modules/render_service/core/pipeline/main_thread/rs_render_service_connection.cpp
@@ -41,7 +41,6 @@
 #include "feature/capture/rs_ui_capture_solo_task_parallel.h"
 #include "feature/capture/rs_surface_capture_task_parallel.h"
 #include "feature/uifirst/rs_uifirst_frame_rate_control.h"
-#include "gfx/fps_info/rs_surface_fps_manager.h"
 #include "gfx/first_frame_notifier/rs_first_frame_notifier.h"
 #ifdef RS_ENABLE_OVERLAY_DISPLAY
 #include "feature/overlay_display/rs_overlay_display_manager.h"
@@ -1055,22 +1054,22 @@ ErrCode RSRenderServiceConnection::GetRefreshInfo(pid_t pid, std::string& enable
     if (renderType == UniRenderEnabledType::UNI_RENDER_ENABLED_FOR_ALL) {
 #ifdef RS_ENABLE_GPU
         RSHardwareThread::Instance().ScheduleTask(
-            [weakThis = wptr<RSRenderServiceConnection>(this), &dumpString, &pid]() {
+            [weakThis = wptr<RSRenderServiceConnection>(this), &dumpString, &surfaceName]() {
                 sptr<RSRenderServiceConnection> connection = weakThis.promote();
                 if (connection == nullptr || connection->screenManager_ == nullptr) {
                     return;
                 }
-                RSSurfaceFpsManager::GetInstance().DumpByPid(dumpString, pid);
+                connection->screenManager_->FpsDump(dumpString, surfaceName);
             }).wait();
 #endif
     } else {
         mainThread_->ScheduleTask(
-            [weakThis = wptr<RSRenderServiceConnection>(this), &dumpString, &pid]() {
+            [weakThis = wptr<RSRenderServiceConnection>(this), &dumpString, &surfaceName]() {
                 sptr<RSRenderServiceConnection> connection = weakThis.promote();
                 if (connection == nullptr || connection->screenManager_ == nullptr) {
                     return;
                 }
-                RSSurfaceFpsManager::GetInstance().DumpByPid(dumpString, pid);
+                connection->screenManager_->FpsDump(dumpString, surfaceName);
             }).wait();
     }
     enable = dumpString;
