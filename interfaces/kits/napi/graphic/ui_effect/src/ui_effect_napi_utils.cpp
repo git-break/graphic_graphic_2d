5066fcb3b72e58cd605d174700b2d35568c7ca2b
xumale <xumale@h-partners.com>
2025-06-15 14:25:04 +0000

    update interfaces/kits/napi/graphic/ui_effect/src/ui_effect_napi_utils.cpp.
    Signed-off-by: xumale <xumale@h-partners.com>

--- interfaces/kits/napi/graphic/ui_effect/src/ui_effect_napi_utils.cpp
+++ interfaces/kits/napi/graphic/ui_effect/src/ui_effect_napi_utils.cpp
@@ -125,20 +125,6 @@ bool ConvertFromJsPoint(napi_env env, napi_value jsObject, double* point, size_t
     return true;
 }
 
-bool ParseJsVector3f(napi_env env, napi_value jsObject, Vector3f& values)
-{
-    for (size_t idx = 0; idx < NUM_3; idx++) {
-        napi_value tempValue = nullptr;
-        double value = 0.0f;
-        napi_get_named_property(env, jsObject, POINT3D_STRING[idx], &tempValue);
-        if (tempValue == nullptr || napi_get_value_double(env, tempValue, &value) != napi_ok) {
-            return false;
-        }
-        values[idx] = value;
-    }
-    return true;
-}
-
 bool ParseJsPoint(napi_env env, napi_value jsObject, Vector2f& point)
 {
     napi_value tmpValue = nullptr;
