1a34f600f03b10dd5e27078f65e7e3c27e4abe74
董洁 <dongjie52@h-partners.com>
2025-06-24 02:50:05 +0000

    update rosen/modules/render_service_base/include/drawable/rs_property_drawable_utils.h.
    Signed-off-by: 董洁 <dongjie52@h-partners.com>

--- rosen/modules/render_service_base/include/drawable/rs_property_drawable_utils.h
+++ rosen/modules/render_service_base/include/drawable/rs_property_drawable_utils.h
@@ -20,7 +20,6 @@
 
 namespace OHOS {
 namespace Rosen {
-class RSHpaeFilterCacheManager;
 class RSPropertyDrawableUtils {
 public:
     static Drawing::RoundRect RRect2DrawingRRect(const RRect& rr);
@@ -57,7 +56,7 @@ public:
     static std::shared_ptr<Drawing::RuntimeBlenderBuilder> MakeDynamicBrightnessLinearBuilder();
     static std::shared_ptr<Drawing::Blender> MakeDynamicBrightnessBlender(const RSDynamicBrightnessPara& params);
     static void DrawBinarization(Drawing::Canvas* canvas, const std::optional<Vector4f>& aiInvert);
-    static void DrawPixelStretch(Drawing::Canvas* canvas, NodeId nodeId, const std::optional<Vector4f>& pixelStretch,
+    static void DrawPixelStretch(Drawing::Canvas* canvas, const std::optional<Vector4f>& pixelStretch,
         const RectF& boundsRect, const bool boundsGeoValid, const Drawing::TileMode pixelStretchTileMode);
     static Drawing::Path CreateShadowPath(const std::shared_ptr<RSPath> rsPath,
         const std::shared_ptr<RSPath>& clipBounds, const RRect& rrect);
