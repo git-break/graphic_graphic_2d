c2ef7b7ac3886dc6be4723abd36d832e405eb681
lingzhengyang <lingzhengyang@h-partners.com>
2025-07-10 08:27:50 +0000

    头文件添加
    Signed-off-by: lingzhengyang <lingzhengyang@h-partners.com>

--- rosen/test/render_service/render_service_client/unittest/property/rs_ui_filter_para_base_test.cpp
+++ rosen/test/render_service/render_service_client/unittest/property/rs_ui_filter_para_base_test.cpp
@@ -15,7 +15,9 @@
 
 #include "gtest/gtest.h"
 #include "ui_effect/filter/include/filter_blur_para.h"
+#include "ui_effect/filter/include/filter_color_gradient_para.h"
 #include "ui_effect/filter/include/filter_direction_light_para.h"
+#include "ui_effect/filter/include/filter_dispersion_para.h"
 #include "ui_effect/filter/include/filter_displacement_distort_para.h"
 #include "ui_effect/filter/include/filter_edge_light_para.h"
 #include "ui_effect/filter/include/filter_mask_transition_para.h"
