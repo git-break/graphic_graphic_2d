0df848fe9f689678846aedd05867b4c9d31d9167
liufei <liufei225@h-partners.com>
2025-07-17 20:21:14 +0800

    fix(fuzztest): limit the length of randomly generated strings
    - Replace ConsumeRandomLengthString() with ConsumeBytesAsString(DATA_MAX_LAYOUT_WIDTH)
    - This change helps to avoid potential memory issues with very long strings
    
    Signed-off-by: liufei <liufei225@h-partners.com>
    Change-Id: I5a6fa6062691ed653912a2a52c6956dc204496d5

--- frameworks/text/test/fuzztest/ndk/ndktypography_fuzzer/drawing_text_typography_fuzzer.cpp
+++ frameworks/text/test/fuzztest/ndk/ndktypography_fuzzer/drawing_text_typography_fuzzer.cpp
@@ -229,7 +229,7 @@ OH_Drawing_TypographyCreate* CreateTypographyHandler(OH_Drawing_TypographyCreate
         .baselineOffset = 0 };
     OH_Drawing_TypographyHandlerAddPlaceholder(handler, &placeholder);
     OH_Drawing_TypographyHandlerPushTextStyle(handler, txtStyle);
-    std::string dest = fdp.ConsumeRandomLengthString();
+    std::string dest = fdp.ConsumeBytesAsString(DATA_MAX_LAYOUT_WIDTH);
     std::u16string u16 = Str8ToStr16ByIcu(dest);
     icu::UnicodeString unicodeString(u16.data(), u16.size());
     unicodeString.toUTF8String(dest);
