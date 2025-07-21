d109f05c0350452e1378ba424b3fd3fcdc7fdfbe
unknown <wangyan471@huawei.com>
2025-07-21 17:03:19 +0800

    revert dump
    Signed-off-by: unknown <wangyan471@huawei.com>

--- rosen/modules/render_service_base/include/pipeline/rs_render_node.h
+++ rosen/modules/render_service_base/include/pipeline/rs_render_node.h
@@ -11,7 +11,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ */ 
 #ifndef RENDER_SERVICE_CLIENT_CORE_PIPELINE_RS_RENDER_NODE_H
 #define RENDER_SERVICE_CLIENT_CORE_PIPELINE_RS_RENDER_NODE_H
 
