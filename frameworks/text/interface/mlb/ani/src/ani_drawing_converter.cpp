d8a6cfe6b35a262048055a1b31539b0516e990b7
PengKejie <pengkejie@h-partners.com>
2025-07-24 17:28:02 +0800

    change color double to int
    Signed-off-by: PengKejie <pengkejie@h-partners.com>

--- frameworks/text/interface/mlb/ani/src/ani_drawing_converter.cpp
+++ frameworks/text/interface/mlb/ani/src/ani_drawing_converter.cpp
@@ -28,7 +28,7 @@ bool GetColorValue(ani_env* env, ani_object colorObj, const char* name, int32_t&
 {
     ani_double tempValue = 0;
     ani_status isAlphaOk =
-        env->Object_GetPropertyByName_Double(reinterpret_cast<ani_object>(colorObj), name, &tempValue);
+        env->Object_GetPropertyByName_Int(reinterpret_cast<ani_object>(colorObj), name, &tempValue);
     if (isAlphaOk != ANI_OK) {
         return false;
     }
@@ -151,20 +151,16 @@ ani_status AniDrawingConverter::ParsePointToAni(ani_env* env, const OHOS::Rosen:
 ani_status AniDrawingConverter::ParseColorToAni(ani_env* env, const OHOS::Rosen::Drawing::Color& color, ani_object& obj)
 {
     ani_object aniObj = AniTextUtils::CreateAniObject(env, ANI_CLASS_COLOR, ":V");
-    if (ANI_OK != env->Object_SetPropertyByName_Double(aniObj, "alpha", ani_double(color.GetAlpha()))) {
-        TEXT_LOGD("Failed to set color param: alpha");
+    if (ANI_OK != env->Object_SetPropertyByName_Int(aniObj, "alpha", ani_double(color.GetAlpha()))) {
         return ANI_INVALID_ARGS;
     }
-    if (ANI_OK != env->Object_SetPropertyByName_Double(aniObj, "red", ani_double(color.GetRed()))) {
-        TEXT_LOGD("Failed to set color param: red");
+    if (ANI_OK != env->Object_SetPropertyByName_Int(aniObj, "red", ani_double(color.GetRed()))) {
         return ANI_INVALID_ARGS;
     }
-    if (ANI_OK != env->Object_SetPropertyByName_Double(aniObj, "green", ani_double(color.GetGreen()))) {
-        TEXT_LOGD("Failed to set color param: green");
+    if (ANI_OK != env->Object_SetPropertyByName_Int(aniObj, "green", ani_double(color.GetGreen()))) {
         return ANI_INVALID_ARGS;
     }
-    if (ANI_OK != env->Object_SetPropertyByName_Double(aniObj, "blue", ani_double(color.GetBlue()))) {
-        TEXT_LOGD("Failed to set color param: blue");
+    if (ANI_OK != env->Object_SetPropertyByName_Int(aniObj, "blue", ani_double(color.GetBlue()))) {
         return ANI_INVALID_ARGS;
     }
     obj = aniObj;
