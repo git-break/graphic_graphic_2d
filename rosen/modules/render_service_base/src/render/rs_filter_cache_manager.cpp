173df22160446a6dfbe6330433de5d9c3cb7fb77
gao0707 <528253359@qq.com>
2025-07-30 09:53:55 +0800

    增加性能维测
    Signed-off-by: gao0707 <528253359@qq.com>

--- rosen/modules/render_service_base/src/render/rs_filter_cache_manager.cpp
+++ rosen/modules/render_service_base/src/render/rs_filter_cache_manager.cpp
@@ -292,6 +292,8 @@ void RSFilterCacheManager::TakeSnapshot(
     std::shared_ptr<Drawing::Image> snapshot;
     auto aibarShaderFilter = filter->GetShaderFilterWithType(RSUIFilterType::AIBAR);
     if ((aibarShaderFilter != nullptr) && (HveFilter::GetHveFilter().GetSurfaceNodeSize() > 0)) {
+        RS_TRACE_NAME_FMT("RSFilterCacheManager::TakeSnapshot surface wh:[%d, %d], snapshotlBounds:%s",
+            drawingSurface->Width(), drawingSurface->Height(), snapshotlBounds.ToString().c_str());
         snapshot = HveFilter::GetHveFilter().SampleLayer(canvas, srcRect);
     } else {
         // Take a screenshot
