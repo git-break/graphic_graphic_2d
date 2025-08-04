054e528175a60eb545998474d41df5ddc4efce38
孙宇航 <sunyuhang9@huawei.com>
2025-08-05 00:05:35 +0800

    添加 RSRenderParticleVector 空序列化实现
    在所有平台（ohos、windows、darwin）的 rs_marshalling_helper.cpp 中
    添加了 RSRenderParticleVector 的空序列化和反序列化实现，返回false，
    避免内存泄漏问题
    
    Signed-off-by: 孙宇航 <sunyuhang9@huawei.com>

--- rosen/modules/render_service_base/src/platform/ohos/rs_marshalling_helper.cpp
+++ rosen/modules/render_service_base/src/platform/ohos/rs_marshalling_helper.cpp
@@ -3276,5 +3276,15 @@ bool RSMarshallingHelper::TransactionVersionCheck(Parcel& parcel, uint8_t suppor
     parcel.RewindRead(offset);
     return false;
 }
+
+bool RSMarshallingHelper::Marshalling(Parcel& parcel, const RSRenderParticleVector& val)
+{
+    return false;
+}
+
+bool RSMarshallingHelper::Unmarshalling(Parcel& parcel, RSRenderParticleVector& val)
+{
+    return false;
+}
 } // namespace Rosen
 } // namespace OHOS
