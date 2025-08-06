5414c75a89815e09bd85ac1626eb254f0daec36d
李想 <lixiang542@h-partners.com>
2025-08-06 09:33:23 +0000

    update rosen/modules/render_service_base/include/common/rs_optional_trace.h.
    Signed-off-by: 李想 <lixiang542@h-partners.com>

--- rosen/modules/render_service_base/include/common/rs_optional_trace.h
+++ rosen/modules/render_service_base/include/common/rs_optional_trace.h
@@ -22,100 +22,101 @@
 #include "platform/common/rs_system_properties.h"
 static inline int g_debugLevel = OHOS::Rosen::RSSystemProperties::GetDebugTraceLevel();
 
-#define RS_OPTIONAL_TRACE_BEGIN_LEVEL(Level, fmt, ...)           \
-    do {                                                         \
-        if (UNLIKELY(g_debugLevel >= (Level))) {                 \
-            RenderTrace::OptionalTraceStart(fmt, ##__VA_ARGS__); \
-        }                                                        \
+#define RS_OPTIONAL_TRACE_BEGIN_LEVEL(Level, fmt, ...)                \
+    do {                                                              \
+        if (UNLIKELY(g_debugLevel >= (Level))) {                      \
+            RSOptionalFmtTrace::StartFormatTrace(fmt, ##__VA_ARGS__); \
+        }                                                             \
     } while (0)
 
-#define RS_OPTIONAL_TRACE_END_LEVEL(Level)                       \
-    do {                                                         \
-        if (UNLIKELY(g_debugLevel >= (Level))) {                 \
-            FinishTrace(HITRACE_TAG_GRAPHIC_AGP);                \
-        }                                                        \
+#define RS_OPTIONAL_TRACE_END_LEVEL(Level)        \
+    do {                                          \
+        if (UNLIKELY(g_debugLevel >= (Level))) {  \
+            FinishTrace(HITRACE_TAG_GRAPHIC_AGP); \
+        }                                         \
     } while (0)
 
-#define RS_OPTIONAL_TRACE_BEGIN(name)                            \
-    do {                                                         \
-        if (Rosen::RSSystemProperties::GetDebugTraceEnabled()) { \
-            RS_TRACE_BEGIN(name);                                \
-        }                                                        \
+#define RS_OPTIONAL_TRACE_BEGIN(name)                                      \
+    do {                                                                   \
+        if (UNLIKELY(Rosen::RSSystemProperties::GetDebugTraceEnabled())) { \
+            RS_TRACE_BEGIN(name);                                          \
+        }                                                                  \
     } while (0)
 
-#define RS_OPTIONAL_TRACE_END()                                  \
-    do {                                                         \
-        if (Rosen::RSSystemProperties::GetDebugTraceEnabled()) { \
-            RS_TRACE_END();                                      \
-        }                                                        \
+#define RS_OPTIONAL_TRACE_END()                                            \
+    do {                                                                   \
+        if (UNLIKELY(Rosen::RSSystemProperties::GetDebugTraceEnabled())) { \
+            RS_TRACE_END();                                                \
+        }                                                                  \
     } while (0)
 
 #define RS_OPTIONAL_TRACE_NAME_FMT(fmt, ...)                                \
     do {                                                                    \
-        if (Rosen::RSSystemProperties::GetDebugTraceEnabled()) {            \
+        if (UNLIKELY(Rosen::RSSystemProperties::GetDebugTraceEnabled())) {  \
             HITRACE_METER_FMT(HITRACE_TAG_GRAPHIC_AGP, fmt, ##__VA_ARGS__); \
         }                                                                   \
     } while (0)
 
-#define RS_OPTIONAL_TRACE_NAME_FMT_LEVEL(Level, fmt, ...)                   \
-    do {                                                                    \
-        if (Rosen::RSSystemProperties::GetDebugTraceLevel() >= Level) {     \
-            HITRACE_METER_FMT(HITRACE_TAG_GRAPHIC_AGP, fmt, ##__VA_ARGS__); \
-        }                                                                   \
+#define RS_OPTIONAL_TRACE_NAME_FMT_LEVEL(Level, fmt, ...)                         \
+    do {                                                                          \
+        if (UNLIKELY(Rosen::RSSystemProperties::GetDebugTraceLevel() >= Level)) { \
+            HITRACE_METER_FMT(HITRACE_TAG_GRAPHIC_AGP, fmt, ##__VA_ARGS__);       \
+        }                                                                         \
     } while (0)
 
-#define RS_APPOINTED_TRACE_BEGIN(node, name)                         \
-    do {                                                             \
-        if (Rosen::RSSystemProperties::GetDebugTraceEnabled() ||     \
-            Rosen::RSSystemProperties::FindNodeInTargetList(node)) { \
-            RS_TRACE_BEGIN(name);                                    \
-        }                                                            \
+#define RS_APPOINTED_TRACE_BEGIN(node, name)                               \
+    do {                                                                   \
+        if (UNLIKELY(Rosen::RSSystemProperties::GetDebugTraceEnabled()) || \
+            Rosen::RSSystemProperties::FindNodeInTargetList(node)) {       \
+            RS_TRACE_BEGIN(name);                                          \
+        }                                                                  \
     } while (0)
 
-#define RS_APPOINTED_TRACE_END(node)                                 \
-    do {                                                             \
-        if (Rosen::RSSystemProperties::GetDebugTraceEnabled() ||     \
-            Rosen::RSSystemProperties::FindNodeInTargetList(node)) { \
-            RS_TRACE_END();                                          \
-        }                                                            \
+#define RS_APPOINTED_TRACE_END(node)                                       \
+    do {                                                                   \
+        if (UNLIKELY(Rosen::RSSystemProperties::GetDebugTraceEnabled()) || \
+            Rosen::RSSystemProperties::FindNodeInTargetList(node)) {       \
+            RS_TRACE_END();                                                \
+        }                                                                  \
     } while (0)
 
-#define RS_OPTIONAL_TRACE_NAME(name) RSOptionalTrace optionalTrace(name)
+#define RS_OPTIONAL_TRACE_NAME(name)                                                        \
+    auto optionalTrace = (UNLIKELY(Rosen::RSSystemProperties::GetDebugTraceEnabled())) ?    \
+                            std::make_unique<RSOptionalTrace>(name) :                       \
+                            nullptr
 
-#define RS_OPTIONAL_TRACE_FUNC() RSOptionalTrace optionalTrace(__func__)
+#define RS_OPTIONAL_TRACE_FUNC()                                                            \
+    auto optionalTrace = (UNLIKELY(Rosen::RSSystemProperties::GetDebugTraceEnabled())) ?    \
+                            std::make_unique<RSOptionalTrace>(__func__) :                   \
+                            nullptr
 
-#define RS_PROCESS_TRACE(forceEnable, name) RSProcessTrace processTrace(forceEnable, name)
+#define RS_PROCESS_TRACE(forceEnable, name)                                                                 \
+    auto processTrace = ((forceEnable) || UNLIKELY(Rosen::RSSystemProperties::GetDebugTraceEnabled())) ?    \
+                            std::make_unique<RSOptionalTrace>(name) :                                       \
+                            nullptr
 
-#define RS_OPTIONAL_TRACE_FMT(fmt, ...)                                 \
-    std::unique_ptr<RSOptionalFmtTrace> optionalFmtTrace =              \
-        OHOS::Rosen::RSSystemProperties::GetDebugFmtTraceEnabled() ?    \
-        std::make_unique<RSOptionalFmtTrace>(fmt, ##__VA_ARGS__) :      \
-        nullptr
-
-class RenderTrace {
-public:
-    static void OptionalTraceStart(const char* fmt, ...)
-    {
-        va_list vaList;
-        char buf[maxSize_];
-        va_start(vaList, fmt);
-        if (vsnprintf_s(buf, sizeof(buf), sizeof(buf) - 1, fmt, vaList) < 0) {
-            va_end(vaList);
-            StartTrace(HITRACE_TAG_GRAPHIC_AGP, "length > 256, error");
-            return;
-        }
-        va_end(vaList);
-        StartTrace(HITRACE_TAG_GRAPHIC_AGP, buf);
-    }
-private:
-    static const int maxSize_ = 256; // 256 Maximum length of a character string to be printed
-};
+#define RS_OPTIONAL_TRACE_FMT(fmt, ...)                                                         \
+    auto optionalFmtTrace = (UNLIKELY(Rosen::RSSystemProperties::GetDebugFmtTraceEnabled())) ?  \
+                                std::make_unique<RSOptionalFmtTrace>(fmt, ##__VA_ARGS__) :      \
+                                nullptr
 
 class RSOptionalFmtTrace {
 public:
     RSOptionalFmtTrace(const char* fmt, ...)
     {
         va_list vaList;
+        va_start(vaList, fmt);
+        StartFormatTrace(fmt, vaList);
+        va_end(vaList);
+    }
+    ~RSOptionalFmtTrace()
+    {
+        FinishTrace(HITRACE_TAG_GRAPHIC_AGP); // 256 Maximum length of a character string to be printed
+    }
+
+    inline static void StartFormatTrace(const char* fmt, ...)
+    {
+        va_list vaList;
         char buf[maxSize_];
         va_start(vaList, fmt);
         if (vsnprintf_s(buf, sizeof(buf), sizeof(buf) - 1, fmt, vaList) < 0) {
@@ -126,55 +127,23 @@ public:
         va_end(vaList);
         StartTrace(HITRACE_TAG_GRAPHIC_AGP, buf);
     }
-    ~RSOptionalFmtTrace()
-    {
-        FinishTrace(HITRACE_TAG_GRAPHIC_AGP); // 256 Maximum length of a character string to be printed
-    }
 
 private:
-    const int maxSize_ = 256;
+    constexpr static int maxSize_ = 256;
 };
 
 class RSOptionalTrace {
 public:
     RSOptionalTrace(const std::string& traceStr)
     {
-        debugTraceEnable_ = OHOS::Rosen::RSSystemProperties::GetDebugTraceEnabled();
-        if (debugTraceEnable_) {
-            RS_TRACE_BEGIN(traceStr);
-        }
+        RS_TRACE_BEGIN(traceStr);
     }
     ~RSOptionalTrace()
     {
-        if (debugTraceEnable_) {
-            RS_TRACE_END();
-        }
+        RS_TRACE_END();
     }
-
-private:
-    bool debugTraceEnable_ = false;
 };
 
-class RSProcessTrace {
-public:
-    RSProcessTrace(bool forceEnable, const std::string& traceStr)
-    {
-        debugTraceEnable_ = OHOS::Rosen::RSSystemProperties::GetDebugTraceEnabled();
-        forceEnable_ = forceEnable;
-        if (debugTraceEnable_ || forceEnable_) {
-            RS_TRACE_BEGIN(traceStr);
-        }
-    }
-    ~RSProcessTrace()
-    {
-        if (debugTraceEnable_ || forceEnable_) {
-            RS_TRACE_END();
-        }
-    }
-private:
-    bool debugTraceEnable_ = false;
-    bool forceEnable_ = false;
-};
 #else
 #define RS_OPTIONAL_TRACE_BEGIN_LEVEL(Level, fmt, ...)
 #define RS_OPTIONAL_TRACE_END_LEVEL(Level)
