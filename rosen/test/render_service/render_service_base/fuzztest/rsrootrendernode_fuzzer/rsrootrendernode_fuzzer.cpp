3359efac0e97ea57e46ea8f44b754ef85b05fde2
unknown <wangyan471@huawei.com>
2025-08-14 17:45:32 +0800

    code sync
    Signed-off-by: unknown <wangyan471@huawei.com>

--- rosen/test/render_service/render_service_base/fuzztest/rsrootrendernode_fuzzer/rsrootrendernode_fuzzer.cpp
+++ rosen/test/render_service/render_service_base/fuzztest/rsrootrendernode_fuzzer/rsrootrendernode_fuzzer.cpp
@@ -80,7 +80,8 @@ bool DoSomethingInterestingWithMyAPI(const uint8_t* data, size_t size)
     rsRootRenderNode.SetEnableRender(true);
     rsRootRenderNode.GetEnableRender();
 
-    rsRootRenderNode.InitRenderParams();
+    std::unique_ptr<RSRenderParams> stagingRenderParams = std::make_unique<RSRenderParams>(id);
+    rsRootRenderNode.stagingRenderParams_ = std::move(stagingRenderParams);
     rsRootRenderNode.UpdateRenderParams();
     bool enable = GetData<bool>();
     rsRootRenderNode.EnableWindowKeyFrame(enable);
