4ef05e6e3ce00cc0ab634e7f0fa8afcb13f2baf2
Sponge-X688 <zhangyifan140@h-partners.com>
2025-08-14 09:34:09 +0800

    add maybe_unused instace
    Signed-off-by: Sponge-X688 <zhangyifan140@h-partners.com>

--- rosen/test/render_service/render_service/fuzztest/rsrenderserviceconnection000_fuzzer/rsrenderserviceconnection000_fuzzer.cpp
+++ rosen/test/render_service/render_service/fuzztest/rsrenderserviceconnection000_fuzzer/rsrenderserviceconnection000_fuzzer.cpp
@@ -27,6 +27,7 @@
 
 #include "pipeline/main_thread/rs_main_thread.h"
 #include "pipeline/main_thread/rs_render_service_connection.h"
+#include "pipeline/rs_render_node_gc.h"
 #include "pipeline/rs_surface_buffer_callback_manager.h"
 #include "platform/ohos/rs_irender_service.h"
 #include "transaction/rs_render_service_connection_stub.h"
@@ -42,6 +43,7 @@ DECLARE_INTERFACE_DESCRIPTOR(u"ohos.rosen.RenderServiceConnection");
 
 int32_t g_pid;
 sptr<OHOS::Rosen::RSScreenManager> screenManagerPtr_ = nullptr;
+[[maybe_unused]] auto& rsRenderNodeGC = RSRenderNodeGC::Instance();
 [[maybe_unused]] auto& RSSurfaceBufferCallbackManager = RSSurfaceBufferCallbackManager::Instance();
 RSMainThread* mainThread_ = RSMainThread::Instance();
 sptr<RSRenderServiceConnectionStub> connectionStub_ = nullptr;
