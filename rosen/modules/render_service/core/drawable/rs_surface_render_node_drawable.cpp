6d0ced0829d5163c0f7bd4edad7cf9f7fabf5201
YouZijun97 <yemingwei1@huawei.com>
2025-08-18 01:49:54 +0000

    del derNodeDrawable::GetProcessedNodeCount();
    Signed-off-by: YouZijun97 <yemingwei1@huawei.com>

--- rosen/modules/render_service/core/drawable/rs_surface_render_node_drawable.cpp
+++ rosen/modules/render_service/core/drawable/rs_surface_render_node_drawable.cpp
@@ -846,7 +846,6 @@ void RSSurfaceRenderNodeDrawable::OnDraw(Drawing::Canvas& canvas)
         if (!(surfaceParams->GetNeedOffscreen() && RotateOffScreenParam::GetRotateOffScreenSurfaceNodeEnable())) {
             curCanvas_->PopDirtyRegion();
         }
-        int processedNodes = RSRenderNodeDrawable::GetProcessedNodeCount();
         RS_TRACE_NAME_FMT("RSUniRenderThread::Render() the number of total ProcessedNodes: %d",
             RSRenderNodeDrawable::GetTotalProcessedNodeCount());
         const RSNodeStatsType nodeStats = CreateRSNodeStatsItem(
