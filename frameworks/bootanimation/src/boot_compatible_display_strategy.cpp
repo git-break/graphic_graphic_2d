5096bfd1880c053533795a1cf8e8997682a530d0
wangpaijun <wangpaijun1@huawei.com>
2025-08-25 11:21:59 +0800

    开机动画进度条通用方案适配
    Signed-off-by: wangpaijun <wangpaijun1@huawei.com>

--- frameworks/bootanimation/src/boot_compatible_display_strategy.cpp
+++ frameworks/bootanimation/src/boot_compatible_display_strategy.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+1
 #include "boot_compatible_display_strategy.h"
 
 #include "log.h"
