44cdb4d6af40677d61a6d701c50015293b4074f5
chenyus <chenyusong1@huawei.com>
2025-09-04 14:50:18 +0800

    fix bug
    Signed-off-by: chenyus <chenyusong1@huawei.com>

--- rosen/modules/render_service/core/feature/hetero_hdr/rs_hdr_manager.cpp
+++ rosen/modules/render_service/core/feature/hetero_hdr/rs_hdr_manager.cpp
@@ -42,14 +42,14 @@ RSHdrManager &RSHdrManager::Instance()
 RSHdrManager::RSHdrManager()
 {
     isHeterogComputingHdrOn_ = RSSystemProperties::GetHeterogComputingHDREnabled();
-    if (!RSHDRPatternManager::Instance().MHCDlOpen()) {
-        RS_LOGE("MHCDlOpen() failed!");
-        isHeterogComputingHdrOn_ = false;
-        return;
-    }
     if (!RSHDRPatternManager::Instance().MHCGraphPatternInit(GRAPH_NUM)) {
         RS_LOGE("MHCGraphPatternInit() failed!");
         isHeterogComputingHdrOn_ = false;
+    }
+    if (!RSHDRPatternManager::Instance().MHCDlOpen()) {
+    RS_LOGE("MHCDlOpen() failed!");
+    isHeterogComputingHdrOn_ = false;
+    return;
     }
 }
 
