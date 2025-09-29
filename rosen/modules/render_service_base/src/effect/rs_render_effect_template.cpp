c4fbb12777235314ae06dd9524156b052b448099
jimliuAtOH <liuzhenhuan3@huawei.com>
2025-09-29 09:01:07 +0800

    update: 更新文件 rs_render_effect_template.cpp 减少多余空行
    Signed-off-by: jimliuAtOH <liuzhenhuan3@huawei.com>

--- rosen/modules/render_service_base/src/effect/rs_render_effect_template.cpp
+++ rosen/modules/render_service_base/src/effect/rs_render_effect_template.cpp
@@ -150,7 +150,6 @@ void RSNGRenderEffectHelper::CalculatePropTagHashImpl(uint32_t& hash, const RRec
 {
     hash = hashFunc_(&value.radius_, RRect::RECT_SIZE, hash);
     hash = hashFunc_(&value.radius_, RRect::RADIUS_SIZE, hash);
-
 }
 
 std::shared_ptr<Drawing::GEVisualEffect> RSNGRenderEffectHelper::CreateGEVisualEffect(RSNGEffectType type)
