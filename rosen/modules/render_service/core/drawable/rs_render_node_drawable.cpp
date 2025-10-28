cda7466f9d027d497cf871ea20af65f0c36ab24a
lihao990224 <huanglihao3@huawei.com>
2025-10-28 14:41:12 +0800

    1
    Signed-off-by: lihao990224 <huanglihao3@huawei.com>

--- rosen/modules/render_service/core/drawable/rs_render_node_drawable.cpp
+++ rosen/modules/render_service/core/drawable/rs_render_node_drawable.cpp
@@ -1001,8 +1001,7 @@ void RSRenderNodeDrawable::UpdateCacheSurface(Drawing::Canvas& canvas, const RSR
         std::lock_guard<std::mutex> lock(drawingCacheInfoMutex_);
         cacheUpdatedNodeMap_.emplace(params.GetId(), true);
     }
-    auto ctx = RSUniRenderThread::Instance().GetRSRenderThreadParams()->GetContext();
-    RSPerfMonitorReporter::GetInstance().EndRendergroupMonitor(startTime, nodeId_, ctx, updateTimes);
+    RSPerfMonitorReporter::GetInstance().EndRendergroupMonitor(startTime, nodeId_, updateTimes);
 }
 
 int RSRenderNodeDrawable::GetTotalProcessedNodeCount()
