5923926baa3bfafa9f0471f3fbda600d202ef8e6
yy__648 <zhoubiyun2@huawei.com>
2025-10-30 16:54:55 +0800

    update: 更新文件 hgm_context_test.cpp
    Signed-off-by: yy__648 <zhoubiyun2@huawei.com>

--- rosen/test/render_service/render_service/unittest/feature/hyper_graphic_manager/hgm_context_test.cpp
+++ rosen/test/render_service/render_service/unittest/feature/hyper_graphic_manager/hgm_context_test.cpp
@@ -1,9 +1,5 @@
 /*
-<<<<<<< 1029_hwc_context
- * Copyright (c) 2024 Huawei Device Co., Ltd.
-=======
  * Copyright (c) 2025 Huawei Device Co., Ltd.
->>>>>>> master
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
