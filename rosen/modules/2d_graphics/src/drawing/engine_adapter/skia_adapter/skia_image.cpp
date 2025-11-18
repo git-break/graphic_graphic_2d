0bf1613924ca083e5aed1e52be72e637f35b1fcc
birds_and_fish_1994 <wangleilei29@h-partners.com>
2025-11-18 16:27:39 +0800

    drawing MakeFromYUVAPixmaps添加colorsspace参数
    Signed-off-by: birds_and_fish_1994 <wangleilei29@h-partners.com>

--- rosen/modules/2d_graphics/src/drawing/engine_adapter/skia_adapter/skia_image.cpp
+++ rosen/modules/2d_graphics/src/drawing/engine_adapter/skia_adapter/skia_image.cpp
@@ -162,10 +162,10 @@ std::shared_ptr<Image> SkiaImage::MakeFromYUVAPixmaps(GPUContext& gpuContext, co
         skColorSpace = colorSpaceImpl ? colorSpaceImpl->GetColorSpace() : SkColorSpace::MakeSRGB();
     }
 #ifdef USE_M133_SKIA
-    auto skImage = SkImages::TextureFromYUVAPixmaps(grContext.get(), skYUVAPixmaps, skgpu::GrMipmapped::kNo, false,
+    auto skImage = SkImages::TextureFromYUVAPixmaps(grContext.get(), skYUVAPixmaps, skgpu::Mipmapped::kNo, false,
         skColorSpace);
 #else
-    auto skImage = SkImage::MakeFromYUVAPixmaps(grContext.get(), skYUVAPixmaps, skgpu::GrMipmapped::kNo, false,
+    auto skImage = SkImage::MakeFromYUVAPixmaps(grContext.get(), skYUVAPixmaps, skgpu::Mipmapped::kNo, false,
         skColorSpace);
 #endif
     if (skImage == nullptr) {
