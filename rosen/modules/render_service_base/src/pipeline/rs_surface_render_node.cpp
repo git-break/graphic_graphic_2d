9106ea14b8d41b06357f710a04ec7c89b97246c7
zhou_eleven <zhouguoqing12@h-partners.com>
2025-11-22 16:05:23 +0800

    开启隐私空间后，从隐私空间切换到正常桌面后，旋转卡顿卡顿丢帧
    Signed-off-by: z30059508 <zhouguoqing12@h-partners.com>

--- rosen/modules/render_service_base/src/pipeline/rs_surface_render_node.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_surface_render_node.cpp
@@ -48,6 +48,7 @@
 #include "transaction/rs_render_service_client.h"
 #include "visitor/rs_node_visitor.h"
 #include "render/rs_image_cache.h"
+#include "pipeline/rs_render_node_map.h"
 #ifndef ROSEN_CROSS_PLATFORM
 #include "metadata_helper.h"
 #include <v1_0/cm_color_space.h>
@@ -2898,6 +2899,12 @@ void RSSurfaceRenderNode::SetIsOnTheTree(bool onTree, NodeId instanceRootNodeId,
         if (parentNode && parentNode->GetFirstLevelNodeId() != INVALID_NODEID) {
             firstLevelNodeId = parentNode->GetFirstLevelNodeId();
         }
+    }
+    auto context = GetContext().lock();
+    if (context && onTree) {
+        context->GetMutableNodeMap().ObtainLauncherNodeId(
+            std::static_pointer_cast<RSSurfaceRenderNode>(shared_from_this())
+        );
     }
     if (IsSecureUIExtension() || IsUnobscuredUIExtensionNode()) {
         if (onTree) {
