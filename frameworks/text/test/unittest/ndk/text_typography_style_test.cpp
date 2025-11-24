798ff35c9e36e82d03191c72d5d6b1c5c22bf696
Lem0nC <caimengyu2@huawei.com>
2025-11-24 21:53:34 +0800

    新需求：行首标点压缩
    Signed-off-by: Lem0nC <caimengyu2@huawei.com>
    Change-Id: If68e64a70d5291bf569f061285644bc05366126f

--- frameworks/text/test/unittest/ndk/text_typography_style_test.cpp
+++ frameworks/text/test/unittest/ndk/text_typography_style_test.cpp
@@ -29,7 +29,9 @@ namespace OHOS {
 namespace {
 constexpr static float FLOAT_DATA_EPSILON = 1e-6f;
 const double DEFAULT_FONT_SIZE = 40;
+const double DEFAULT_FONT_SIZE1 = 50;
 const double DEFAULT_LAYOUT_WIDTH = 1000;
+const double DEFAULT_LAYOUT_WIDTH1 = 200;
 const double DEFAULT_DOUBLE_PALCEHOLDER = 100.0;
 const int DEFAULT_INT_TWO = 2;
 const int DEFAULT_INT_THREE = 3;
@@ -43,6 +45,8 @@ public:
         OH_Drawing_TextDecoration decoration, OH_Drawing_FontWeight fontWeight = FONT_WEIGHT_400);
     void PrepareCreateParagraphWithMulTextStyle(vector<std::string>& textVec, int maxLine,
         OH_Drawing_EllipsisModal ellipsisModal, double layoutWidth);
+    void PrepareWorkForCompressPunctuationTest(std::string& text, double layoutWidth,
+        int fontFamiliesNumber = 0, const char* fontFamilies[] = nullptr, bool isSetFontFeatureSs08 = false);
 
 protected:
     OH_Drawing_FontCollection* fontCollection_{nullptr};
@@ -89,6 +93,42 @@ void NdkTypographyStyleTest::PrepareWorkForAutoSpaceTest(std::string& text, doub
     typography2_ = OH_Drawing_CreateTypography(handler2_);
     ASSERT_NE(typography2_, nullptr);
     OH_Drawing_TypographyLayout(typography2_, DEFAULT_LAYOUT_WIDTH);
+}
+
+void NdkTypographyStyleTest::PrepareWorkForCompressPunctuationTest(std::string& text, double layoutWidth, 
+    int fontFamiliesNumber, const char* fontFamilies[], bool isSetFontFeatureSs08)
+{
+    PrepareWorkForTypographyStyleTest();
+    // paragraph1 with compressPunctuation
+    typoStyle_ = OH_Drawing_CreateTypographyStyle();
+    ASSERT_NE(typoStyle_, nullptr);
+    OH_Drawing_SetTypographyStyleAttributeBool(typoStyle_, TYPOGRAPHY_STYLE_ATTR_B_COMPRESS_HEAD_PUNCTUATION , true);
+    handler_ = OH_Drawing_CreateTypographyHandler(typoStyle_, fontCollection_);
+    ASSERT_NE(handler_, nullptr);
+    OH_Drawing_SetTextStyleFontSize(txtStyle_, DEFAULT_FONT_SIZE1);
+    if (fontFamilies != nullptr) {
+        OH_Drawing_SetTextStyleFontFamilies(txtStyle_, fontFamiliesNumber, fontFamilies);
+    }
+    if (isSetFontFeatureSs08) {
+        OH_Drawing_TextStyleAddFontFeature(txtStyle_, "ss08", 1);
+    }
+    OH_Drawing_TypographyHandlerPushTextStyle(handler_, txtStyle_);
+    OH_Drawing_TypographyHandlerAddText(handler_, text.c_str());
+    typography_ = OH_Drawing_CreateTypography(handler_);
+    ASSERT_NE(typography_, nullptr);
+    OH_Drawing_TypographyLayout(typography_, layoutWidth);
+
+    // paragraph2 without compressPunctuation
+    typoStyle2_ = OH_Drawing_CreateTypographyStyle();
+    ASSERT_NE(typoStyle2_, nullptr);
+    OH_Drawing_SetTypographyStyleAttributeBool(typoStyle2_, TYPOGRAPHY_STYLE_ATTR_B_COMPRESS_HEAD_PUNCTUATION , false);
+    handler2_ = OH_Drawing_CreateTypographyHandler(typoStyle2_, fontCollection_);
+    ASSERT_NE(handler2_, nullptr);
+    OH_Drawing_TypographyHandlerPushTextStyle(handler2_, txtStyle_);
+    OH_Drawing_TypographyHandlerAddText(handler2_, text.c_str());
+    typography2_ = OH_Drawing_CreateTypography(handler2_);
+    ASSERT_NE(typography2_, nullptr);
+    OH_Drawing_TypographyLayout(typography2_, layoutWidth);
 }
 
 void NdkTypographyStyleTest::TearDown()
@@ -229,6 +269,252 @@ void NdkTypographyStyleTest::PrepareCreateParagraphWithMulTextStyle(vector<std::
     typography_ = OH_Drawing_CreateTypography(handler_);
     ASSERT_NE(typography_, nullptr);
     OH_Drawing_TypographyLayout(typography_, layoutWidth);
+}
+
+/*
+ * @tc.name: TypographyCompressPunctuation001
+ * @tc.desc: test for compress punctuation:《【『「『（〈【〖〔［｛
+ * @tc.type: FUNC
+ */
+HWTEST_F(NdkTypographyStyleTest, TypographyCompressPunctuation001, TestSize.Level0)
+{
+    // Prepare Paragraph 1 and Paragraph 2, and turn on and turn off compressPunctuation respectively.
+    std::string text = "《围城【测试『你好「哈哈（可以〈很好【丰盛〖盛打〔给你［极速｛自己";
+    PrepareWorkForCompressPunctuationTest(text, DEFAULT_LAYOUT_WIDTH1);
+    constexpr double lineWidthComp1 = 124.999878;
+    constexpr double lineWidthComp2 = 124.999847;
+    constexpr double lineWidthComp3 = 124.999908;
+    constexpr double lineWidthComp4 = 124.999969;
+    constexpr size_t lineNumber = 11;
+
+    double lineWidthsTrue[lineNumber] = { lineWidthComp1, lineWidthComp1, lineWidthComp2, lineWidthComp2, lineWidthComp2,
+        lineWidthComp2, lineWidthComp3, lineWidthComp4, lineWidthComp4, lineWidthComp4, lineWidthComp4 };
+    size_t lineNumberTrue = OH_Drawing_TypographyGetLineCount(typography_);
+    size_t lineNumberFalse = OH_Drawing_TypographyGetLineCount(typography2_);
+    EXPECT_EQ(lineNumberTrue, lineNumberFalse);
+    EXPECT_EQ(lineNumberTrue, lineNumber);
+    for (int i = 0; i < lineNumber; i++) {
+        double lineTrue = OH_Drawing_TypographyGetLineWidth(typography_, i);
+        EXPECT_NEAR(lineTrue, lineWidthsTrue[i], FLOAT_DATA_EPSILON);
+        double lineFalse = OH_Drawing_TypographyGetLineWidth(typography2_, i);
+        EXPECT_LT(lineTrue, lineFalse);
+    }
+    double longeslineTrue = OH_Drawing_TypographyGetLongestLine(typography_);
+    EXPECT_NEAR(longeslineTrue, lineWidthComp4, FLOAT_DATA_EPSILON);
+    double longeslineFalse = OH_Drawing_TypographyGetLongestLine(typography2_);
+    EXPECT_LT(longeslineTrue, longeslineFalse);
+}
+
+/*
+ * @tc.name: TypographyCompressPunctuation002
+ * @tc.desc: test for compress punctuation:“'
+ * @tc.type: FUNC
+ */
+HWTEST_F(NdkTypographyStyleTest, TypographyCompressPunctuation002, TestSize.Level0)
+{
+    // Prepare Paragraph 1 and Paragraph 2, and turn on and turn off compressPunctuation respectively.
+    std::string text = "‘好的 “标签";
+    PrepareWorkForCompressPunctuationTest(text, DEFAULT_LAYOUT_WIDTH1);
+    constexpr double lineWidthComp1 = 124.999878;
+    constexpr double lineWidthComp2 = 112.049896;
+    constexpr size_t lineNumber = 2;
+
+    double lineWidthsFalse[lineNumber] = { lineWidthComp1, lineWidthComp2 };
+    size_t lineNumberTrue = OH_Drawing_TypographyGetLineCount(typography_);
+    size_t lineNumberFalse = OH_Drawing_TypographyGetLineCount(typography2_);
+    EXPECT_EQ(lineNumberTrue, lineNumberFalse);
+    EXPECT_EQ(lineNumberTrue, lineNumber);
+    for (int i = 0; i < lineNumber; i++) {
+        double lineTrue = OH_Drawing_TypographyGetLineWidth(typography_, i);
+        EXPECT_NEAR(lineTrue, lineWidthsFalse[i], FLOAT_DATA_EPSILON);
+        double lineFalse = OH_Drawing_TypographyGetLineWidth(typography2_, i);
+        EXPECT_NEAR(lineFalse, lineWidthsFalse[i], FLOAT_DATA_EPSILON);
+        EXPECT_NEAR(lineTrue, lineFalse, FLOAT_DATA_EPSILON);
+    }
+    double longeslineTrue = OH_Drawing_TypographyGetLongestLine(typography_);
+    double longeslineFalse = OH_Drawing_TypographyGetLongestLine(typography2_);
+    EXPECT_NEAR(longeslineTrue, longeslineFalse, FLOAT_DATA_EPSILON);
+}
+
+/*
+ * @tc.name: TypographyCompressPunctuation003
+ * @tc.desc: test for compress punctuation with hard break
+ * @tc.type: FUNC
+ */
+HWTEST_F(NdkTypographyStyleTest, TypographyCompressPunctuation003, TestSize.Level0)
+{
+    // Prepare Paragraph 1 and Paragraph 2, and turn on and turn off compressPunctuation respectively.
+    std::string text = "《围城\n【测试\n『你好\n「哈哈\n（可以\n〈很好\n【丰盛\n〖盛打\n〔给你\n［极速\n｛自己";
+    PrepareWorkForCompressPunctuationTest(text, DEFAULT_LAYOUT_WIDTH1);
+    constexpr size_t lineNumber = 11;
+
+    for (int i = 0; i < lineNumber; i++) {
+        double lineTrue = OH_Drawing_TypographyGetLineWidth(typography_, i);
+        double lineFalse = OH_Drawing_TypographyGetLineWidth(typography2_, i);
+        EXPECT_LT(lineTrue, lineFalse);
+    }
+    double longeslineTrue = OH_Drawing_TypographyGetLongestLine(typography_);
+    double longeslineFalse = OH_Drawing_TypographyGetLongestLine(typography2_);
+    EXPECT_LT(longeslineTrue, longeslineFalse);
+}
+
+/*
+ * @tc.name: TypographyCompressPunctuation004
+ * @tc.desc: test for compress punctuation with font Family name:“'
+ * @tc.type: FUNC
+ */
+HWTEST_F(NdkTypographyStyleTest, TypographyCompressPunctuation004, TestSize.Level0)
+{
+    // Prepare Paragraph 1 and Paragraph 2, and turn on and turn off compressPunctuation respectively.
+    std::string text = "‘好的 “标签";
+    // Binary representation of the Chinese font library in the system font for testing
+    const char fontFamiliesTest[] = { 0x48, 0x61, 0x72, 0x6d, 0x6f, 0x6e, 0x79, 0x4f, 0x53, 0x20, 0x53, 0x61, 0x6e,
+        0x73, 0x20, 0x53, 0x43};
+    const char* fontFamilies[] = { fontFamiliesTest };
+    PrepareWorkForCompressPunctuationTest(text, DEFAULT_LAYOUT_WIDTH1, 1, fontFamilies);
+    constexpr double lineWidthComp1 = 124.999878;
+    constexpr double lineWidthComp2 = 112.049896;
+    constexpr size_t lineNumber = 2;
+
+    double lineWidthsFalse[lineNumber] = { lineWidthComp1, lineWidthComp2 };
+    for (int i = 0; i < lineNumber; i++) {
+        double lineTrue = OH_Drawing_TypographyGetLineWidth(typography_, i);
+        EXPECT_NEAR(lineTrue, lineWidthsFalse[i], FLOAT_DATA_EPSILON);
+        double lineFalse = OH_Drawing_TypographyGetLineWidth(typography2_, i);
+        EXPECT_NEAR(lineFalse, lineWidthsFalse[i], FLOAT_DATA_EPSILON);
+        EXPECT_NEAR(lineTrue, lineFalse, FLOAT_DATA_EPSILON);
+    }
+    double longeslineTrue = OH_Drawing_TypographyGetLongestLine(typography_);
+    double longeslineFalse = OH_Drawing_TypographyGetLongestLine(typography2_);
+    EXPECT_NEAR(longeslineTrue, longeslineFalse, FLOAT_DATA_EPSILON);
+}
+
+/*
+ * @tc.name: TypographyCompressPunctuation005
+ * @tc.desc: test for compress punctuation when setting font feature ss08.
+ * ss08 can compress all head punctuation marks.
+ * @tc.type: FUNC
+ */
+HWTEST_F(NdkTypographyStyleTest, TypographyCompressPunctuation005, TestSize.Level0)
+{
+    // Prepare Paragraph 1 and Paragraph 2, and turn on and turn off compressPunctuation respectively.
+    std::string text = "《围城\n【测试\n『你好\n「哈哈\n（可以\n〈很好\n【丰盛\n〖盛打\n〔给你\n［极速\n｛自己";
+    // Binary representation of the Chinese font library in the system font for testing
+    const char fontFamiliesTest[] = { 0x48, 0x61, 0x72, 0x6d, 0x6f, 0x6e, 0x79, 0x4f, 0x53, 0x20, 0x53, 0x61, 0x6e,
+        0x73, 0x20, 0x53, 0x43};
+    const char* fontFamilies[] = { fontFamiliesTest };
+    PrepareWorkForCompressPunctuationTest(text, DEFAULT_LAYOUT_WIDTH1, 1, fontFamilies, true);
+    constexpr size_t lineNumber = 11;
+
+    for (int i = 0; i < lineNumber; i++) {
+        double lineTrue = OH_Drawing_TypographyGetLineWidth(typography_, i);
+        double lineFalse = OH_Drawing_TypographyGetLineWidth(typography2_, i);
+        EXPECT_NEAR(lineTrue, lineFalse, FLOAT_DATA_EPSILON);
+    }
+    double longeslineTrue = OH_Drawing_TypographyGetLongestLine(typography_);
+    double longeslineFalse = OH_Drawing_TypographyGetLongestLine(typography2_);
+    EXPECT_NEAR(longeslineTrue, longeslineFalse, FLOAT_DATA_EPSILON);
+}
+
+/*
+ * @tc.name: TypographyCompressPunctuation006
+ * @tc.desc: test for compress punctuation when setting font feature ss08.
+ * ss08 cannot compress all head punctuation marks.
+ * @tc.type: FUNC
+ */
+HWTEST_F(NdkTypographyStyleTest, TypographyCompressPunctuation006, TestSize.Level0)
+{
+    // Prepare Paragraph 1 and Paragraph 2, and turn on and turn off compressPunctuation respectively.
+    std::string text = "《测《试《测《试";
+    // Binary representation of the Chinese font library in the system font for testing
+    const char fontFamiliesTest[] = { 0x48, 0x61, 0x72, 0x6d, 0x6f, 0x6e, 0x79, 0x4f, 0x53, 0x20, 0x53, 0x61, 0x6e,
+        0x73, 0x20, 0x53, 0x43};
+    const char* fontFamilies[] = { fontFamiliesTest };
+    constexpr double lineWidthComp1 = 174.999832;
+    constexpr double lineWidthComp2 = 199.999802;
+    PrepareWorkForCompressPunctuationTest(text, DEFAULT_LAYOUT_WIDTH1, 1, fontFamilies, true);
+
+    double lineTrue = OH_Drawing_TypographyGetLineWidth(typography_, 0);
+    double lineFalse = OH_Drawing_TypographyGetLineWidth(typography2_, 0);
+    EXPECT_NEAR(lineTrue, lineFalse, FLOAT_DATA_EPSILON);
+    EXPECT_NEAR(lineTrue, lineWidthComp1, FLOAT_DATA_EPSILON);
+    double line1True = OH_Drawing_TypographyGetLineWidth(typography_, 1);
+    double line1False = OH_Drawing_TypographyGetLineWidth(typography2_, 1);
+    EXPECT_LT(line1True, line1False);
+    EXPECT_NEAR(line1True, lineWidthComp1, FLOAT_DATA_EPSILON);
+    EXPECT_NEAR(line1False, lineWidthComp2, FLOAT_DATA_EPSILON);
+    double longeslineTrue = OH_Drawing_TypographyGetLongestLine(typography_);
+    double longeslineFalse = OH_Drawing_TypographyGetLongestLine(typography2_);
+    EXPECT_LT(longeslineTrue, longeslineFalse);
+}
+
+/*
+ * @tc.name: TypographyCompressPunctuation007
+ * @tc.desc: test for compress punctuation when text consists entirely of punctuation and ss08 is on.
+ * ss08 cannot compress all head punctuation marks.
+ * @tc.type: FUNC
+ */
+HWTEST_F(NdkTypographyStyleTest, TypographyCompressPunctuation007, TestSize.Level0)
+{
+    // Prepare Paragraph 1 and Paragraph 2, and turn on and turn off compressPunctuation respectively.
+    std::string text = "《《《《《《《《》》》》》》》》";
+    // Binary representation of the Chinese font library in the system font for testing
+    const char fontFamiliesTest[] = { 0x48, 0x61, 0x72, 0x6d, 0x6f, 0x6e, 0x79, 0x4f, 0x53, 0x20, 0x53, 0x61, 0x6e,
+        0x73, 0x20, 0x53, 0x43};
+    const char* fontFamilies[] = { fontFamiliesTest };
+    constexpr double lineWidthComp = 199.999756;
+    constexpr size_t lineNumber = 2;
+
+    PrepareWorkForCompressPunctuationTest(text, DEFAULT_LAYOUT_WIDTH1, 1, fontFamilies, true);
+    size_t lineNumberTrue = OH_Drawing_TypographyGetLineCount(typography_);
+    size_t lineNumberFalse = OH_Drawing_TypographyGetLineCount(typography2_);
+    EXPECT_EQ(lineNumberTrue, lineNumberFalse);
+    EXPECT_EQ(lineNumberTrue, lineNumber);
+
+    double lineTrue = OH_Drawing_TypographyGetLineWidth(typography_, 0);
+    double lineFalse = OH_Drawing_TypographyGetLineWidth(typography2_, 0);
+    EXPECT_NEAR(lineTrue, lineFalse, FLOAT_DATA_EPSILON);
+    EXPECT_NEAR(lineTrue, lineWidthComp, FLOAT_DATA_EPSILON);
+    double line1True = OH_Drawing_TypographyGetLineWidth(typography_, 1);
+    double line1False = OH_Drawing_TypographyGetLineWidth(typography2_, 1);
+    EXPECT_NEAR(line1True, line1False, FLOAT_DATA_EPSILON);
+    EXPECT_NEAR(line1False, lineWidthComp, FLOAT_DATA_EPSILON);
+    double longeslineTrue = OH_Drawing_TypographyGetLongestLine(typography_);
+    double longeslineFalse = OH_Drawing_TypographyGetLongestLine(typography2_);
+    EXPECT_NEAR(longeslineTrue, longeslineFalse, FLOAT_DATA_EPSILON);
+}
+
+/*
+ * @tc.name: TypographyCompressPunctuation008
+ * @tc.desc: test for compress punctuation when text consists entirely of punctuation and ss08 is off.
+ * ss08 cannot compress all head punctuation marks.
+ * @tc.type: FUNC
+ */
+HWTEST_F(NdkTypographyStyleTest, TypographyCompressPunctuation008, TestSize.Level0)
+{
+    // Prepare Paragraph 1 and Paragraph 2, and turn on and turn off compressPunctuation respectively.
+    std::string text = "《《《《》》》》";
+    // Binary representation of the Chinese font library in the system font for testing
+    const char fontFamiliesTest[] = { 0x48, 0x61, 0x72, 0x6d, 0x6f, 0x6e, 0x79, 0x4f, 0x53, 0x20, 0x53, 0x61, 0x6e,
+        0x73, 0x20, 0x53, 0x43};
+    const char* fontFamilies[] = { fontFamiliesTest };
+    constexpr size_t lineNumber = 2;
+
+    PrepareWorkForCompressPunctuationTest(text, DEFAULT_LAYOUT_WIDTH1, 1, fontFamilies, true);
+    size_t lineNumberTrue = OH_Drawing_TypographyGetLineCount(typography_);
+    size_t lineNumberFalse = OH_Drawing_TypographyGetLineCount(typography2_);
+    EXPECT_EQ(lineNumberTrue, lineNumberFalse);
+    EXPECT_EQ(lineNumberTrue, lineNumber);
+
+    double lineTrue = OH_Drawing_TypographyGetLineWidth(typography_, 0);
+    double lineFalse = OH_Drawing_TypographyGetLineWidth(typography2_, 0);
+    EXPECT_LT(lineTrue, lineFalse);
+    double line1True = OH_Drawing_TypographyGetLineWidth(typography_, 1);
+    double line1False = OH_Drawing_TypographyGetLineWidth(typography2_, 1);
+    EXPECT_NEAR(line1True, line1False, FLOAT_DATA_EPSILON);
+    double longeslineTrue = OH_Drawing_TypographyGetLongestLine(typography_);
+    double longeslineFalse = OH_Drawing_TypographyGetLongestLine(typography2_);
+    EXPECT_NEAR(longeslineTrue, longeslineFalse, FLOAT_DATA_EPSILON);
 }
 
 /*
