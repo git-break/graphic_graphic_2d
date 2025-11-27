2050e1c9bb6445dda8272d41f2736e8f43c49bc6
guanweixiang <guanweixiang1@h-partners.com>
2025-11-27 15:09:11 +0800

    remove IsReportRsHwcDead
    Signed-off-by: guanweixiang <guanweixiang1@h-partners.com>

--- rosen/modules/render_service/core/screen_manager/rs_screen_manager.cpp
+++ rosen/modules/render_service/core/screen_manager/rs_screen_manager.cpp
@@ -456,6 +456,7 @@ void RSScreenManager::OnHwcDeadEvent()
         std::lock_guard<std::mutex> lock(screenMapMutex_);
         for (auto it = screens_.begin(); it != screens_.end();) {
             if (it->second && !it->second->IsVirtual()) {
+                TriggerCallbacks(it->first, ScreenEvent::DISCONNECTED, ScreenChangeReason::HWCDEAD);
                 auto node = screens_.extract(it++);
                 screens.insert(std::move(node));
             } else {
@@ -586,8 +587,10 @@ void RSScreenManager::ProcessPendingConnections()
     for (auto id : pendingConnectedIds) {
         if (!isHwcDead_) {
             NotifyScreenNodeChange(id, true);
+            TriggerCallbacks(id, ScreenEvent::CONNECTED);
+        } else {
+            TriggerCallbacks(id, ScreenEvent::CONNECTED, ScreenChangeReason::HWCDEAD);
         }
-        TriggerCallbacks(id, ScreenEvent::CONNECTED, isHwcDead_ ? ScreenChangeReason::HWCDEAD : ScreenChangeReason::DEFAULT);
         auto screen = GetScreen(id);
         if (screen == nullptr) {
             continue;
