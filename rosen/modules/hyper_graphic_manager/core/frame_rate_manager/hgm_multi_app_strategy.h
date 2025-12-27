8bd5ea36d53a690a47f6cd44513a956e5d7d7b51
zhaozhuoyi2 <zhaozhuoyi2@huawei.com>
2025-12-27 11:40:48 +0800

    update: 更新文件 hgm_multi_app_strategy.h
    Signed-off-by: zhaozhuoyi2 <zhaozhuoyi2@huawei.com>

--- rosen/modules/hyper_graphic_manager/core/frame_rate_manager/hgm_multi_app_strategy.h
+++ rosen/modules/hyper_graphic_manager/core/frame_rate_manager/hgm_multi_app_strategy.h
@@ -86,6 +86,7 @@ public:
     static std::tuple<std::string, pid_t, int32_t> AnalyzePkgParam(const std::string& param);
 
     void SetDisableSafeVoteValue(bool disableSafeVote) { disableSafeVote_ = disableSafeVote; }
+    pid_t GetSceneBoardPid() const { return sceneBoardPid_; }
 private:
     void UseStrategyNum();
     void FollowFocus();
@@ -121,6 +122,7 @@ private:
     PolicyConfigData::ScreenSetting& screenSettingCache_;
     PolicyConfigData::StrategyConfigMap& strategyConfigMapCache_;
     bool disableSafeVote_ = false;
+    pid_t sceneBoardPid_ = DEFAULT_PID;
 };
 } // namespace Rosen
 } // namespace OHOS
