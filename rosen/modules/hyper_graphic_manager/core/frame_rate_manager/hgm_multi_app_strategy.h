c4700b691e9bacdb9542949f7a6cdfa9a7abcd19
zhaozhuoyi2 <zhaozhuoyi2@huawei.com>
2025-12-27 17:15:11 +0800

    update: 更新文件 hgm_multi_app_strategy.h
    Signed-off-by: zhaozhuoyi2 <zhaozhuoyi2@huawei.com>

--- rosen/modules/hyper_graphic_manager/core/frame_rate_manager/hgm_multi_app_strategy.h
+++ rosen/modules/hyper_graphic_manager/core/frame_rate_manager/hgm_multi_app_strategy.h
@@ -86,7 +86,9 @@ public:
     static std::tuple<std::string, pid_t, int32_t> AnalyzePkgParam(const std::string& param);
 
     void SetDisableSafeVoteValue(bool disableSafeVote) { disableSafeVote_ = disableSafeVote; }
+
     pid_t GetSceneBoardPid() const { return sceneBoardPid_; }
+
 private:
     void UseStrategyNum();
     void FollowFocus();
