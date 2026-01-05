831de1b17115916a7a1e3c5552d09ab64d5a79d3
jianghongxi <jianghongxi3@h-partners.com>
2026-01-05 10:34:26 +0800

    car 2 tdd compile
    Signed-off-by: jianghongxi <jianghongxi3@h-partners.com>

--- graphic_test/graphic_test/rs_display_effect/animation/animation_protocol_test.cpp
+++ graphic_test/graphic_test/rs_display_effect/animation/animation_protocol_test.cpp
@@ -46,8 +46,7 @@ static std::shared_ptr<RSUIContext> GetRSUIContext()
 {
     static std::shared_ptr<RSUIContext> rsUIContext = nullptr;
     if (rsUIContext == nullptr) {
-        sptr<IRemoteObject> remoteObject = nullptr;
-        rsUIContext = RSUIContextManager::MutableInstance().CreateRSUIContext(remoteObject);
+        rsUIContext = RSUIContextManager::MutableInstance().CreateRSUIContext();
     }
     return rsUIContext;
 }
