ba931e58187d374e94f19869ee2815d78743b61c
鲁保良 <lubaoliang1@h-partners.com>
2026-01-12 11:10:51 +0800

    添加日志
    Signed-off-by: 鲁保良 <lubaoliang1@h-partners.com>

--- rosen/modules/render_service_client/core/modifier/rs_modifier_manager.cpp
+++ rosen/modules/render_service_client/core/modifier/rs_modifier_manager.cpp
@@ -59,6 +59,7 @@ void RSModifierManager::AddAnimation(const std::shared_ptr<RSRenderAnimation>& a
         return;
     }
     animations_.emplace(key, animation);
+    RS_OPTIONAL_TRACE_NAME_FMT("AddAnimation set hasFirstFrameAnimation");
     hasFirstFrameAnimation_ = true;
 
     std::shared_ptr<RSRenderDisplaySync> displaySync = std::make_shared<RSRenderDisplaySync>(animation);
