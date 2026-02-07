bc0dca895436b326ff03b3532df119d50e7bc7da
Tianer Zhou <zhoutianer@huawei.com>
2026-02-07 11:39:01 +0800

    remove wrong test
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>
    Change-Id: Ic5b7e65c2f2d3d5dcb0a0b3c4567b67bb8d77f9d

--- rosen/test/render_service/render_service_base/unittest/pipeline/rs_render_node_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/pipeline/rs_render_node_test.cpp
@@ -3998,31 +3998,11 @@ HWTEST_F(RSRenderNodeTest, GetColorPickerDrawable001, TestSize.Level1)
 
 /**
  * @tc.name: GetColorPickerDrawable002
- * @tc.desc: Test GetColorPickerDrawable returns nullptr when drawable is not ColorPicker type
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(RSRenderNodeTest, GetColorPickerDrawable002, TestSize.Level1)
-{
-    auto node = std::make_shared<RSRenderNode>(1);
-    ASSERT_NE(node, nullptr);
-
-    // Set a non-ColorPicker drawable at the COLOR_PICKER slot
-    auto filterDrawable = std::make_shared<DrawableV2::RSFilterDrawable>();
-    ASSERT_NE(filterDrawable, nullptr);
-    node->GetDrawableVec(__func__).at(static_cast<int8_t>(RSDrawableSlot::COLOR_PICKER)) = filterDrawable;
-
-    auto colorPickerDrawable = node->GetColorPickerDrawable();
-    EXPECT_EQ(colorPickerDrawable, nullptr);
-}
-
-/**
- * @tc.name: GetColorPickerDrawable003
  * @tc.desc: Test GetColorPickerDrawable successfully retrieves ColorPicker drawable
  * @tc.type: FUNC
  * @tc.require:
  */
-HWTEST_F(RSRenderNodeTest, GetColorPickerDrawable003, TestSize.Level1)
+HWTEST_F(RSRenderNodeTest, GetColorPickerDrawable002, TestSize.Level1)
 {
     RSRenderNode node(1);
 
