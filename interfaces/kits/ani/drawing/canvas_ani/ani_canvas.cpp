21a1f21a2948eee328881c705f789fa0e6d7eae5
weixin_48719330 <zhushushu@huawei.com>
2026-02-09 19:35:37 +0800

    arkts1.2
    Signed-off-by: weixin_48719330 <zhushushu@huawei.com>

--- interfaces/kits/ani/drawing/canvas_ani/ani_canvas.cpp
+++ interfaces/kits/ani/drawing/canvas_ani/ani_canvas.cpp
@@ -340,6 +340,7 @@ static const std::array g_methods = {
     ani_native_function { "resetMatrix", nullptr, reinterpret_cast<void*>(AniCanvas::ResetMatrix) },
     ani_native_function { "quickRejectPath", nullptr, reinterpret_cast<void*>(AniCanvas::QuickRejectPath) },
     ani_native_function { "quickRejectRect", nullptr, reinterpret_cast<void*>(AniCanvas::QuickRejectRect) },
+    ani_native_function { "drawSingleCharacterWithFeatures", nullptr, reinterpret_cast<void*>(AniCanvas::DrawSingleCharacterWithFeatures) },
 };
 
 ani_status AniCanvas::AniInit(ani_env *env)
@@ -2153,6 +2154,67 @@ ani_boolean AniCanvas::QuickRejectPath(ani_env* env, ani_object obj, ani_object 
     Drawing::Canvas* canvas = aniCanvas->GetCanvas();
     bool result = canvas->QuickReject(*aniPath->GetPath());
     return static_cast<ani_boolean>(result);
+}
+
+void AniCanvas::DrawSingleCharacterWithFeatures(ani_env* env, ani_object obj, ani_string text, ani_object fontobj,
+    ani_double x, ani_double y, ani_array featuresobj)
+{
+    auto aniCanvas = GetNativeFromObj<AniCanvas>(env, obj, AniGlobalField::GetInstance().canvasNativeObj);
+    if (aniCanvas == nullptr || aniCanvas->GetCanvas() == nullptr) {
+        ThrowBusinessError(env, DrawingErrorCode::ERROR_INVALID_PARAM,
+            "AniCanvas::drawSingleCharacterWithFeatures canvas is nullptr.");
+        return;
+    }
+
+    auto aniFont = GetNativeFromObj<AniFont>(env, fontobj, AniGlobalField::GetInstance().fontNativeObj);
+    if (aniFont == nullptr || aniFont->GetFont() == nullptr) {
+        ThrowBusinessError(env, DrawingErrorCode::ERROR_INVALID_PARAM,
+            "AniCanvas::DrawSingleCharacterWithFeatures font is nullptr.");
+        return;
+    }
+
+    ani_size len = 0;
+    env->String_GetUTF8Size(text, &len);
+    if (len == 0 || len > 4) { // 4 is the maximum length of a character encoded in UTF8.
+        ThrowBusinessError(env, DrawingErrorCode::ERROR_INVALID_PARAM,
+            "AniCanvas::DrawSingleCharacterWithFeatures Parameter verification failed. text should be single character.");
+        return;
+    }
+
+    char str[len + 1];
+    ani_size realLen = 0;
+    env->String_GetUTF8(text, str, len + 1, &realLen);
+    str[realLen] = '\0';
+    const char* currentStr = str;
+    int32_t unicode = SkUTF::NextUTF8(&currentStr, currentStr + len);
+
+    std::shared_ptr<Font> font = aniFont->GetFont();
+    std::shared_ptr<Font> themeFont = MatchThemeFont(font, unicode);
+    if (themeFont != nullptr) {
+        font = themeFont;
+    }
+    size_t byteLen = currentStr - str;
+    if (byteLen != len) {
+        ThrowBusinessError(env, DrawingErrorCode::ERROR_INVALID_PARAM,
+            "AniCanvas::DrawSingleCharacterWithFeatures Parameter verification failed. text should be single character.");
+        return;
+    }
+    ani_size aniLength;
+    if (ANI_OK != env->Array_GetLength(featuresobj, &aniLength)) {
+        ROSEN_LOGE("AniCanvas::DrawSingleCharacterWithFeatures points are invalid");
+        ThrowBusinessError(env, DrawingErrorCode::ERROR_INVALID_PARAM, "AniCanvas::DrawSingleCharacterWithFeatures incorrect type points.");
+        return;
+    }
+    uint32_t size = static_cast<uint32_t>(aniLength);
+
+    std::shared_ptr<Drawing::DrawingFontFeatures> drawingFontFeatures = std::make_shared<Drawing::DrawingFontFeatures>();
+    if(!MakeFontFeaturesFromAniObjArray(env, drawingFontFeatures, size, featuresobj)) {
+        ROSEN_LOGE("AniCanvas::drawSingleCharacterWithFeatures MakeFontFeaturesFromAniObjArray is fail");
+        return;
+    }
+
+    aniCanvas->GetCanvas()->DrawSingleCharacterWithFeatures(str, *font, x, y, drawingFontFeatures);
+    aniCanvas->NotifyDirty();
 }
 
 ani_boolean AniCanvas::QuickRejectRect(ani_env* env, ani_object obj, ani_object rectObj)
