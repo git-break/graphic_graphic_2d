42ec8b13a04c5422b20079c67eec413ddbabf5c4
gcw_yxxmv6uE <guojunfu@huawei.com>
2026-02-13 15:27:35 +0800

    update: 更新文件 filter.h
    Signed-off-by: gcw_yxxmv6uE <guojunfu@huawei.com>

--- rosen/modules/effect/effect_ndk/src/filter/filter.h
+++ rosen/modules/effect/effect_ndk/src/filter/filter.h
@@ -46,11 +46,10 @@ public:
         const std::shared_ptr<Drawing::GEWaterDropletTransitionFilterParams>& geWaterDropletParams);
     bool WaterGlass(const std::shared_ptr<Drawing::GEWaterGlassDataParams>& params);
     bool ReededGlass(const std::shared_ptr<Drawing::GEReededGlassDataParams>& params);
-    bool RenderResult(bool forceCPU, OH_NativeBuffer* dstNativeBuffer);
+    bool RenderNativeBuffer(bool forceCPU, OH_NativeBuffer* dstNativeBuffer);
 private:
     void AddNextFilter(std::shared_ptr<EffectImageFilter> filter);
     bool Render(bool forceCPU);
-    bool Render(bool forceCPU, OH_NativeBuffer* dstNativeBuffer);
     std::vector<std::shared_ptr<EffectImageFilter>> effectFilters_;
     std::shared_ptr<OHOS::Media::PixelMap> srcPixelMap_ = nullptr;
     std::shared_ptr<OHOS::Media::PixelMap> dstPixelMap_ = nullptr;
