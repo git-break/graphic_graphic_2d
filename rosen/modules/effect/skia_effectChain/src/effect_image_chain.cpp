de25a3f9b8c7e48581d990008407deb28fb05a99
gcw_yxxmv6uE <guojunfu@huawei.com>
2026-02-13 15:33:51 +0800

    update: 更新文件 effect_image_chain.cpp
    Signed-off-by: gcw_yxxmv6uE <guojunfu@huawei.com>

--- rosen/modules/effect/skia_effectChain/src/effect_image_chain.cpp
+++ rosen/modules/effect/skia_effectChain/src/effect_image_chain.cpp
@@ -129,8 +129,6 @@ DrawingError EffectImageChain::Prepare(const std::shared_ptr<Media::PixelMap>& s
         return DrawingError::ERR_ILLEGAL_INPUT;
     }
     srcPixelMap_ = srcPixelMap;
-    srcWidth_ = srcPixelMap_->GetWidth();
-    srcHeight_ = srcPixelMap_->GetHeight();
 
     if (InitWithoutCanvas(srcPixelMap_) != DrawingError::ERR_OK) {
         EFFECT_LOG_E("EffectImageChain::Prepare: Failed to init.");
@@ -294,9 +292,9 @@ DrawingError EffectImageChain::ApplyEllipticalGradientBlur(float blurRadius, flo
 
     Drawing::GEVariableRadiusBlurShaderFilterParams filterParams{radialGradientShaderMask, blurRadius, true};
     auto variableRadiusBlurFilter = std::make_shared<GEVariableRadiusBlurShaderFilter>(filterParams);
-    image_ = variableRadiusBlurFilter->ProcessImage(*canvas_,	 
-        image_,	 
-        Drawing::Rect(0, 0, srcPixelMap_->GetWidth(), srcPixelMap_->GetHeight()),	 
+    image_ = variableRadiusBlurFilter->ProcessImage(*canvas_,
+        image_,
+        Drawing::Rect(0, 0, srcPixelMap_->GetWidth(), srcPixelMap_->GetHeight()),
         Drawing::Rect(0, 0, srcPixelMap_->GetWidth(), srcPixelMap_->GetHeight()));
     ROSEN_TRACE_END(HITRACE_TAG_GRAPHIC_AGP);
     return DrawingError::ERR_OK;
