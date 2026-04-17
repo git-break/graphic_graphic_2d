ee563eadca9471cd3b288dc35da814ebd6541695
lingzhengyang <lingzhengyang@h-partners.com>
2026-04-17 17:10:16 +0800

    Update demo: use 4 different mask types
    - Node 1: RippleMask
    - Node 2: RadialGradientMask
    - Node 3: WaveGradientMask
    - Node 4: DoubleRippleMask
    - All magic numbers replaced with constexpr constants
    
    Signed-off-by: lingzhengyang <lingzhengyang@h-partners.com>
    Co-Authored-By: Agent

--- rosen/modules/render_service_client/test/render_service_client_spatial_point_light_demo.cpp
+++ rosen/modules/render_service_client/test/render_service_client_spatial_point_light_demo.cpp
@@ -23,6 +23,7 @@
 #include "ui/rs_ui_context.h"
 #include "ui/rs_ui_director.h"
 #include "ui_effect/property/include/rs_ui_shader_base.h"
+#include "ui_effect/property/include/rs_ui_mask_base.h"
 #include "common/rs_vector2.h"
 #include "common/rs_vector3.h"
 #include "common/rs_vector4.h"
@@ -54,27 +55,47 @@ constexpr int NODE4_POS_Y = 700;
 
 // Light parameters
 constexpr float DEFAULT_LIGHT_INTENSITY = 1.0f;
-constexpr float RED_LIGHT_INTENSITY = 1.5f;
-constexpr float GREEN_LIGHT_INTENSITY = 2.0f;
 constexpr float DEFAULT_LIGHT_ATTENUATION = 0.5f;
-constexpr float RED_LIGHT_ATTENUATION = 0.3f;
-constexpr float GREEN_LIGHT_ATTENUATION = 0.8f;
 constexpr float LIGHT_DEPTH = 50.0f;
 
-// Light positions
-constexpr float LIGHT1_POS_X = 400.0f;
-constexpr float LIGHT1_POS_Y = 400.0f;
-constexpr float LIGHT2_POS_X = 900.0f;
-constexpr float LIGHT2_POS_Y = 400.0f;
-constexpr float LIGHT3_POS_X = 400.0f;
-constexpr float LIGHT3_POS_Y = 900.0f;
+// Light positions (centered in each node)
+constexpr float LIGHT_CENTER_OFFSET_X = 200.0f;
+constexpr float LIGHT_CENTER_OFFSET_Y = 200.0f;
 
-// BorderLight cascade parameters
-constexpr float BORDER_LIGHT_INTENSITY = 0.8f;
-constexpr float BORDER_LIGHT_WIDTH = 5.0f;
-constexpr float BORDER_LIGHT_CORNER_RADIUS = 30.0f;
-constexpr float BORDER_LIGHT_POS_X = 0.5f;
-constexpr float BORDER_LIGHT_POS_Y = 0.5f;
+// RippleMask parameters
+constexpr float RIPPLE_MASK_RADIUS = 5.0f;
+constexpr float RIPPLE_MASK_WIDTH = 5.0f;
+constexpr float RIPPLE_MASK_CENTER_X = 0.5f;
+constexpr float RIPPLE_MASK_CENTER_Y = 0.5f;
+constexpr float RIPPLE_MASK_OFFSET = 0.0f;
+
+// RadialGradientMask parameters
+constexpr float RADIAL_MASK_CENTER_X = 0.5f;
+constexpr float RADIAL_MASK_CENTER_Y = 0.5f;
+constexpr float RADIAL_MASK_RADIUS_X = 0.3f;
+constexpr float RADIAL_MASK_RADIUS_Y = 0.3f;
+constexpr float RADIAL_MASK_COLOR_START = 1.0f;
+constexpr float RADIAL_MASK_COLOR_END = 0.0f;
+constexpr float RADIAL_MASK_POSITION_START = 0.0f;
+constexpr float RADIAL_MASK_POSITION_END = 1.0f;
+
+// WaveGradientMask parameters
+constexpr float WAVE_MASK_CENTER_X = 0.5f;
+constexpr float WAVE_MASK_CENTER_Y = 0.5f;
+constexpr float WAVE_MASK_WIDTH = 0.2f;
+constexpr float WAVE_MASK_PROPAGATION_RADIUS = 0.5f;
+constexpr float WAVE_MASK_BLUR_RADIUS = 0.1f;
+constexpr float WAVE_MASK_TURBULENCE = 0.5f;
+
+// DoubleRippleMask parameters
+constexpr float DOUBLE_RIPPLE_MASK_RADIUS = 5.0f;
+constexpr float DOUBLE_RIPPLE_MASK_WIDTH = 5.0f;
+constexpr float DOUBLE_RIPPLE_MASK_CENTER1_X = 0.3f;
+constexpr float DOUBLE_RIPPLE_MASK_CENTER1_Y = 0.3f;
+constexpr float DOUBLE_RIPPLE_MASK_CENTER2_X = 0.7f;
+constexpr float DOUBLE_RIPPLE_MASK_CENTER2_Y = 0.7f;
+constexpr float DOUBLE_RIPPLE_MASK_TURBULENCE = 0.5f;
+constexpr float DOUBLE_RIPPLE_MASK_HALO_THICKNESS = 2.0f;
 
 // Animation parameters
 constexpr int ANIMATION_FRAME_COUNT = 60;
@@ -94,18 +115,6 @@ constexpr float COLOR_WHITE_R = 1.0f;
 constexpr float COLOR_WHITE_G = 1.0f;
 constexpr float COLOR_WHITE_B = 1.0f;
 constexpr float COLOR_WHITE_A = 1.0f;
-constexpr float COLOR_RED_R = 1.0f;
-constexpr float COLOR_RED_G = 0.0f;
-constexpr float COLOR_RED_B = 0.0f;
-constexpr float COLOR_RED_A = 1.0f;
-constexpr float COLOR_GREEN_R = 0.0f;
-constexpr float COLOR_GREEN_G = 1.0f;
-constexpr float COLOR_GREEN_B = 0.0f;
-constexpr float COLOR_GREEN_A = 1.0f;
-constexpr float COLOR_BORDER_GREEN_R = 0.0f;
-constexpr float COLOR_BORDER_GREEN_G = 1.0f;
-constexpr float COLOR_BORDER_GREEN_B = 0.0f;
-constexpr float COLOR_BORDER_GREEN_A = 1.0f;
 
 const std::string SURFACE_NODE_NAME = "spatial_point_light_demo";
 
@@ -120,16 +129,52 @@ std::shared_ptr<RSNGSpatialPointLight> CreateSpatialPointLightShader(
     return shader;
 }
 
-std::shared_ptr<RSNGBorderLight> CreateBorderLightShader()
+std::shared_ptr<RSNGRippleMask> CreateRippleMask()
 {
-    auto shader = std::make_shared<RSNGBorderLight>();
-    shader->Setter<BorderLightPositionTag>(Vector3f(BORDER_LIGHT_POS_X, BORDER_LIGHT_POS_Y, 0.0f));
-    shader->Setter<BorderLightColorTag>(
-        Vector4f(COLOR_BORDER_GREEN_R, COLOR_BORDER_GREEN_G, COLOR_BORDER_GREEN_B, COLOR_BORDER_GREEN_A));
-    shader->Setter<BorderLightIntensityTag>(BORDER_LIGHT_INTENSITY);
-    shader->Setter<BorderLightWidthTag>(BORDER_LIGHT_WIDTH);
-    shader->Setter<BorderLightCornerRadiusTag>(BORDER_LIGHT_CORNER_RADIUS);
-    return shader;
+    auto mask = std::make_shared<RSNGRippleMask>();
+    mask->Setter<RippleMaskCenterTag>(Vector2f(RIPPLE_MASK_CENTER_X, RIPPLE_MASK_CENTER_Y));
+    mask->Setter<RippleMaskRadiusTag>(RIPPLE_MASK_RADIUS);
+    mask->Setter<RippleMaskWidthTag>(RIPPLE_MASK_WIDTH);
+    mask->Setter<RippleMaskOffsetTag>(RIPPLE_MASK_OFFSET);
+    return mask;
+}
+
+std::shared_ptr<RSNGRadialGradientMask> CreateRadialGradientMask()
+{
+    auto mask = std::make_shared<RSNGRadialGradientMask>();
+    mask->Setter<RadialGradientMaskCenterTag>(Vector2f(RADIAL_MASK_CENTER_X, RADIAL_MASK_CENTER_Y));
+    mask->Setter<RadialGradientMaskRadiusXTag>(RADIAL_MASK_RADIUS_X);
+    mask->Setter<RadialGradientMaskRadiusYTag>(RADIAL_MASK_RADIUS_Y);
+    mask->Setter<RadialGradientMaskColorsTag>(
+        std::vector<float>{RADIAL_MASK_COLOR_START, RADIAL_MASK_COLOR_END});
+    mask->Setter<RadialGradientMaskPositionsTag>(
+        std::vector<float>{RADIAL_MASK_POSITION_START, RADIAL_MASK_POSITION_END});
+    return mask;
+}
+
+std::shared_ptr<RSNGWaveGradientMask> CreateWaveGradientMask()
+{
+    auto mask = std::make_shared<RSNGWaveGradientMask>();
+    mask->Setter<WaveGradientMaskWaveCenterTag>(Vector2f(WAVE_MASK_CENTER_X, WAVE_MASK_CENTER_Y));
+    mask->Setter<WaveGradientMaskWaveWidthTag>(WAVE_MASK_WIDTH);
+    mask->Setter<WaveGradientMaskPropagationRadiusTag>(WAVE_MASK_PROPAGATION_RADIUS);
+    mask->Setter<WaveGradientMaskBlurRadiusTag>(WAVE_MASK_BLUR_RADIUS);
+    mask->Setter<WaveGradientMaskTurbulenceStrengthTag>(WAVE_MASK_TURBULENCE);
+    return mask;
+}
+
+std::shared_ptr<RSNGDoubleRippleMask> CreateDoubleRippleMask()
+{
+    auto mask = std::make_shared<RSNGDoubleRippleMask>();
+    mask->Setter<DoubleRippleMaskCenter1Tag>(
+        Vector2f(DOUBLE_RIPPLE_MASK_CENTER1_X, DOUBLE_RIPPLE_MASK_CENTER1_Y));
+    mask->Setter<DoubleRippleMaskCenter2Tag>(
+        Vector2f(DOUBLE_RIPPLE_MASK_CENTER2_X, DOUBLE_RIPPLE_MASK_CENTER2_Y));
+    mask->Setter<DoubleRippleMaskRadiusTag>(DOUBLE_RIPPLE_MASK_RADIUS);
+    mask->Setter<DoubleRippleMaskWidthTag>(DOUBLE_RIPPLE_MASK_WIDTH);
+    mask->Setter<DoubleRippleMaskTurbulenceTag>(DOUBLE_RIPPLE_MASK_TURBULENCE);
+    mask->Setter<DoubleRippleMaskHaloThicknessTag>(DOUBLE_RIPPLE_MASK_HALO_THICKNESS);
+    return mask;
 }
 
 class SpatialPointLightDemo {
@@ -220,49 +265,55 @@ public:
             return false;
         }
 
-        // Node 1: Basic spatial point light shader (white light)
+        // Node 1: SpatialPointLight with RippleMask
         auto node1 = CreateCanvasNode(NODE1_POS_X, NODE1_POS_Y, NODE_WIDTH, NODE_HEIGHT);
         auto shader1 = CreateSpatialPointLightShader(
-            Vector3f(LIGHT1_POS_X, LIGHT1_POS_Y, LIGHT_DEPTH),
+            Vector3f(NODE1_POS_X + LIGHT_CENTER_OFFSET_X, NODE1_POS_Y + LIGHT_CENTER_OFFSET_Y, LIGHT_DEPTH),
             Vector4f(COLOR_WHITE_R, COLOR_WHITE_G, COLOR_WHITE_B, COLOR_WHITE_A),
             DEFAULT_LIGHT_INTENSITY,
             DEFAULT_LIGHT_ATTENUATION);
+        auto mask1 = std::static_pointer_cast<RSNGMaskBase>(CreateRippleMask());
+        shader1->Setter<SpatialPointLightMaskTag>(mask1);
         node1->SetBackgroundNGShader(shader1);
         canvasNodes_.push_back(node1);
         rootNode_->AddChild(node1, -1);
 
-        // Node 2: Red point light
+        // Node 2: SpatialPointLight with RadialGradientMask
         auto node2 = CreateCanvasNode(NODE2_POS_X, NODE2_POS_Y, NODE_WIDTH, NODE_HEIGHT);
         auto shader2 = CreateSpatialPointLightShader(
-            Vector3f(LIGHT2_POS_X, LIGHT2_POS_Y, LIGHT_DEPTH),
-            Vector4f(COLOR_RED_R, COLOR_RED_G, COLOR_RED_B, COLOR_RED_A),
-            RED_LIGHT_INTENSITY,
-            RED_LIGHT_ATTENUATION);
+            Vector3f(NODE2_POS_X + LIGHT_CENTER_OFFSET_X, NODE2_POS_Y + LIGHT_CENTER_OFFSET_Y, LIGHT_DEPTH),
+            Vector4f(COLOR_WHITE_R, COLOR_WHITE_G, COLOR_WHITE_B, COLOR_WHITE_A),
+            DEFAULT_LIGHT_INTENSITY,
+            DEFAULT_LIGHT_ATTENUATION);
+        auto mask2 = std::static_pointer_cast<RSNGMaskBase>(CreateRadialGradientMask());
+        shader2->Setter<SpatialPointLightMaskTag>(mask2);
         node2->SetBackgroundNGShader(shader2);
         canvasNodes_.push_back(node2);
         rootNode_->AddChild(node2, -1);
 
-        // Node 3: Green point light
+        // Node 3: SpatialPointLight with WaveGradientMask
         auto node3 = CreateCanvasNode(NODE3_POS_X, NODE3_POS_Y, NODE_WIDTH, NODE_HEIGHT);
         auto shader3 = CreateSpatialPointLightShader(
-            Vector3f(LIGHT3_POS_X, LIGHT3_POS_Y, LIGHT_DEPTH),
-            Vector4f(COLOR_GREEN_R, COLOR_GREEN_G, COLOR_GREEN_B, COLOR_GREEN_A),
-            GREEN_LIGHT_INTENSITY,
-            GREEN_LIGHT_ATTENUATION);
+            Vector3f(NODE3_POS_X + LIGHT_CENTER_OFFSET_X, NODE3_POS_Y + LIGHT_CENTER_OFFSET_Y, LIGHT_DEPTH),
+            Vector4f(COLOR_WHITE_R, COLOR_WHITE_G, COLOR_WHITE_B, COLOR_WHITE_A),
+            DEFAULT_LIGHT_INTENSITY,
+            DEFAULT_LIGHT_ATTENUATION);
+        auto mask3 = std::static_pointer_cast<RSNGMaskBase>(CreateWaveGradientMask());
+        shader3->Setter<SpatialPointLightMaskTag>(mask3);
         node3->SetBackgroundNGShader(shader3);
         canvasNodes_.push_back(node3);
         rootNode_->AddChild(node3, -1);
 
-        // Node 4: Cascade shader (SpatialPointLight + BorderLight)
+        // Node 4: SpatialPointLight with DoubleRippleMask
         auto node4 = CreateCanvasNode(NODE4_POS_X, NODE4_POS_Y, NODE_WIDTH, NODE_HEIGHT);
-        auto spatialShader = CreateSpatialPointLightShader(
-            Vector3f(LIGHT1_POS_X, LIGHT1_POS_Y, LIGHT_DEPTH),
+        auto shader4 = CreateSpatialPointLightShader(
+            Vector3f(NODE4_POS_X + LIGHT_CENTER_OFFSET_X, NODE4_POS_Y + LIGHT_CENTER_OFFSET_Y, LIGHT_DEPTH),
             Vector4f(COLOR_WHITE_R, COLOR_WHITE_G, COLOR_WHITE_B, COLOR_WHITE_A),
             DEFAULT_LIGHT_INTENSITY,
             DEFAULT_LIGHT_ATTENUATION);
-        auto borderShader = CreateBorderLightShader();
-        spatialShader->Append(borderShader);
-        node4->SetBackgroundNGShader(spatialShader);
+        auto mask4 = std::static_pointer_cast<RSNGMaskBase>(CreateDoubleRippleMask());
+        shader4->Setter<SpatialPointLightMaskTag>(mask4);
+        node4->SetBackgroundNGShader(shader4);
         canvasNodes_.push_back(node4);
         rootNode_->AddChild(node4, -1);
 
@@ -298,7 +349,7 @@ public:
         transaction_->FlushImplicitTransaction();
         rsUiDirector_->SendMessages();
         rsUiDirector_->FlushModifier();
-        std::cout << "test spatial point light success" << std::endl;
+        std::cout << "test spatial point light with masks success" << std::endl;
         sleep(WAIT_TIME_LONG);
     }
 
@@ -316,10 +367,12 @@ public:
             float intensity = ANIMATION_INTENSITY_BASE + ANIMATION_INTENSITY_RANGE * sin(phase);
 
             auto shader = CreateSpatialPointLightShader(
-                Vector3f(LIGHT1_POS_X, LIGHT1_POS_Y, LIGHT_DEPTH),
+                Vector3f(NODE1_POS_X + LIGHT_CENTER_OFFSET_X, NODE1_POS_Y + LIGHT_CENTER_OFFSET_Y, LIGHT_DEPTH),
                 Vector4f(COLOR_WHITE_R, COLOR_WHITE_G, COLOR_WHITE_B, COLOR_WHITE_A),
                 intensity,
                 DEFAULT_LIGHT_ATTENUATION);
+            auto mask = std::static_pointer_cast<RSNGMaskBase>(CreateRippleMask());
+            shader->Setter<SpatialPointLightMaskTag>(mask);
             canvasNodes_[0]->SetBackgroundNGShader(shader);
 
             transaction_->FlushImplicitTransaction();
