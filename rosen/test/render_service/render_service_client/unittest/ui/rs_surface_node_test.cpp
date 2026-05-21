ee9d2c2485060d87853c96121136e9effe64cd29
l30067243 <leipeng42@h-partners.com>
2026-05-21 11:04:31 +0800

    add tdd
    Signed-off-by: l30067243 <leipeng42@h-partners.com>

--- rosen/test/render_service/render_service_client/unittest/ui/rs_surface_node_test.cpp
+++ rosen/test/render_service/render_service_client/unittest/ui/rs_surface_node_test.cpp
@@ -1721,6 +1721,23 @@ HWTEST_F(RSSurfaceNodeTest, OnBoundsSizeChanged, TestSize.Level1)
 }
 
 /**
+ * @tc.name: OnAlphaValueChanged Test
+ * @tc.desc: OnAlphaValueChanged
+ * @tc.type: FUNC
+ * @tc.require:SR000HSUII
+ */
+HWTEST_F(RSSurfaceNodeTest, OnAlphaValueChanged, TestSize.Level1)
+{
+    RSSurfaceNodeConfig c;
+    RSSurfaceNode::SharedPtr surfaceNode = RSSurfaceNode::Create(c);
+    ASSERT_NE(surfaceNode, nullptr);
+    AlphaChangedCallback callback = [](float alpha) {};
+    surfaceNode->SetAlphaChangedCallback(callback);
+    surfaceNode->SetAlpha(0.1f);
+    surfaceNode->OnAlphaValueChanged();
+}
+
+/**
  * @tc.name: SetSurfaceIdToRenderNode Test
  * @tc.desc: SetSurfaceIdToRenderNode
  * @tc.type: FUNC
