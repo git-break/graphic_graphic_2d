1836814397ecf0909c869553b8d68a38805a8f69
lybbq <liuyang1044@huawei.com>
2026-05-29 17:35:13 +0800

    fix
    Signed-off-by: lybbq <liuyang1044@huawei.com>
    Change-Id: I4ec100ea734d622866677c9431f1227cac6e8e83

--- rosen/test/render_service/render_service/unittest/transaction/rs_client_to_service_connection_stub_test.cpp
+++ rosen/test/render_service/render_service/unittest/transaction/rs_client_to_service_connection_stub_test.cpp
@@ -66,7 +66,7 @@
 using namespace testing;
 using namespace testing::ext;
 using namespace OHOS::Security::AccessToken;
-
+#if defined(RS_ENABLE_UNI_RENDER)
 namespace {
 constexpr const int DEFAULT_WIDTH = 2160;
 constexpr const int DEFAULT_HEIGHT = 1080;
@@ -77,7 +77,6 @@ constexpr const int DELAY_TEAR_DOWN_TESE_CASE = 110;
 // constexpr const size_t PARCEL_MAX_CAPACITY = 2000 * 1024;
 
 };
-#if defined(RS_ENABLE_UNI_RENDER)
 namespace OHOS::Rosen {
 namespace {
 class MockRSBrightnessInfoChangeCallback : public IRemoteProxy<RSIBrightnessInfoChangeCallback> {
