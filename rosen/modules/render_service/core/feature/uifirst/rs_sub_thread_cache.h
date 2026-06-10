5a637099f37c023a9542f272370410675cb73782
anderxiaml <xiapengcheng4@h-partners.com>
2026-06-10 20:25:33 +0800

    上划进入多任务，动效降档（分辨率）     Co-Authored-By: Agent     Signed-off-by: anderxiaml <xiapengcheng4@h-partners.com>

--- rosen/modules/render_service/core/feature/uifirst/rs_sub_thread_cache.h
+++ rosen/modules/render_service/core/feature/uifirst/rs_sub_thread_cache.h
@@ -325,13 +325,13 @@ private:
 
         bool IsUifirstScale() const
         {
-            return ROSEN_NGE(scaleRatio, 0 .0f) && ROSEN_NE(scaleRatio, 1.0f);
+            return ROSEN_GNE(scaleRatio, 0.0f) && ROSEN_NE(scaleRatio, 1.0f);
         }
 
         void SetCacheUifirstScale(float paramScaleRatio)
         {
-            scaleRatio = ROSEN_NGE(paramScaleRatio, 0.0f) ? paramScaleRatio : 1.0f;
-            inverseScaleRatio = ROSEN_NGE(scaleRatio, 0.0f) ? 1.0f / scaleRatio : 1.0f;
+            scaleRatio = ROSEN_GNE(paramScaleRatio, 0.0f) ? paramScaleRatio : 1.0f;
+            inverseScaleRatio = ROSEN_GNE(scaleRatio, 0.0f) ? 1.0f / scaleRatio : 1.0f;
         }
     };
     CacheSurfaceInfo cacheSurfaceInfo_;
