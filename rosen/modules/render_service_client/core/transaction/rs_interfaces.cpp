fa809b9968f43e63905cfa9720905de757baa8b0
anderxiaml <xiapengcheng4@h-partners.com>
2026-06-11 00:33:45 +0800

    上划进入多任务，动效降档（分辨率）     Co-Authored-By: Agent     Signed-off-by: anderxiaml <xiapengcheng4@h-partners.com>

--- rosen/modules/render_service_client/core/transaction/rs_interfaces.cpp
+++ rosen/modules/render_service_client/core/transaction/rs_interfaces.cpp
@@ -1239,7 +1239,7 @@ int32_t RSInterfaces::SetLogicalCameraRotationCorrection(ScreenId id, ScreenRota
 bool RSInterfaces::SetUifirstScale(float scaleFactor)
 {
     scaleFactor = std::round(scaleFactor * 100.0f) / 100.0f;
-    if (scaleFactor <= 0.0f || scaleFactor > 1.0f) {
+    if (ROSEN_LE(scaleFactor, 0.0f) || ROSEN_GNE(scaleFactor, 1.0f)) {
         ROSEN_LOGE("RSInterfaces::SetUifirstScale invalid scaleFactor:%{public}f", scaleFactor);
         return false;
     }
