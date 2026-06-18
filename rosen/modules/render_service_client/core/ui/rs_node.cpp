7a30f4c2ff33247a2e2cfe435cd0c4cd57bd6313
cong2048 <zhangzhicong3@huawei.com>
2026-06-18 16:30:18 +0800

    fix SetHDRColorHeadroom
    Signed-off-by: cong2048 <zhangzhicong3@huawei.com>

--- rosen/modules/render_service_client/core/ui/rs_node.cpp
+++ rosen/modules/render_service_client/core/ui/rs_node.cpp
@@ -2846,6 +2846,10 @@ void RSNode::SetHDRColorHeadroom(const float& headroom)
         ROSEN_LOGE("SetHDRColorHeadroom only can be greater than or equal to one");
         return;
     }
+    if (std::fabs(lastHDRColorHeadroom_ - headroom) < FLT_EPSILON) {
+        return;
+    }
+    lastHDRColorHeadroom_ = headroom;
     SetPropertyNG<ModifierNG::RSHDRBrightnessModifier, &ModifierNG::RSHDRBrightnessModifier::SetHDRColorHeadroom>(
         headroom);
 }
