f7709109f390c4c11f89814299aef025d48494be
uhappy-jun <huangweijun18@huawei.com>
2026-07-15 16:48:56 +0800

    tdd 修改
    Signed-off-by: uhappy-jun <huangweijun18@huawei.com>

--- rosen/test/render_service/render_service_base/unittest/command/rs_node_command_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/command/rs_node_command_test.cpp
@@ -912,55 +912,6 @@ HWTEST_F(RSNodeCommandTest, SetUIFirstSwitchTest002, TestSize.Level1)
 }
 
 /**
- * @tc.name: UpdatePropertyTypeMismatch001
- * @tc.desc: Test UpdateProperty with type mismatch - property type is float but update with int.
- * @tc.type: FUNC
- */
-HWTEST_F(RSNodeCommandTest, UpdatePropertyTypeMismatch001, TestSize.Level1)
-{
-    RSContext context;
-    NodeId nodeId = 1;
-    PropertyId propertyId = 100;
-
-    RSCanvasNodeCommandHelper::Create(context, nodeId, false);
-    auto node = context.GetNodeMap().GetRenderNode<RSRenderNode>(nodeId);
-    ASSERT_NE(node, nullptr);
-
-    auto floatProperty = std::make_shared<RSRenderProperty<float>>(1.0f, propertyId);
-    node->RegisterProperty(floatProperty);
-    EXPECT_FALSE(RSNodeCommandHelper::CheckPropertyType(*floatProperty, RSPropertyType::BOOL, nodeId));
-
-    float initialValue = floatProperty->Get();
-    RSNodeCommandHelper::UpdateProperty<bool>(context, nodeId, true, propertyId, UPDATE_TYPE_OVERWRITE);
-
-    EXPECT_EQ(floatProperty->Get(), initialValue);
-}
-
-/**
- * @tc.name: UpdatePropertyTypeMatch001
- * @tc.desc: Test UpdateProperty with correct type match - float property with float value.
- * @tc.type: FUNC
- */
-HWTEST_F(RSNodeCommandTest, UpdatePropertyTypeMatch001, TestSize.Level1)
-{
-    RSContext context;
-    NodeId nodeId = 2;
-    PropertyId propertyId = 200;
-
-    RSCanvasNodeCommandHelper::Create(context, nodeId, false);
-    auto node = context.GetNodeMap().GetRenderNode<RSRenderNode>(nodeId);
-    ASSERT_NE(node, nullptr);
-
-    auto floatProperty = std::make_shared<RSRenderProperty<float>>(0.0f, propertyId);
-    node->RegisterProperty(floatProperty);
-    EXPECT_TRUE(RSNodeCommandHelper::CheckPropertyType(*floatProperty, RSPropertyType::FLOAT, nodeId));
-
-    RSNodeCommandHelper::UpdateProperty<float>(context, nodeId, 42.5f, propertyId, UPDATE_TYPE_OVERWRITE);
-
-    EXPECT_EQ(floatProperty->Get(), 42.5f);
-}
-
-/**
  * @tc.name: UpdateModifierNGDrawCmdListTypeMismatch001
  * @tc.desc: Test UpdateModifierNGDrawCmdList with type mismatch property.
  * @tc.type: FUNC
